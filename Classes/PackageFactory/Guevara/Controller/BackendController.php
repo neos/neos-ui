ce1f78cfda235c0015c2b5e4ec4138d5ddf66d7f
Wilhelm Behncke <behncke@sitegeist.de>
2015-12-06 17:53:24 +0100

    [TASK] Hack some login routine in front of the neos backend

--- Classes/PackageFactory/Guevara/Controller/BackendController.php
+++ Classes/PackageFactory/Guevara/Controller/BackendController.php
@@ -9,9 +9,45 @@ namespace PackageFactory\Guevara\Controller;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Mvc\Controller\ActionController;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\TYPO3CR\Domain\Service\ContextFactoryInterface;
+use TYPO3\Neos\Domain\Repository\DomainRepository;
+use TYPO3\Neos\Domain\Repository\SiteRepository;
+use TYPO3\Neos\Domain\Service\ContentContext;
+use TYPO3\Neos\Service\UserService;
+use TYPO3\Flow\Persistence\PersistenceManagerInterface;
 
 class BackendController extends ActionController
 {
+
+    /**
+     * @Flow\Inject
+     * @var UserService
+     */
+    protected $userService;
+
+    /**
+     * @Flow\Inject
+     * @var ContextFactoryInterface
+     */
+    protected $contextFactory;
+
+    /**
+     * @Flow\Inject
+     * @var DomainRepository
+     */
+    protected $domainRepository;
+
+    /**
+     * @Flow\Inject
+     * @var SiteRepository
+     */
+    protected $siteRepository;
+
+    /**
+     * @Flow\Inject
+     * @var PersistenceManagerInterface
+     */
+    protected $persistenceManager;
 
     /**
      * Displays the backend interface
@@ -19,9 +55,47 @@ class BackendController extends ActionController
      * @param NodeInterface $node The node that will be displayed on the first tab
      * @return void
      */
-    public function indexAction(NodeInterface $node)
+    public function indexAction(NodeInterface $node = null)
     {
-      	$this->view->assign('node', $node);
+        if($user = $this->userService->getBackendUser()) {
+            if ($node === null) {
+                $workspaceName = $this->userService->getCurrentWorkspaceName();
+                $contentContext = $this->createContext($workspaceName);
+
+                $contentContext->getWorkspace();
+                $this->persistenceManager->persistAll();
+
+                $node = $contentContext->getCurrentSiteNode();
+            }
+
+            $this->view->assign('node', $node);
+            return;
+        }
+
+        $this->redirectToUri($this->uriBuilder->uriFor('index', array(), 'Login', 'PackageFactory.Guevara'));
     }
 
+    /**
+     * Create a ContentContext to be used for the backend redirects.
+     *
+     * @param string $workspaceName
+     * @return ContentContext
+     */
+    protected function createContext($workspaceName)
+    {
+        $contextProperties = array(
+            'workspaceName' => $workspaceName,
+            'invisibleContentShown' => true,
+            'inaccessibleContentShown' => true
+        );
+
+        $currentDomain = $this->domainRepository->findOneByActiveRequest();
+        if ($currentDomain !== null) {
+            $contextProperties['currentSite'] = $currentDomain->getSite();
+            $contextProperties['currentDomain'] = $currentDomain;
+        } else {
+            $contextProperties['currentSite'] = $this->siteRepository->findFirstOnline();
+        }
+        return $this->contextFactory->create($contextProperties);
+    }
 }

d2b5e717642398eec7f49377a775fb019a9fa967
Wilhelm Behncke <behncke@sitegeist.de>
2015-12-06 12:32:24 +0100

    [TASK] Initial Commit

---
+++ Classes/PackageFactory/Guevara/Aspects/AugmentationAspect.php
@@ -0,0 +1,18 @@
+<?php
+namespace PackageFactory\Guevara\Aspects;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "PackageFactory.Guevara".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Aspect
+ */
+class AugmentationAspect {
+
+
+
+}

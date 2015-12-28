9b8f87100e348c6e24f1bde5c10a696a0e971026
Tyll Weiß <inkdpixels@googlemail.com>
2015-12-28 09:51:55 +0100

    [TASK] Move from gulp to npm scripts to flatten the dependency tree

--- Build/Gulp/Tasks/Minify.js
+++
@@ -1,3 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('minify', ['minify:scripts', 'minify:styles', 'minify:images']);
--- Gulpfile.js
+++
@@ -1,7 +0,0 @@
-var build = require('./Build/Gulp/Index.js');
-
-// Add package your configurations here.
-// The tasks for each package will be automatically created.
-build.addPackages([
-    require('./BuildConfig.js')
-]);

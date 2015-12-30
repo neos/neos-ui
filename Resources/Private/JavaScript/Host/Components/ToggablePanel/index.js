926459890edde4d1b6e1db8e71f07772045737b4
Tyll Weiß <inkdpixels@googlemail.com>
2015-12-30 19:53:44 +0100

    [TASK] Animate the ToggablePanel component

--- Resources/Private/JavaScript/Host/Components/ToggablePanel/index.js
+++ Resources/Private/JavaScript/Host/Components/ToggablePanel/index.js
@@ -1,8 +1,15 @@
 import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+import {TransitionMotion, spring} from 'react-motion';
 import mergeClassNames from 'classnames';
 import Headline from '../Headline/';
 import IconButton from '../IconButton/';
 import style from './style.css';
+
+const gentle = [120, 14];
+const defaultAnimatedStyles = {
+    height: spring(0)
+};
 
 export default class ToggablePanel extends Component {
     static propTypes = {
@@ -17,7 +24,11 @@ export default class ToggablePanel extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {isOpened: false};
+        this.state = {
+            isOpened: false,
+            isContentHeightMirrorHidden: false,
+            contentHeight: 0
+        };
     }
 
     render() {
@@ -26,29 +37,94 @@ export default class ToggablePanel extends Component {
             className,
             children
         } = this.props;
-        const {isOpened} = this.state;
+        const {isOpened, isContentHeightMirrorHidden} = this.state;
         const classNames = mergeClassNames({
             [className]: className && className.length,
             [style.panel]: true,
             [style['panel--isOpen']]: isOpened
         });
         const icon = isOpened ? 'chevron-up' : 'chevron-down';
+        const contentHeightMirrorStyle = {
+            display: isContentHeightMirrorHidden ? 'none' : 'block'
+        };
 
         return (
             <div className={classNames}>
                 <Headline className={style.panel__headline} title={title} type="h1" style="h4" />
                 <IconButton className={style.panel__toggleBtn} icon={icon} onClick={this.togglePanel.bind(this)} />
-
-                <div className={style.panel__contents}>
+                <div ref="contentHeightMirror" style={contentHeightMirrorStyle}>
                     {children}
                 </div>
+
+                <TransitionMotion
+                    defaultStyles={this.getDefaultValue()}
+                    styles={this.getEndValue()}
+                    willLeave={this.willLeave}
+                    willEnter={this.willEnter}
+                    >
+                  {styles =>
+                      <div className={style.panel__contents}>
+                        {Object.keys(styles).map((key, index) => {
+                            const {...inlineStyle} = styles[key];
+                            return (
+                              <div key={index} style={inlineStyle} className={style.panel__contents__target}>
+                                {children}
+                              </div>
+                          );
+                        })}
+                    </div>
+                  }
+                </TransitionMotion>
             </div>
         );
+    }
+
+    componentDidMount() {
+        this.updateContentHeight();
+        this.hideContentHeightMirror();
     }
 
     togglePanel() {
         this.setState({
             isOpened: !this.state.isOpened
+        });
+    }
+
+    getDefaultValue() {
+        return {
+            children: defaultAnimatedStyles
+        };
+    }
+
+    getEndValue() {
+        const {isOpened, contentHeight} = this.state;
+
+        return isOpened ? {
+            children: {
+                height: spring(contentHeight, gentle)
+            }
+        } : {};
+    }
+
+    willEnter() {
+        return defaultAnimatedStyles;
+    }
+
+    willLeave() {
+        return defaultAnimatedStyles;
+    }
+
+    updateContentHeight() {
+        const node = ReactDOM.findDOMNode(this.refs.contentHeightMirror);
+
+        this.setState({
+            contentHeight: node.clientHeight
+        });
+    }
+
+    hideContentHeightMirror() {
+        this.setState({
+            isContentHeightMirrorHidden: true
         });
     }
 }

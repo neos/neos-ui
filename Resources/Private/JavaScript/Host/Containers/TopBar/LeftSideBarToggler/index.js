4a18a8ae52cef50cf7b64a1d6b9f971c96f3f800
Tyll Weiß <inkdpixels@googlemail.com>
2015-12-30 20:56:21 +0100

    [TASK] Add an active state for the ToggleLeftSideBar-Button Component

--- Resources/Private/JavaScript/Host/Containers/TopBar/LeftSideBarToggler/index.js
+++ Resources/Private/JavaScript/Host/Containers/TopBar/LeftSideBarToggler/index.js
@@ -1,20 +1,32 @@
 import React, { Component, PropTypes } from 'react';
 import {connect} from 'react-redux';
+import mergeClassNames from 'classnames';
 import actions from '../../../Actions/';
 import {Button, I18n} from '../../../Components/';
+import {immutableOperations} from '../../../../Shared/Util';
+import style from './style.css';
 
-@connect()
+const {$get} = immutableOperations;
+
+@connect(state => ({
+    isSideBarHidden: $get(state, 'ui.leftSidebar.isHidden')
+}))
 export default class LeftSideBarToggler extends Component {
     static propTypes = {
         className: PropTypes.string,
+        isSideBarHidden: PropTypes.bool.isRequired,
         dispatch: PropTypes.any.isRequired
     };
 
     render() {
-        const {className} = this.props;
+        const {className, isSideBarHidden} = this.props;
+        const classNames = mergeClassNames({
+            [className]: true,
+            [style['btn--isActive']]: !isSideBarHidden
+        });
 
         return (
-            <Button className={className} style="clean" hoverStyle="clean" onClick={this.onLeftSidebarToggle.bind(this)}>
+            <Button className={classNames} style="clean" hoverStyle="clean" onClick={this.onLeftSidebarToggle.bind(this)}>
                 <I18n target="Navigate" />
             </Button>
         );

ec681b5eadb260ae47397644587e16539a1f1fca
Tyll Weiß <inkdpixels@googlemail.com>
2015-12-30 12:05:44 +0100

    [TASK] Correct the order of the PageTreeToolBar Buttons

--- Resources/Private/JavaScript/Host/Containers/LeftSideBar/NodeTreeToolBar/index.js
+++ Resources/Private/JavaScript/Host/Containers/LeftSideBar/NodeTreeToolBar/index.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
-import * as buttons from './Buttons/';
-import map from 'lodash.map';
+import {
+    AddNode,
+    CopySelectedNode,
+    CutSelectedNode,
+    DeleteSelectedNode,
+    EditSelectedNode,
+    HideSelectedNode,
+    PasteClipBoardNode
+} from './Buttons/';
 import style from './style.css';
 
 export default class NodeTreeToolBar extends Component {
     render() {
         return (
             <div className={style.toolBar}>
-                {map(buttons, (Button, index) => <Button key={index} />)}
+                <AddNode />
+                <EditSelectedNode />
+                <HideSelectedNode />
+                <CopySelectedNode />
+                <CutSelectedNode />
+                <PasteClipBoardNode />
+                <DeleteSelectedNode />
             </div>
         );
     }

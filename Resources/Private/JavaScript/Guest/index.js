16e89dea433ef9d625a8e9b2db4302d6b529561b
Wilhelm Behncke <behncke@sitegeist.de>
2015-12-30 21:03:14 +0100

    [TASK] Close dropdowns on window/iframe click

--- Resources/Private/JavaScript/Guest/index.js
+++ Resources/Private/JavaScript/Guest/index.js
@@ -9,3 +9,19 @@ tabManager.commitDocumentLoad(window.name, window['@PackageFactory.Guevara:Docum
 const DOMConnector = domConnector();
 
 document.addEventListener('DOMContentLoaded', () => DOMConnector.run());
+
+//
+// Forward click event to host frame, since this seems not to happen automatically *weird*
+//
+document.addEventListener('click', (e) => {
+    const parentDocument = window.parent.document;
+
+    if (parentDocument.createEvent) {
+        const eventObject = parentDocument.createEvent('MouseEvents');
+        eventObject.initEvent('click', true, false);
+        parentDocument.dispatchEvent(eventObject);
+    } else if (parentDocument.createEventObject) {
+        const eventObject = parentDocument.createEventObject();
+        parentDocument.fireEvent('onclick', eventObject);
+    }
+});

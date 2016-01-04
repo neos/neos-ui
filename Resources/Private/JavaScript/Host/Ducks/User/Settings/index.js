2635199771fe53a2dff428946e890c66b9a611d9
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-01-04 21:16:21 +0100

    [TASK] Implement User/Settings as ducks container

---
+++ Resources/Private/JavaScript/Host/Ducks/User/Settings/index.js
@@ -0,0 +1,29 @@
+import {immutableOperations} from '../../../Shared/Util/';
+
+const {$get, $set} = immutableOperations;
+
+const TOGGLE_AUTO_PUBLISHING = '@packagefactory/guevara/User/Settings/TOGGLE_AUTO_PUBLISHING';
+
+export default function reducer (state, action) {
+    switch(action.type) {
+
+        case TOGGLE_AUTO_PUBLISHING:
+            const isCurrentlyEnabled = $get(state, 'user.settings.isAutoPublishingEnabled');
+
+            return $set(state, 'user.settings.isAutoPublishingEnabled', !isCurrentlyEnabled);
+
+        default: return state;
+
+    }
+}
+
+/**
+ * Toggles the auto publishing mode for the current logged in user.
+ *
+ * @return {Object}
+ */
+export function toggleAutoPublishing() {
+    return {
+        type: TOGGLE_AUTO_PUBLISHING
+    };
+}

35895dc242c54075f9aa05834c07f598ed508435
Tyll Weiß <inkdpixels@googlemail.com>
2016-01-23 14:09:36 +0100

    TASK: Add tests for the Tabs reducer/actions

--- Resources/Private/JavaScript/Host/Redux/UI/Tabs/index.spec.js
+++ Resources/Private/JavaScript/Host/Redux/UI/Tabs/index.spec.js
@@ -102,4 +102,76 @@ describe('"Tabs" ', () => {
             expect(activeTab.get('id')).to.equal('myTabId');
         });
     });
+
+    describe('"setMetaData" action.', () => {
+        it('should set the the given meta data to the given "tabId" map.', () => {
+            const metaData = {
+                title: 'My tab title',
+                contextPath: '/context/path/to/node@user-username;language=en_US',
+                workspace: {
+                    name: 'username',
+                    publishingState: {
+                        publishableNodes: [{
+                            contextPath: '/sites/neosdemotypo3org/pageNode/nodeType/node52a3147fd7aae@user-username;language=en_US',
+                            documentContextPath: '/sites/neosdemotypo3org/nodeType@user-username;language=en_US'
+                        }]
+                    }
+                }
+            };
+
+            store.dispatch(add('myTabId', 'http://127.0.0.1/'));
+            store.dispatch(setMetaData('myTabId', metaData));
+
+            const tab = store.getState().get('byId').get('myTabId');
+            const workspace = tab.get('workspace');
+
+            expect(tab).to.be.an.instanceof(Immutable.Map);
+            expect(tab.get('title')).to.equal('My tab title');
+            expect(tab.get('contextPath')).to.equal('/context/path/to/node@user-username;language=en_US');
+            expect(workspace).to.be.an.instanceof(Immutable.Map);
+            expect(workspace.get('name')).to.equal('username');
+            expect(workspace.get('publishingState')).to.be.an.instanceof(Immutable.Map);
+            expect(workspace.get('publishingState').get('publishableNodes')).to.be.an.instanceof(Immutable.List);
+            expect(workspace.get('publishingState').get('publishableNodes').get(0)).to.be.an.instanceof(Immutable.Map);
+        });
+    });
+
+    describe('"updateWorkspaceInfo" action.', () => {
+        it('should update the the given workspace data in the given "tabId" map.', () => {
+            const metaData = {
+                title: 'My tab title',
+                contextPath: '/context/path/to/node@user-username;language=en_US',
+                workspace: {
+                    name: 'username',
+                    publishingState: {
+                        publishableNodes: [{
+                            contextPath: '/sites/neosdemotypo3org/pageNode/nodeType/node52a3147fd7aae@user-username;language=en_US',
+                            documentContextPath: '/sites/neosdemotypo3org/nodeType@user-username;language=en_US'
+                        }]
+                    }
+                }
+            };
+
+            store.dispatch(add('myTabId', 'http://127.0.0.1/'));
+            store.dispatch(setMetaData('myTabId', metaData));
+
+            const updatedWorkspace = [{
+                contextPath: '/sites/neosdemotypo3org/pageNode/nodeType/node52a3147fd7aae@user-username;language=en_US',
+                documentContextPath: '/sites/neosdemotypo3org/nodeType@user-username;language=en_US'
+            }, {
+                contextPath: '/sites/neosdemotypo3org/pageNode/nodeType/node32b31a97fe6aae@user-username;language=en_US',
+                documentContextPath: '/sites/neosdemotypo3org/nodeType@user-username;language=en_US'
+            }];
+
+            store.dispatch(updateWorkspaceInfo('/context/path/to/node@user-username;language=en_US', 'username', updatedWorkspace));
+
+            const tab = store.getState().get('byId').get('myTabId');
+            const workspace = tab.get('workspace');
+
+            expect(workspace.get('publishingState').get('publishableNodes').count()).to.equal(2);
+            expect(
+                workspace.get('publishingState').get('publishableNodes').get(1).get('contextPath')
+            ).to.equal('/sites/neosdemotypo3org/pageNode/nodeType/node32b31a97fe6aae@user-username;language=en_US');
+        });
+    });
 });

3dd276de1a141137e295051b3993ac1e3e24eb8e
Tyll Weiß <inkdpixels@googlemail.com>
2016-02-02 20:50:46 +0100

    DOCS: Add a short explanary header for the webpack.tests.js file

--- webpack.tests.js
+++ webpack.tests.js
@@ -1,3 +1,8 @@
+//
+// This file serves as the main entry file for karma.
+// It will require all files ending with `.spec.js`
+// and compile them with webpack on the fly.
+//
 const context = require.context('./Resources/Private/JavaScript', true, /.spec\.js$/);
 
 context.keys().forEach(context);

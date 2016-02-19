792b47cfbfa8e582adc047d1f8c749f093f9f8a2
Tyll Weiß <inkdpixels@googlemail.com>
2016-02-19 14:02:48 +0100

    [TASK] Initial draft for the behavior test setup

---
+++ Resources/Private/JavaScript/Host/index.behavior.js
@@ -0,0 +1,5 @@
+describe('Neos UI', () => {
+    it('should have the correct page title', () => {
+        expect(browser.getTitle()).to.equal('PackageFactory Guevara - Viva la Revolución!');
+    });
+});

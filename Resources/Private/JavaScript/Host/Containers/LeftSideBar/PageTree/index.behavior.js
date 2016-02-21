4399ec35d9d5f9baf81b2c5a01f07a648efd5faa
Tyll Weiß <inkdpixels@googlemail.com>
2016-02-21 15:38:08 +0100

    TASK: Await the existence of the PageTree contents before expecting results in the PageTree integrational spec

--- Resources/Private/JavaScript/Host/Containers/LeftSideBar/PageTree/index.behavior.js
+++ Resources/Private/JavaScript/Host/Containers/LeftSideBar/PageTree/index.behavior.js
@@ -1,30 +1,40 @@
+const wrapperSelector = '#neos__leftSidebar__pageTree';
+const subTreeTogglerSelector = '#neos__leftSidebar__pageTree [data-neos-integrational-test="tree__item__nodeHeader__subTreetoggle"]';
+const itemNameSelector = '#neos__leftSidebar__pageTree [data-neos-integrational-test="tree__item__nodeHeader__itemLabel"]';
+
 describe('LeftSideBar.PageTree', () => {
     it('should be able to open child trees when clicking on an treeToggleChevron.', () => {
-        const sizeBefore = browser.getElementSize('#neos__leftSidebar__pageTree');
+        browser.waitForVisible(subTreeTogglerSelector);
 
-        browser.click('#neos__leftSidebar__pageTree [data-neos-integrational-test="tree__item__nodeHeader__subTreetoggle"]');
+        const sizeBefore = browser.getElementSize(wrapperSelector);
+
+        browser.click(subTreeTogglerSelector);
         browser.pause(1000);
 
-        const sizeAfter = browser.getElementSize('#neos__leftSidebar__pageTree');
+        const sizeAfter = browser.getElementSize(wrapperSelector);
 
         expect(sizeAfter.height).to.be.above(sizeBefore.height);
     });
 
     it('should be able to close child trees when clicking on an treeToggleChevron.', () => {
-        const sizeBefore = browser.getElementSize('#neos__leftSidebar__pageTree');
+        browser.waitForVisible(subTreeTogglerSelector);
 
-        browser.click('#neos__leftSidebar__pageTree [data-neos-integrational-test="tree__item__nodeHeader__subTreetoggle"]');
+        const sizeBefore = browser.getElementSize(wrapperSelector);
+
+        browser.click(subTreeTogglerSelector);
         browser.pause(1000);
 
-        const sizeAfter = browser.getElementSize('#neos__leftSidebar__pageTree');
+        const sizeAfter = browser.getElementSize(wrapperSelector);
 
         expect(sizeAfter.height).to.be.below(sizeBefore.height);
     });
 
     it('should change to the given page when clicking on the name of a pagetree item.', () => {
+        browser.waitForVisible(subTreeTogglerSelector);
+
         const srcBefore = browser.getAttribute('#neos__contentView iframe', 'src');
 
-        browser.click('#neos__leftSidebar__pageTree [data-neos-integrational-test="tree__item__nodeHeader__itemLabel"]');
+        browser.click(itemNameSelector);
         browser.pause(1000);
 
         const srcAfter = browser.getAttribute('#neos__contentView iframe', 'src');

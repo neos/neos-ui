92f29a6d4abde227b19c686676e14866735303b4
Tyll Weiß <inkdpixels@googlemail.com>
2016-02-21 17:11:03 +0100

    TASK: Add and speed up the integrational tests for the UserDropDown

--- Resources/Private/JavaScript/Host/Containers/TopBar/UserDropDown/index.behavior.js
+++ Resources/Private/JavaScript/Host/Containers/TopBar/UserDropDown/index.behavior.js
@@ -1,7 +1,11 @@
 describe('TopBar.UserDropDown', () => {
+    it('should initially hide the dropdown items.', () => {
+        expect(browser.isVisibleWithinViewport('#neos__topBar__userDropDown__logoutButton')).to.equal(false);
+        expect(browser.isVisibleWithinViewport('#neos__topBar__userDropDown__userSettings')).to.equal(false);
+    });
+
     it('should display the dropdown items after the button was clicked.', () => {
         browser.click('#neos__topBar__userDropDownButton');
-        browser.pause(400);
 
         expect(browser.isVisibleWithinViewport('#neos__topBar__userDropDown__logoutButton')).to.equal(true);
         expect(browser.isVisibleWithinViewport('#neos__topBar__userDropDown__userSettings')).to.equal(true);
@@ -9,7 +13,6 @@ describe('TopBar.UserDropDown', () => {
 
     it('should hide the dropdown items after the button was clicked again.', () => {
         browser.click('#neos__topBar__menuToggler');
-        browser.pause(400);
 
         expect(browser.isVisibleWithinViewport('#neos__topBar__userDropDown__logoutButton')).to.equal(false);
         expect(browser.isVisibleWithinViewport('#neos__topBar__userDropDown__userSettings')).to.equal(false);

6d3e36ea1087fa5bad686175f99c3d4a859fbf4a
Tyll Weiß <inkdpixels@googlemail.com>
2016-02-23 14:33:55 +0100

    TASK: Remove unused assertions from the InlineEditing story

--- Tests/InlineEditing.story.js
+++ Tests/InlineEditing.story.js
@@ -1,4 +1,4 @@
-const {selectors, keys} = __neosSelenium;
+const {selectors} = __neosSelenium;
 let beforeAddedText;
 let addedText;
 
@@ -28,36 +28,6 @@ describe('Inline editing', () => {
     // Make sure that each test starts in the Host frame.
     beforeEach(() => browser.frameParent());
 
-    it('should initially hide the publish dropdown contents.', () => {
-        return browser.isVisibleWithinViewport(selectors.topBar.publishDropDown.contents).should.equal(false);
-    });
-
-    it('should initially mark the `Publish` button as disabled.', () => {
-        return browser.isEnabled(selectors.topBar.publishDropDown.publishBtn).should.equal(false);
-    });
-
-    it('should initially mark the `Publish All` button as disabled.', () => {
-        return browser.isEnabled(selectors.topBar.publishDropDown.publishAllBtn).should.equal(false);
-    });
-
-    it('should initially mark the `Discard` button as disabled.', () => {
-        return browser.isEnabled(selectors.topBar.publishDropDown.discardBtn).should.equal(false);
-    });
-
-    it('should initially mark the `Discard All` button as disabled.', () => {
-        return browser.isEnabled(selectors.topBar.publishDropDown.discardAllBtn).should.equal(false);
-    });
-
-    it('should display the publish dropdown contents after the publish dropdown chevron was clicked.', () => {
-        return browser.click(selectors.topBar.publishDropDown.btn)
-            .isVisibleWithinViewport(selectors.topBar.publishDropDown.contents).should.equal(true);
-    });
-
-    it('should hide the dropdown items after the button was clicked again.', () => {
-        return browser.click(selectors.topBar.publishDropDown.btn)
-            .isVisibleWithinViewport(selectors.topBar.publishDropDown.contents).should.equal(false);
-    });
-
     it('should be able to type into a nodeType in the guest frame.', () => {
         addedText = `my added text ${Math.random()}`;
 
@@ -67,25 +37,28 @@ describe('Inline editing', () => {
         beforeAddedText = browser.getText(selectors.guestFrame.inlineEditableNodeTypes)[0];
 
         const result = browser.click(selectors.guestFrame.inlineEditableNodeTypes)
-            .keys([keys.CTRL_CMD, 'a'])
-            .keys([keys.CTRL_CMD, 'x'])
             .keys(addedText)
             .elementIdText(browser.element(selectors.guestFrame.inlineEditableNodeTypes).value.ELEMENT);
 
         expect(result.value).to.contain(addedText);
 
+        // Wait a bit until the server request has finished.
         browser.pause(2000);
     });
 
     it('should persist the changes on the server after editing the nodeType in the guest frame and reloading the backend.', () => {
         return browser.refresh()
             .frame(browser.element(selectors.guestFrame.iframe).value)
-            .waitUntil(() => browser.getText(selectors.guestFrame.inlineEditableNodeTypes).then(res => res[0] === `x${addedText}`));
+            .waitUntil(() => browser.getText(selectors.guestFrame.inlineEditableNodeTypes).then(res => res[0].indexOf(addedText) > -1));
+    });
+
+    it('should display the publish dropdown contents after the publish dropdown chevron was clicked.', () => {
+        return browser.click(selectors.topBar.publishDropDown.btn)
+            .isVisibleWithinViewport(selectors.topBar.publishDropDown.contents).should.equal(true);
     });
 
     it('should reset all changes when clicking the discard button.', () => {
         return browser
-            .click(selectors.topBar.publishDropDown.btn)
             .click(selectors.topBar.publishDropDown.discardBtn)
             .frame(browser.element(selectors.guestFrame.iframe).value)
             .waitUntil(() => browser.getText(selectors.guestFrame.inlineEditableNodeTypes).then(res => res[0] === beforeAddedText));

18161448035049c9f2c9d497f1321fcf54831ddf
Tyll Weiß <inkdpixels@googlemail.com>
2016-03-03 12:21:00 +0100

    TASK: Simplify the DimensionSwitcher Container into smaller Components #14

--- Resources/Private/JavaScript/Host/Containers/ContextBar/DimensionSwitcher/index.js
+++ Resources/Private/JavaScript/Host/Containers/ContextBar/DimensionSwitcher/index.js
@@ -3,6 +3,24 @@ import {connect} from 'react-redux';
 import {DropDown, Icon} from 'Host/Components/';
 import style from './style.css';
 
+const DimensionCategory = props => {
+    const {data, key} = props;
+
+    return (
+        <li key={key} className={style.dimensionCategory}>
+            <Icon icon="globe" padded="right" className={style.dimensionCategory__icon} />
+            {data.name}
+            <br />
+
+            ToDo: Render the select for all dimension items
+        </li>
+    );
+};
+DimensionCategory.propTypes = {
+    data: PropTypes.object.isRequired,
+    key: PropTypes.number
+};
+
 @connect()
 export default class DimensionSwitcher extends Component {
     static propTypes = {
@@ -19,31 +37,12 @@ export default class DimensionSwitcher extends Component {
                     ToDo: Current dimension name
                 </DropDown.Header>
                 <DropDown.Contents className={style.dropDown__contents}>
-                    {dimensions.map((dimensionCategory, index) => this.renderDimensionCategory(dimensionCategory, index))}
+                    {dimensions.map((dimensionCategory, index) => <DimensionCategory data={dimensionCategory} key={index} />)}
                 </DropDown.Contents>
             </DropDown.Wrapper>
         );
     }
-
-    renderDimensionCategory(dimensionCategory, index) {
-        return (
-            <li key={index} className={style.dimensionCategory}>
-                <Icon icon="globe" padded="right" className={style.dimensionCategory__icon} />
-                {dimensionCategory.name}
-                <br />
-
-                ToDo: Render the select for all dimensions
-            </li>
-        );
-    }
 }
 DimensionSwitcher.defaultProps = {
-    dimensions: [{
-        name: 'Language',
-        items: [{
-            name: 'Deutsch'
-        }, {
-            name: 'English'
-        }]
-    }]
+    dimensions: []
 };

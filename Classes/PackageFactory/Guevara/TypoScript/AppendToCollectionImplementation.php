e474e3074a209bf6972130eac8799cfcc3bd453c
Wilhelm Behncke <behncke@sitegeist.de>
2016-03-03 15:04:18 +0100

    TASK: Describe the initial application state as TypoScript

---
+++ Classes/PackageFactory/Guevara/TypoScript/AppendToCollectionImplementation.php
@@ -0,0 +1,28 @@
+<?php
+namespace PackageFactory\Guevara\TypoScript;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TypoScript\TypoScriptObjects\AbstractTypoScriptObject;
+
+class AppendToCollectionImplementation extends AbstractTypoScriptObject
+{
+    /**
+     * Appends an item to the given collection
+     *
+     * @return string
+     */
+    public function evaluate()
+    {
+        $collection = $this->tsValue('collection');
+        $key = $this->tsValue('key');
+        $item = $this->tsValue('item');
+
+        if ($key) {
+            $collection[$key] = $item;
+        } else {
+            $collection[] = $item;
+        }
+
+        return $collection;
+    }
+}

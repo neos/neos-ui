6f19baa80ac5b96ecc7992498330e70177c37876
Philipp Bucher <bucher@navigate.de>
2016-03-07 14:50:39 +0100

    TAK: run command as a separate build-script

---
+++ Build/Docker/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#
+# Safe way of propagating the exit code of all commands through the script.
+# Without this line, commands could fail/exit 1 and the script itself would
+# complete and exit with code 0.
+#
+set -e
+
+#
+# First of, install all application depedencies.
+#
+
+npm prune && npm update
+
+
+npm run watch:build

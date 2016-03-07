c11bbd11be28df2a8f2bba39a9081b96e8a85517
Tyll Weiß <inkdpixels@googlemail.com>
2016-03-07 18:12:06 +0100

    WIP: Add initial unit tests for the neos js API

--- Resources/Private/JavaScript/API/Use/index.spec.js
+++ Resources/Private/JavaScript/API/Use/index.spec.js
@@ -1,4 +1,28 @@
-describe('Neos JS API > use', () => {
-    it('should add a plugin to the API by using the plugin name');
+import chai, {expect} from 'chai';
+import sinon from 'sinon/pkg/sinon.js';
+import sinonChai from 'sinon-chai';
+import initializeUse from './index.js';
+
+chai.should();
+chai.use(sinonChai);
+
+describe.only('Neos JS API > use', () => {
+    it('should return a function when called', () => {
+        expect(initializeUse()).to.be.an('function');
+    });
+
+    it('should call the "addLibrary" function when initializing a plugin factory called', () => {
+        const apiTarget = {};
+        const addLibrary = sinon.spy();
+        const use = initializeUse(addLibrary, apiTarget);
+        const pluginFactory = () => {};
+        pluginFactory.identifier = 'myPlugin';
+
+        use(pluginFactory);
+
+        expect(addLibrary).to.callCount(1);
+        expect(addLibrary).to.have.been.calledWith('myPlugin', pluginFactory());
+    });
+
     it('should pass the api to the plugin factory');
 });
--- Resources/Private/JavaScript/API/index.spec.js
+++ Resources/Private/JavaScript/API/index.spec.js
@@ -1,8 +1,55 @@
+import api, {createPlugin} from './index.js';
+
 describe('Neos JS API', () => {
-    it('should place itself inside the specified parent object.');
-    it('should be aliasable.');
-    it('should not be writable.');
-    it('should throw an error, if a value already exists under the same name in parent.');
-    it('should expose the use method by default.');
-    it('should expose a method to create plugins.');
+    it('should place itself inside the specified parent object with the specified alias.', () => {
+        const win = {};
+
+        api(win, 'test');
+
+        expect(win.test).to.exist; // eslint-disable-line
+    });
+
+    it('should have a fallback alias "neos" if none was specified.', () => {
+        const win = {};
+
+        api(win);
+
+        expect(win.neos).to.exist; // eslint-disable-line
+    });
+
+    it('should throw an error, if a value already exists under the same name in parent.', () => {
+        const win = {};
+        const fn = () => api(win);
+        api(win);
+
+        expect(fn).to.throw();
+    });
+
+    it('should not be writable.', () => {
+        const win = {};
+        const fn = () => {
+            win.neos = 'test';
+        };
+
+        api(win);
+
+        const oldApi = win.neos;
+
+        expect(fn).to.throw();
+        expect(win.neos).to.equal(oldApi);
+    });
+
+    it('should expose the use method by default.', () => {
+        const win = {};
+
+        api(win);
+
+        expect(win.neos.use).to.be.an('function');
+    });
+
+    it('should expose a method to create plugins which will add the given identifier to the constructor.', () => {
+        const plugin = createPlugin('myName', () => null);
+
+        expect(plugin.identifier).to.equal('myName');
+    });
 });

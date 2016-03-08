6e516bfeca01112b0d1d7bcde72af0a8c1f40026
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm-media.de>
2016-03-08 17:21:04 +0100

    TASK: rudimentary tab panel in inspector

--- Resources/Private/JavaScript/Host/Containers/RightSideBar/Inspector/TabPanel/index.js
+++ Resources/Private/JavaScript/Host/Containers/RightSideBar/Inspector/TabPanel/index.js
@@ -1,22 +1,39 @@
 import React, {PropTypes} from 'react';
 
-import {Tabs, ToggablePanel, Label, TextInput} from 'Host/Components/';
+import {Tabs, ToggablePanel, Label, TextInput, I18n} from 'Host/Components/';
 
 import style from '../../style.css';
 
+const generateInspectorGroups = (nodeType, tabIdentifier) => {
+    const groups = nodeType.ui.inspector.groups;
+    return Object.keys(groups).map(groupId => ({
+        ...groups[groupId],
+        id: groupId
+    }))
+    .filter((group) => group.tab === tabIdentifier)
+    .sort((a, b) => (a.position - b.position) || (a.id - b.id));
+};
+
+const renderInspectorGroup = (inspectorGroup, focusedNode) => {
+    return (<ToggablePanel className={style.rightSideBar__section} key={inspectorGroup.id}>
+        <ToggablePanel.Header>
+            <I18n id={inspectorGroup.label}/>
+        </ToggablePanel.Header>
+        <ToggablePanel.Contents>
+            <Label htmlFor="testInput">
+                Title
+            </Label>
+            <TextInput placeholder="Type to search" id="testInput"/>
+        </ToggablePanel.Contents>
+    </ToggablePanel>);
+};
+
 const TabPanel = props => {
-        return (<Tabs.Panel>
-        <ToggablePanel className={style.rightSideBar__section}>
-            <ToggablePanel.Header>
-                My fancy configuration
-            </ToggablePanel.Header>
-            <ToggablePanel.Contents>
-                <Label htmlFor="testInput">
-                    Title
-                </Label>
-                <TextInput placeholder="Type to search" id="testInput" />
-            </ToggablePanel.Contents>
-        </ToggablePanel>
+
+    const inspectorGroups = generateInspectorGroups(props.focusedNode.nodeType, props.tab.id);
+
+    return (<Tabs.Panel>
+        {inspectorGroups.map(inspectorGroup => renderInspectorGroup(inspectorGroup, props.focusedNode))}
     </Tabs.Panel>);
 };
 TabPanel.displayName = 'Inspector Tab Panel';

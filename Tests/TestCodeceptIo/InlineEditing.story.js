1a3c0d0758f0d9691753ec2aff4cabc16d1a62d4
Philipp Bucher <bucher@navigate.de>
2016-03-10 13:08:17 +0100

    TASK: init acceptance tests with codeceptio

---
+++ Tests/TestCodeceptIo/InlineEditing.story.js
@@ -0,0 +1,31 @@
+
+Feature('Inline editing');
+
+
+Scenario('should be able to type into a nodeType in the guest frame.', (I) => {
+    I.amOnPage('/che!/');
+    I.fillField('#username', 'admin');
+    I.fillField('#password', 'password');
+    I.click('//*[@id="neos-login-box"]/div/form/fieldset/div[3]/button[1]');
+    I.waitForElement('#appContainer', 30);
+
+    I.amOnPage('/che!/');
+    I.seeElement({id: 'appContainer'});
+
+    I.waitForElement({css: '#neos__contentView iframe'}, 30);
+    I.focusAndEditInGuestFrame('neos-content-main', 'Hello Worlds');
+});
+
+Scenario('should be able to display the publish dropdown contents after the publish dropdown chevron was clicked.', (I) => {
+    I.amOnPage('/che!/');
+    I.fillField('#username', 'admin');
+    I.fillField('#password', 'password');
+    I.click('//*[@id="neos-login-box"]/div/form/fieldset/div[3]/button[1]');
+    I.waitForElement('#appContainer', 30);
+
+
+    I.dontSeeElementInViewport('#neos__topBar__publishDropDown__contents');
+    I.click('#neos__topBar__publishDropDown__btn');
+    I.seeElementInViewport('#neos__topBar__publishDropDown__contents');
+
+});

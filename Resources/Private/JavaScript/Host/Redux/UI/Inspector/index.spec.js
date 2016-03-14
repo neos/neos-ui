9c82246a310f529375fe6255b0fb98d581ea8bb1
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm-media.de>
2016-03-14 07:48:06 +0100

    TASK: fix styling nitpicks

--- Resources/Private/JavaScript/Host/Redux/UI/Inspector/index.spec.js
+++ Resources/Private/JavaScript/Host/Redux/UI/Inspector/index.spec.js
@@ -38,17 +38,17 @@ describe('"host.redux.ui.inspector" ', () => {
     describe('"writeValue" action.', () => {
         it('should store the last modification.', () => {
             store.dispatch(writeValue('/my/path@user-foo', 'test', 'value'));
-            expect(store.getState().ui.inspector.valuesByNodePath['/my/path@user-foo']['test']).to.equal('value');
+            expect(store.getState().ui.inspector.valuesByNodePath['/my/path@user-foo'].test).to.equal('value');
 
             store.dispatch(writeValue('/my/path@user-foo', 'test', 'new value'));
-            expect(store.getState().ui.inspector.valuesByNodePath['/my/path@user-foo']['test']).to.equal('new value');
+            expect(store.getState().ui.inspector.valuesByNodePath['/my/path@user-foo'].test).to.equal('new value');
         });
     });
 
     describe('"cancel" action.', () => {
         it('should cancel all modifications.', () => {
             store.dispatch(writeValue('/my/path@user-foo', 'test', 'value'));
-            expect(store.getState().ui.inspector.valuesByNodePath['/my/path@user-foo']['test']).to.equal('value');
+            expect(store.getState().ui.inspector.valuesByNodePath['/my/path@user-foo'].test).to.equal('value');
 
             store.dispatch(cancel('/my/path@user-foo'));
             expect(store.getState().ui.inspector.valuesByNodePath['/my/path@user-foo']).to.equal(undefined);

0646468c936a0edab11e89d2ac7fbedd1dc76a31
Wilhelm Behncke <behncke@sitegeist.de>
2016-03-20 14:46:01 +0100

    CLEANUP: Remove EditSelectedNode and RefreshPageTree subcomponents from NodeToolbar, since they are not used

--- Resources/Private/JavaScript/Guest/Containers/NodeToolbar/Buttons/index.js
+++ Resources/Private/JavaScript/Guest/Containers/NodeToolbar/Buttons/index.js
@@ -2,18 +2,14 @@ import AddNode from './AddNode/index';
 import CopySelectedNode from './CopySelectedNode/index';
 import CutSelectedNode from './CutSelectedNode/index';
 import DeleteSelectedNode from './DeleteSelectedNode/index';
-import EditSelectedNode from './EditSelectedNode/index';
 import HideSelectedNode from './HideSelectedNode/index';
 import PasteClipBoardNode from './PasteClipBoardNode/index';
-import RefreshPageTree from './RefreshPageTree/index';
 
 export {
     AddNode,
     CopySelectedNode,
     CutSelectedNode,
     DeleteSelectedNode,
-    EditSelectedNode,
     HideSelectedNode,
-    PasteClipBoardNode,
-    RefreshPageTree
+    PasteClipBoardNode
 };

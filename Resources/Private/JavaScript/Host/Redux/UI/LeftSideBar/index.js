c43f2b661dd24e94f368b466b61148683070afd1
Wilhelm Behncke <behncke@sitegeist.de>
2016-03-27 15:42:45 +0200

    TASK: Describe initial state of ui.leftSideBar as a hydrator function

--- Resources/Private/JavaScript/Host/Redux/UI/LeftSideBar/index.js
+++ Resources/Private/JavaScript/Host/Redux/UI/LeftSideBar/index.js
@@ -1,4 +1,5 @@
 import {createAction} from 'redux-actions';
+import {Map} from 'immutable';
 import {$toggle} from 'plow-js';
 
 const TOGGLE = '@packagefactory/guevara/UI/LeftSideBar/TOGGLE';
@@ -16,11 +17,15 @@ export const actions = {
 };
 
 //
-// Export the initial state
+// Export the initial state hydrator
 //
-export const initialState = {
-    isHidden: false
-};
+export const hydrate = () => new Map({
+    ui: new Map({
+        leftSideBar: new Map({
+            isHidden: false
+        })
+    })
+});
 
 //
 // Export the reducer

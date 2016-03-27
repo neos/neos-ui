ca4d19c80e206ab9fc522d148fe2ead7a75e85b6
Wilhelm Behncke <behncke@sitegeist.de>
2016-03-27 15:58:00 +0200

    TASK: Describe initial state of ui.remote as a hydrator function

--- Resources/Private/JavaScript/Host/Redux/UI/Remote/index.js
+++ Resources/Private/JavaScript/Host/Redux/UI/Remote/index.js
@@ -1,4 +1,5 @@
 import {createAction} from 'redux-actions';
+import {Map} from 'immutable';
 import {$set} from 'plow-js';
 
 const START_SAVING = '@packagefactory/guevara/UI/Remote/START_SAVING';
@@ -51,13 +52,17 @@ export const actions = {
 };
 
 //
-// Export the initial state
+// Export the initial state hydrator
 //
-export const initialState = {
-    isSaving: false,
-    isPublishing: false,
-    isDiscarding: false
-};
+export const hydrate = () => new Map({
+    ui: new Map({
+        remote: new Map({
+            isSaving: false,
+            isPublishing: false,
+            isDiscarding: false
+        })
+    })
+});
 
 //
 // Export the reducer

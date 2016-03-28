dbd0cdb45b5a6e2b115c60d0d8b782aaf290376d
Wilhelm Behncke <behncke@sitegeist.de>
2016-03-28 12:31:08 +0200

    TASK: Create custom prop types for node and nodeType

---
+++ Resources/Private/JavaScript/Shared/PropTypes/CR/Node/index.js
@@ -0,0 +1,23 @@
+import {PropTypes} from 'react';
+
+import {nodeType} from '../NodeType/index';
+
+const nodeShape = {
+    contextPath: PropTypes.string.isRequired,
+    identifier: PropTypes.string.isRequired,
+    isAutoCreated: PropTypes.bool.isRequired,
+    label: PropTypes.string,isRequired,
+    name: PropTypes.string.isRequired,
+    properties: PropTypes.object.isRequired,
+    uri: PropTypes.string.isRequired
+};
+
+export const storedNode = PropTypes.shape({
+    ...nodeShape,
+    nodeType: PropTypes.string.isRequired
+});
+
+export const node = PropTypes.shape({
+    ...nodeShape,
+    nodeType: nodeType.isRequired
+});
---
+++ Resources/Private/JavaScript/Shared/PropTypes/CR/index.js
@@ -0,0 +1,8 @@
+import {storedNode, node} from './Node/index';
+import {nodeType} from './NodeType/index';
+
+export {
+    storedNode,
+    node,
+    nodeType
+};

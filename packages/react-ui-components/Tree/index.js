9548885154abed43c185690d25c9e57b419cac37
Wilhelm Behncke <behncke@sitegeist.de>
2016-03-28 17:42:10 +0200

    TASK: Refactor page tree to a more performant form

--- packages/react-ui-components/Tree/index.js
+++ packages/react-ui-components/Tree/index.js
@@ -1,49 +1,37 @@
 import React, {Component, PropTypes} from 'react';
 import mergeClassNames from 'classnames';
 
-import NodeHeader from './NodeHeader/index';
+import Node from './Node/index';
 import style from './style.css';
 
-export default class Tree extends Component {
+export class Tree extends Component {
     static propTypes = {
-        rootNode: PropTypes.object,
         focused: PropTypes.string,
         active: PropTypes.string,
+        className: PropTypes.string,
+
         onNodeToggle: PropTypes.func,
         onNodeClick: PropTypes.func,
         onNodeFocus: PropTypes.func,
-        className: PropTypes.string
+
+        children: PropTypes.node.isRequired
     };
 
     render() {
-        const {className, rootNode, ...directProps} = this.props;
+        const {className, ...directProps} = this.props;
         const classNames = mergeClassNames({
             [className]: className && className.length,
             [style.treeWrapper]: true
         });
 
         return (
-            <div className={classNames} tabIndex="0" {...directProps}>
-                {this.renderNode(rootNode)}
-            </div>
-        );
-    }
-
-    renderNode(node) {
-        const {contextPath, isCollapsed, children} = node;
-        const {onNodeToggle, onNodeClick, onNodeFocus} = this.props;
-        return (
-            <div key={contextPath}>
-                <NodeHeader
-                    node={node}
-                    onToggle={() => onNodeToggle(node)}
-                    onClick={() => onNodeFocus(node)}
-                    onLabelClick={() => onNodeClick(node)}
-                    />
-                <div className={style.treeWrapper__treeChildren}>
-                    {children && !isCollapsed ? children.map(child => this.renderNode(child)) : null}
-                </div>
-            </div>
+            <Node className={classNames} tabIndex="0" {...directProps}>
+                {this.props.children}
+            </Node>
         );
     }
 }
+
+Tree.Node = Node;
+
+export default Tree;

5e2862dae03a1593250cdcb1a0f2c79b44f7a798
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm-media.de>
2016-03-29 20:45:58 +0200

    WIP: load image data

--- packages/react-ui-components/Editors/Image/index.js
+++ packages/react-ui-components/Editors/Image/index.js
@@ -3,7 +3,7 @@ import reactCrop from 'react-image-crop';
 
 
 const Image = (value) => {
-    console.log("IMAGE", value);
+    //console.log("IMAGE", value);
     return (<div>TODO image</div>);
 };
 

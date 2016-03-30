28e21a2d310987709694a6e2fb364e54e392b620
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm-media.de>
2016-03-30 07:39:15 +0200

    move editors to "Host" directory for now, as discussed with Wilhelm

--- packages/react-ui-components/Editors/DateTime/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {TextInput} from 'Components';
-
-const DateTime = props => {
-    return (<TextInput {...props} />);
-};
-
-export default DateTime;
--- packages/react-ui-components/Editors/TextArea/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TextArea = () => {
-    return (<div>TODO textarea</div>);
-};
-
-export default TextArea;

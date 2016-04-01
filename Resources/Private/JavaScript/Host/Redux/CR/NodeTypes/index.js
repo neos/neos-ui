754564abd52166853229e23ce150a399b8b355f7
Wilhelm Behncke <behncke@sitegeist.de>
2016-04-01 15:24:44 +0200

    TASK: Create a workaround for the ambiguous array conversion of PHPs json_encode

--- Resources/Private/JavaScript/Host/Redux/CR/NodeTypes/index.js
+++ Resources/Private/JavaScript/Host/Redux/CR/NodeTypes/index.js
@@ -1,4 +1,4 @@
-import Immutable, {Map} from 'immutable';
+import Immutable, {Map, List} from 'immutable';
 import {$set, $get} from 'plow-js';
 
 //
@@ -8,12 +8,37 @@ export const actions = {
 };
 
 //
+// Helper function to exchange immutable types at certain paths
+//
+const fixProperties = (...conversions) => subject => conversions.reduce(
+    (subject, [path, wrongType, correctedType]) => $set(
+        path,
+        $get(path, subject) instanceof wrongType
+            ? new correctedType()
+            : $get(path, subject)
+        ,
+        subject
+    ),
+    subject
+);
+
+//
 // Export the initial state hydrator
 //
 export const hydrate = state => $set(
     'cr.nodeTypes',
     new Map({
-        byName: Immutable.fromJS($get('cr.nodeTypes.byName', state)),
+        byName: Immutable.fromJS($get('cr.nodeTypes.byName', state)).map(
+            //
+            // Workaround to fix the result of json_encode's ambiguous
+            // array conversion
+            // TODO: find a better solution for this
+            //
+            fixProperties(
+                ['properties.title.ui.aloha.format', List, Map],
+                ['properties.title.ui.aloha.link', List, Map]
+            )
+        ),
         constraints: Immutable.fromJS($get('cr.nodeTypes.constraints', state)),
         inheritanceMap: Immutable.fromJS($get('cr.nodeTypes.inheritanceMap', state)),
         groups: Immutable.fromJS($get('cr.nodeTypes.groups', state))

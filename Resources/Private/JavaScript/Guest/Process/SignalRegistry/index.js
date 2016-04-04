30e7b81460b0ab0679ee65d29f6a8fc8e53569de
Wilhelm Behncke <behncke@sitegeist.de>
2016-04-04 13:28:41 +0200

    TASK: Introduce Signal prop type

--- Resources/Private/JavaScript/Guest/Process/SignalRegistry/index.js
+++ Resources/Private/JavaScript/Guest/Process/SignalRegistry/index.js
@@ -10,6 +10,12 @@ export const isSignalDescription = maybeSignalDescription =>
     maybeSignalDescription && maybeSignalDescription.$__handle !== undefined;
 
 //
+// Checks, if the given value is a signal
+//
+export const isSignal = maybeSignal =>
+    maybeSignal && maybeSignal.$__signal !== undefined;
+
+//
 // Converts a signal description into a signal
 //
 export const process = signalDescription => {
@@ -38,3 +44,39 @@ export const call = signal => {
 export const createSignal = handler => ({
     $__handle: handler // eslint-disable-line camelcase
 });
+
+const createChainablePropType = validate => {
+    const checkType = (isRequired, props, propName, componentName, location) =>{
+        componentName = componentName || 'ANONYMOUS';
+
+        if (props[propName] == null) {
+            if (isRequired) {
+                return new Error(`Required \`${location}\` \`${propName}\` was not specified in \`${componentName}\``);
+            }
+
+            return null;
+        }
+
+        return validate(props, propName, componentName, location);
+    };
+
+    const chainedCheckType = (...args) => checkType(false, ...args);
+    checkType.isRequired = (...args) => checkType(true, ...args);
+
+    return checkType;
+};
+
+//
+// A custom prop type to validate signals
+//
+export const SignalPropType = createChainablePropType(
+    (props, propName, componentName, location) => {
+        const prop = props[propName];
+
+        if (!isSignal(prop)) {
+            return new Error(`\`${location}\` \`${propName}\` in \`${componentName}\` is not a valid signal`);
+        }
+
+        return null;
+    }
+);

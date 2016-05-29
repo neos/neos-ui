8ce29e6d9a960a1c8c439fd52782efd2b0b38287
Tyll Weiß <info@craft-interactive.de>
2016-05-29 14:39:21 +0200

    TASK: Remove the set -e statement from the init shell script since it isn't needed in this case

--- Build/init.sh
+++ Build/init.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
 #
-# Safe way of propagating the exit code of all commands through the script.
-# Without this line, commands could fail/exit 1 and the script itself would
-# complete and exit with code 0.
-#
-set -e
-
-#
 # Make sure the node & npm version matches our required constraints.
 #
 nvm install

06ba561d5f38f21f264f078cb88e0fda65c47b53
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2016-07-14 11:21:16 +0200

    TASK: Rename "Host/Expose" to "Host/Extensibility"

--- Resources/Private/JavaScript/Host/Sagas/System/index.js
+++ Resources/Private/JavaScript/Host/Sagas/System/index.js
@@ -6,8 +6,8 @@ import createInspectorEditorRegistry from 'Host/Process/InspectorEditorRegistry/
 import createHookRegistry from 'Host/Process/HookRegistry/index';
 import initializeJSAPI from 'API/index';
 import {ui} from 'Host/Plugins/index';
-import createApi from 'Host/Expose/API/index';
-import createHostApi from 'Host/Expose/index';
+import createApi from 'Host/Extensibility/API/index';
+import createHostApi from 'Host/Extensibility/index';
 
 let injectStore = null;
 export const getStore = discover(function* () {

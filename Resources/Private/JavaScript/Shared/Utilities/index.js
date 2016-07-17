3ac894ac35f5ccd2d5af572d9d3c9aa172add96c
Tyll Weiß <info@craft-interactive.de>
2016-07-17 12:10:42 +0200

    CLEANUP: Remove dead code

--- Resources/Private/JavaScript/Shared/Utilities/index.js
+++ Resources/Private/JavaScript/Shared/Utilities/index.js
@@ -1,11 +1,9 @@
-import * as immutableOperations from './ImmutableOperations.js';
 import executeCallback from './ExecuteCallback.js';
 import filterDeep from './FilterDeep.js';
 import * as fontAwesome from './FontAwesome.js';
 import handleActions from './HandleActions.js';
 
 export {
-    immutableOperations,
     executeCallback,
     filterDeep,
     fontAwesome,

532db1850929a0d9bc64e8fad80c71aa35274d20
Tyll Weiß <info@craft-interactive.de>
2016-07-17 13:07:06 +0200

    TASK: Throw font awesome icon warnings only in production for now, since they make debugging a mess

--- packages/react-ui-components/Icon/index.js
+++ packages/react-ui-components/Icon/index.js
@@ -1,7 +1,11 @@
 import React, {PropTypes} from 'react';
 import mergeClassNames from 'classnames';
+import {service} from 'Shared/index';
 import style from './style.css';
-import {fontAwesome} from 'Shared/Utilities/index';
+import {api, fontAwesome} from 'Shared/Utilities/';
+const {logger} = service;
+
+const cachedWarnings = {};
 
 const Icon = props => {
     const {size, padded} = props;
@@ -22,10 +26,36 @@ const Icon = props => {
         <i className={classNames}></i>
     );
 };
-
 Icon.propTypes = {
     // The icon key of Font-Awesome.
-    icon: PropTypes.string,
+    icon(props, propName) {//eslint-disable-line
+        const id = props[propName];
+        const {isValid, isMigrationNeeded, iconName} = fontAwesome.validateId(id);
+        const {systemEnv} = api.get();
+
+        //
+        // ToDo: In the future we need to adjust the `systemEnv`
+        // condition to throw warnings in Development, but not in Production.
+        // For now we disable these warnings in development since they
+        // make debugging a mess.
+        //
+        if (!isValid && systemEnv !== 'Development') {
+            if (isMigrationNeeded && iconName && !cachedWarnings[iconName]) {
+                cachedWarnings[iconName] = true;
+                logger.warn(`Font-Awesome has been updated. The icon name "${id}" has been renamed.
+
+Please adjust the icon configurations in your .yaml files to the new icon name "${iconName}".
+
+https://github.com/FortAwesome/Font-Awesome/wiki/Upgrading-from-3.2.1-to-4`);
+            } else if (!iconName || !cachedWarnings[iconName]) {
+                return new Error(`Icon name "${id}" was not a found in Font-Awesome 4.5.
+Please use the icon names from the Font-Awesome website.
+
+http://fortawesome.github.io/Font-Awesome/icons/`);
+            }
+        }
+    },
+
     // Style related propTypes.
     size: PropTypes.oneOf(['big', 'small', 'tiny']),
     padded: PropTypes.oneOf(['none', 'left', 'right']),

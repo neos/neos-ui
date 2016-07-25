616683389991d7f50804100da142d92cb028b297
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2016-07-25 19:05:07 +0200

    FEATURE: add "italic" styling

--- Resources/Private/JavaScript/Host/manifest.js
+++ Resources/Private/JavaScript/Host/manifest.js
@@ -6,14 +6,25 @@ registry.ckEditor.formattingAndStyling.add('h2', { style: { element: 'h2' } });
 registry.ckEditor.formattingAndStyling.add('h3', { style: { element: 'h3' } });
 registry.ckEditor.formattingAndStyling.add('h4', { style: { element: 'h4' } });
 registry.ckEditor.formattingAndStyling.add('h5', { style: { element: 'h5' } });
+
+
 registry.ckEditor.formattingAndStyling.add('bold', { command: 'bold' });
-
-
 registry.ckEditor.toolbar.add('bold', {
     formatting: 'bold',
     component: IconButton,
     callbackPropName: 'onClick',
 
     icon: 'bold',
+    hoverStyle: 'brand'
+});
+
+
+registry.ckEditor.formattingAndStyling.add('italic', { command: 'italic' });
+registry.ckEditor.toolbar.add('italic', {
+    formatting: 'italic',
+    component: IconButton,
+    callbackPropName: 'onClick',
+
+    icon: 'italic',
     hoverStyle: 'brand'
 });

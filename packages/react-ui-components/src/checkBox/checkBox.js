ec25d4e74a096fa28cc3f4cb71f24eb0554221f1
Tyll Weiß <info@craft-interactive.de>
2016-07-31 14:52:45 +0200

    TASK: Move to an ES6 class for the <CheckBox/> component to increase the performance of the component.
    Before this commit, the component got re-rendered everytime something on the upper react level has changed, due to the fact that a function for the onChange handler got created everytime. This commit fixes this performance issue, by using a class, which can have static methods with respect to the context.

--- packages/react-ui-components/src/checkBox/checkBox.js
+++ packages/react-ui-components/src/checkBox/checkBox.js
@@ -1,54 +1,64 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import mergeClassNames from 'classnames';
 
-const onChangeHandler = (cb, isChecked) => {
-    if (cb) {
-        cb(isChecked);
+class CheckBox extends Component {
+    static propTypes = {
+        isChecked: PropTypes.bool.isRequired,
+        className: PropTypes.string,
+        onChange: PropTypes.func,
+        theme: PropTypes.shape({
+            'checkbox': PropTypes.string,
+            'checkbox__input': PropTypes.string,
+            'checkbox__inputMirror': PropTypes.string,
+            'checkbox__inputMirror--active': PropTypes.string
+        }).isRequired
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
     }
-};
 
-const CheckBox = props => {
-    const {
-        isChecked,
-        className,
-        onChange,
-        theme,
-        ...rest
-    } = props;
-    const classNames = mergeClassNames({
-        [className]: className && className.length,
-        [theme.checkbox]: true
-    });
-    const mirrorClassNames = mergeClassNames({
-        [theme.checkbox__inputMirror]: true,
-        [theme['checkbox__inputMirror--active']]: isChecked
-    });
+    render() {
+        const {
+            isChecked,
+            className,
+            theme,
+            ...rest
+        } = this.props;
+        const classNames = mergeClassNames({
+            [className]: className && className.length,
+            [theme.checkbox]: true
+        });
+        const mirrorClassNames = mergeClassNames({
+            [theme.checkbox__inputMirror]: true,
+            [theme['checkbox__inputMirror--active']]: isChecked
+        });
 
-    return (
-        <div className={classNames}>
-            <input
-                {...rest}
-                className={theme.checkbox__input}
-                type="checkbox"
-                role="checkbox"
-                checked={isChecked}
-                aria-checked={isChecked}
-                onChange={() => onChangeHandler(onChange, !isChecked)}
-                />
-            <div className={mirrorClassNames}></div>
-        </div>
-    );
-};
-CheckBox.propTypes = {
-    isChecked: PropTypes.bool.isRequired,
-    className: PropTypes.string,
-    onChange: PropTypes.func,
-    theme: PropTypes.shape({
-        'checkbox': PropTypes.string,
-        'checkbox__input': PropTypes.string,
-        'checkbox__inputMirror': PropTypes.string,
-        'checkbox__inputMirror--active': PropTypes.string
-    }).isRequired
-};
+        return (
+            <div className={classNames}>
+                <input
+                    {...rest}
+                    className={theme.checkbox__input}
+                    type="checkbox"
+                    role="checkbox"
+                    checked={isChecked}
+                    aria-checked={isChecked}
+                    onChange={this.handleChange}
+                    />
+                <div className={mirrorClassNames}></div>
+            </div>
+        );
+    }
+
+    handleChange() {
+        const {onChange, isChecked} = this.props;
+
+        if (onChange) {
+            onChange(!isChecked);
+        }
+    }
+}
 
 export default CheckBox;

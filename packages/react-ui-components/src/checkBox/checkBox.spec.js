8c08d27a96d6225dcaba0310bacf07bd87699ea5
Tyll Weiß <info@craft-interactive.de>
2016-07-31 14:54:52 +0200

    TASK: Add full test coverage for the <CheckBox/> component

--- packages/react-ui-components/src/checkBox/checkBox.spec.js
+++ packages/react-ui-components/src/checkBox/checkBox.spec.js
@@ -14,14 +14,19 @@ test('should render a "input" node with the role="button" attribute.', t => {
 
     t.truthy(input.length === 1);
 });
+test('should render the "className" prop if passed.', t => {
+    const input = shallow({
+        className: 'barClassName'
+    });
 
+    t.truthy(input.hasClass('barClassName'));
+});
 test('should throw no errors if no "onChange" prop was passed when clicking on the hidden checkbox.', t => {
     const cb = shallow();
     const fn = () => cb.find('[type="checkbox"]').simulate('change');
 
     t.notThrows(fn);
 });
-
 test('should call the passed "onChange" prop when clicking on the hidden checkbox.', t => {
     const onChange = sinon.spy();
     const cb = shallow({onChange});
@@ -30,14 +35,12 @@ test('should call the passed "onChange" prop when clicking on the hidden checkbo
 
     t.truthy(onChange.callCount === 1);
 });
-
 test('should set truthy aria and checked attribute when passing a truthy "isChecked" prop.', t => {
     const markup = shallow({isChecked: true}).find('[type="checkbox"]').html();
 
     t.truthy(markup.includes('checked="true"'));
     t.truthy(markup.includes('aria-checked="true"'));
 });
-
 test('should set falsy aria and checked attribute when passing a falsy "isChecked" prop.', t => {
     const markup = shallow({isChecked: false}).find('[type="checkbox"]').html();
 

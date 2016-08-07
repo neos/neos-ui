ebba96708061632e06a6ec52cd0d4eb279e5ff11
Tyll Weiß <info@craft-interactive.de>
2016-08-07 15:15:14 +0200

    FEATURE: Increase the performance of the <TextInput/> component

--- packages/react-ui-components/src/textInput/textInput.js
+++ packages/react-ui-components/src/textInput/textInput.js
@@ -1,80 +1,90 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 import mergeClassNames from 'classnames';
 
-const onChangeHandler = (e, cb) => {
-    const value = e.target.value.substr(0, 140);
+class TextInput extends Component {
+    static propTypes = {
+        /**
+         * This prop controls if the TextArea is rendered as invalid or not.
+         */
+        isValid: PropTypes.bool.isRequired,
 
-    if (cb) {
-        cb(value);
+        /**
+         * An optional className to render on the textarea node.
+         */
+        className: PropTypes.string,
+
+        /**
+         * An optional HTML5 placeholder.
+         */
+        placeholder: PropTypes.string,
+
+        /**
+         * The handler which will be called once the user changes the value of the input.
+         */
+        onChange: PropTypes.func,
+
+        /**
+         * The handler which will be called once the user takes focus on the input.
+         */
+        onFocus: PropTypes.func,
+
+        /**
+         * The handler which will be called once the user leaves focus of the input.
+         */
+        onBlur: PropTypes.func,
+
+        /**
+         * An optional css theme to be injected.
+         */
+        theme: PropTypes.shape({
+            'textInput': PropTypes.string,
+            'textInput--invalid': PropTypes.string
+        }).isRequired
+    };
+
+    static defaultProps = {
+        isValid: true
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.handleValueChange = this.handleValueChange.bind(this);
     }
-};
 
-const TextInput = props => {
-    const {
-        placeholder,
-        className,
-        isValid,
-        onChange,
-        theme,
-        ...rest
-    } = props;
-    const classNames = mergeClassNames({
-        [className]: className && className.length,
-        [theme.textInput]: true,
-        [theme['textInput--invalid']]: !isValid
-    });
+    render() {
+        const {
+            placeholder,
+            className,
+            isValid,
+            theme,
+            ...rest
+        } = this.props;
+        const classNames = mergeClassNames({
+            [className]: className && className.length,
+            [theme.textInput]: true,
+            [theme['textInput--invalid']]: !isValid
+        });
 
-    return (
-        <input
-            {...rest}
-            className={classNames}
-            role="textbox"
-            placeholder={placeholder}
-            onChange={e => onChangeHandler(e, onChange)}
-            />
-    );
-};
-TextInput.propTypes = {
-    /**
-     * This prop controls if the TextArea is rendered as invalid or not.
-     */
-    isValid: PropTypes.bool.isRequired,
+        return (
+            <input
+                {...rest}
+                className={classNames}
+                role="textbox"
+                placeholder={placeholder}
+                onChange={this.handleValueChange}
+                />
+        );
+    }
 
-    /**
-     * An optional className to render on the textarea node.
-     */
-    className: PropTypes.string,
+    handleValueChange(e) {
+        const value = e.target.value.substr(0, 140);
+        const {onChange} = this.props;
 
-    /**
-     * An optional HTML5 placeholder.
-     */
-    placeholder: PropTypes.string,
-
-    /**
-     * The handler which will be called once the user changes the value of the input.
-     */
-    onChange: PropTypes.func,
-
-    /**
-     * The handler which will be called once the user takes focus on the input.
-     */
-    onFocus: PropTypes.func,
-
-    /**
-     * The handler which will be called once the user leaves focus of the input.
-     */
-    onBlur: PropTypes.func,
-
-    /**
-     * An optional css theme to be injected.
-     */
-    theme: PropTypes.shape({
-        'textInput': PropTypes.string,
-        'textInput--invalid': PropTypes.string
-    }).isRequired
-};
-TextInput.defaultProps = {
-    isValid: true
-};
+        if (onChange) {
+            onChange(value);
+        }
+    }
+}
 
 export default TextInput;

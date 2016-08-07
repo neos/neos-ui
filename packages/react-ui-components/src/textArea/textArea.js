3afcab0759d7aac0ddd2070ca9d22233e2894ab2
Tyll Weiß <info@craft-interactive.de>
2016-08-07 15:18:07 +0200

    FEATURE: Increase the performance of the <TextArea/> component

--- packages/react-ui-components/src/textArea/textArea.js
+++ packages/react-ui-components/src/textArea/textArea.js
@@ -1,63 +1,81 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 import mergeClassNames from 'classnames';
 import TextareaAutoresize from 'react-textarea-autosize';
 
-const TextArea = props => {
-    const {
-        placeholder,
-        className,
-        isValid,
-        onChange,
-        theme,
-        ...rest
-    } = props;
-    const classNames = mergeClassNames({
-        [className]: className && className.length,
-        [theme.textArea]: true,
-        [theme['textArea--invalid']]: !isValid
-    });
+class TextArea extends Component {
+    static propTypes = {
+        /**
+         * This prop controls if the TextArea is rendered as invalid or not.
+         */
+        isValid: PropTypes.bool.isRequired,
 
-    return (
-        <TextareaAutoresize
-            {...rest}
-            className={classNames}
-            role="textbox"
-            placeholder={placeholder}
-            onChange={e => onChange(e.target.value)}
-            />
-    );
-};
-TextArea.propTypes = {
-    /**
-     * This prop controls if the TextArea is rendered as invalid or not.
-     */
-    isValid: PropTypes.bool.isRequired,
+        /**
+         * An optional className to render on the textarea node.
+         */
+        className: PropTypes.string,
 
-    /**
-     * An optional className to render on the textarea node.
-     */
-    className: PropTypes.string,
+        /**
+         * An optional HTML5 placeholder.
+         */
+        placeholder: PropTypes.string,
 
-    /**
-     * An optional HTML5 placeholder.
-     */
-    placeholder: PropTypes.string,
+        /**
+         * The handler which will be called once the user changes the value of the input.
+         */
+        onChange: PropTypes.func,
 
-    /**
-     * The handler which will be called once the user changes the value of the input.
-     */
-    onChange: PropTypes.func,
+        /**
+         * An optional css theme to be injected.
+         */
+        theme: PropTypes.shape({
+            'textArea': PropTypes.string,
+            'textArea--invalid': PropTypes.string
+        }).isRequired
+    };
 
-    /**
-     * An optional css theme to be injected.
-     */
-    theme: PropTypes.shape({
-        'textArea': PropTypes.string,
-        'textArea--invalid': PropTypes.string
-    }).isRequired
-};
-TextArea.defaultProps = {
-    isValid: true
-};
+    static defaultProps = {
+        isValid: true
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.handleValueChange = this.handleValueChange.bind(this);
+    }
+
+    render() {
+        const {
+            placeholder,
+            className,
+            isValid,
+            theme,
+            ...rest
+        } = this.props;
+        const classNames = mergeClassNames({
+            [className]: className && className.length,
+            [theme.textArea]: true,
+            [theme['textArea--invalid']]: !isValid
+        });
+
+        return (
+            <TextareaAutoresize
+                {...rest}
+                className={classNames}
+                role="textbox"
+                placeholder={placeholder}
+                onChange={this.handleValueChange}
+                />
+        );
+    }
+
+    handleValueChange(e) {
+        const value = e.target.value;
+        const {onChange} = this.props;
+
+        if (onChange) {
+            onChange(value);
+        }
+    }
+}
 
 export default TextArea;

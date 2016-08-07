4002ed086e51e2bc2845710dbe9fd8855f21f51e
Tyll Weiß <info@craft-interactive.de>
2016-08-07 16:04:43 +0200

    !!! TASK: Remove the <Portal/> component in favor of the react-portal package

--- packages/react-ui-components/src/_lib/storyUtils.js
+++ packages/react-ui-components/src/_lib/storyUtils.js
@@ -6,7 +6,7 @@ import React, {PropTypes} from 'react';
  */
 export const StoryWrapper = ({title, children, ...rest}) => (
     <div
-        style={{background: '#000', color: '#FFF', padding: '1rem'}}
+        style={{background: '#000', padding: '1rem'}}
         {...rest}
         >
         <h1 style={{fontFamily: 'sans-serif', margin: '0 0 0.5rem'}}>{title}</h1>

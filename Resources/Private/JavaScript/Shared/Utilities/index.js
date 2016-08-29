4ac143e6dff85dd976955809e01c55d26aab0a58
Tyll Weiß <info@craft-interactive.de>
2016-08-29 10:03:48 +0200

    CLEANUP: Remove the unused fontAwesome util

--- Resources/Private/JavaScript/Shared/Utilities/index.js
+++ Resources/Private/JavaScript/Shared/Utilities/index.js
@@ -1,12 +1,10 @@
 import * as api from './Api.js';
-import * as fontAwesome from './FontAwesome.js';
 import handleActions from './HandleActions.js';
 import * as logger from './Logger.js';
 import loadScript from './LoadScript.js';
 
 export {
     api,
-    fontAwesome,
     handleActions,
     logger,
     loadScript

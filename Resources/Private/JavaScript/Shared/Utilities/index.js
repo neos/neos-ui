834c4327c1841f2afea3af848ae81c1dce6ee1db
Tyll Weiß <info@craft-interactive.de>
2016-08-29 10:03:10 +0200

    CLEANUP: Remove the unused filterDeep util

--- Resources/Private/JavaScript/Shared/Utilities/index.js
+++ Resources/Private/JavaScript/Shared/Utilities/index.js
@@ -1,5 +1,4 @@
 import * as api from './Api.js';
-import filterDeep from './FilterDeep.js';
 import * as fontAwesome from './FontAwesome.js';
 import handleActions from './HandleActions.js';
 import * as logger from './Logger.js';
@@ -7,7 +6,6 @@ import loadScript from './LoadScript.js';
 
 export {
     api,
-    filterDeep,
     fontAwesome,
     handleActions,
     logger,

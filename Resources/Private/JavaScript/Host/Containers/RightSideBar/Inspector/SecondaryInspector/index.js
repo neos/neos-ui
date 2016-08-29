2060582bc5ad54e8250f9ef7591a8afad4872590
Tyll Weiß <info@craft-interactive.de>
2016-08-29 16:34:13 +0200

    TASK: Set correct styling for the asset choose dialog

--- Resources/Private/JavaScript/Host/Containers/RightSideBar/Inspector/SecondaryInspector/index.js
+++ Resources/Private/JavaScript/Host/Containers/RightSideBar/Inspector/SecondaryInspector/index.js
@@ -1,31 +1,58 @@
-import React, {PropTypes} from 'react';
-import style from './style.css';
+import React, {PropTypes, Component} from 'react';
+import shallowCompare from 'react-addons-shallow-compare';
+import {connect} from 'react-redux';
 import Portal from 'react-portal';
+import mergeClassNames from 'classnames';
+import {$transform, $get} from 'plow-js';
 import Icon from '@neos-project/react-ui-components/lib/Icon/';
 import Button from '@neos-project/react-ui-components/lib/Button/';
+import style from './style.css';
 
-const SecondaryInspector = props => {
-    const {onClose, children} = props;
+@connect($transform({
+    isFringeLeft: $get('ui.leftSideBar.isHidden'),
+    isFringeRight: $get('ui.rightSideBar.isHidden'),
+    isFullScreen: $get('ui.fullScreen.isFullScreen')
+}))
+export default class SecondaryInspector extends Component {
+    static propTypes = {
+        isFringeLeft: PropTypes.bool.isRequired,
+        isFringeRight: PropTypes.bool.isRequired,
 
-    return (
-        <Portal isOpened={true}>
-            <div className={style.secondaryInspector}>
-                <Button
-                    style="cleanWithBorder"
-                    className={style.close}
-                    onClick={onClose}
-                    >
-                    <Icon icon="close"/>
-                </Button>
-                {children}
-            </div>
-        </Portal>
-    );
-};
-SecondaryInspector.propTypes = {
-    // Interaction related propTypes.
-    onClose: PropTypes.func.isRequired,
-    children: PropTypes.element.isRequired
-};
+        // Interaction related propTypes.
+        onClose: PropTypes.func.isRequired,
+        children: PropTypes.element.isRequired
+    };
 
-export default SecondaryInspector;
+    shouldComponentUpdate(...args) {
+        return shallowCompare(this, ...args);
+    }
+
+    render() {
+        const {
+            onClose,
+            children,
+            isFringeLeft,
+            isFringeRight
+        } = this.props;
+        const finalClassName = mergeClassNames({
+            [style.secondaryInspector]: true,
+            [style['secondaryInspector--isFringeLeft']]: isFringeLeft,
+            [style['secondaryInspector--isFringeRight']]: isFringeRight
+        });
+
+        return (
+            <Portal isOpened={true}>
+                <div className={finalClassName}>
+                    <Button
+                        style="cleanWithBorder"
+                        className={style.close}
+                        onClick={onClose}
+                        >
+                        <Icon icon="close"/>
+                    </Button>
+                    {children}
+                </div>
+            </Portal>
+        );
+    }
+}

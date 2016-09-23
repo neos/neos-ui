b78911cac9a4cd955bce5bcf30953474fed4be6d
Wilhelm Behncke <behncke@sitegeist.de>
2016-09-23 15:47:58 +0200

    FEATURE: Add history pushstate calls for whenever the document node changes

--- Resources/Private/JavaScript/Host/Sagas/index.js
+++ Resources/Private/JavaScript/Host/Sagas/index.js
@@ -1,3 +1,4 @@
+import {sagas as BrowserSagas} from './Browser/index';
 import {sagas as ChangesSagas} from './Changes/index';
 import {sagas as CRSagas} from './CR/index';
 import {sagas as PublishSagas} from './Publish/index';
@@ -7,6 +8,7 @@ import {sagas as UISagas} from './UI/index';
 import {sagas as ViewSagas} from './View/index';
 
 export default [
+    ...BrowserSagas,
     ...ChangesSagas,
     ...CRSagas,
     ...PublishSagas,

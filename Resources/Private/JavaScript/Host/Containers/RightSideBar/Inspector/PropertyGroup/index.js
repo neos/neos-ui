5927a7af384e8e3032c8c1087ca2f2d6c5bfce12
Wilhelm Behncke <behncke@sitegeist.de>
2016-10-07 15:53:28 +0200

    BUGFIX: Limit width of inspector panel to dimensions of right side bar

--- Resources/Private/JavaScript/Host/Containers/RightSideBar/Inspector/PropertyGroup/index.js
+++ Resources/Private/JavaScript/Host/Containers/RightSideBar/Inspector/PropertyGroup/index.js
@@ -5,7 +5,8 @@ import ToggablePanel from '@neos-project/react-ui-components/lib/ToggablePanel/'
 import {I18n} from 'Host/Containers/index';
 
 import EditorEnvelope from '../EditorEnvelope/index';
-import style from '../../style.css';
+import sidebarStyle from '../../style.css';
+import style from './style.css';
 
 export default class PropertyGroup extends Component {
     static propTypes = {
@@ -15,9 +16,12 @@ export default class PropertyGroup extends Component {
 
     render() {
         const {properties, label} = this.props;
+        const headerTheme = {
+            panel__headline: style.propertyGroupLabel // eslint-disable-line camelcase
+        };
         const propertyGroup = properties => (
-            <ToggablePanel isOpen={true} className={style.rightSideBar__section}>
-                <ToggablePanel.Header>
+            <ToggablePanel isOpen={true} className={sidebarStyle.rightSideBar__section}>
+                <ToggablePanel.Header theme={headerTheme}>
                     <I18n id={label}/>
                 </ToggablePanel.Header>
                 <ToggablePanel.Contents>

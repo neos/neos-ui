195f978bb4804e4453e91b1a5485a8797a3ca1e2
Wilhelm Behncke <behncke@sitegeist.de>
2016-10-19 13:15:11 +0200

    TASK: Prepare ava stack for neos ui package, Make API index spec work with ava

--- packages/neos-ui/src/API/index.spec.js
+++ packages/neos-ui/src/API/index.spec.js
@@ -1,87 +1,95 @@
+import test from 'ava';
 import api, {createPlugin, define} from './index.js';
 
-describe('"api.factory"', () => {
-    describe('"createPlugin" utility', () => {
-        it('should expose a method to create plugins which will add the given identifier to the constructor.', () => {
-            const plugin = createPlugin('myName', () => null);
+test(`
+    "createPlugin" utility should expose a method to create plugins which will
+    add the given identifier to the constructor.`, t => {
+    const plugin = createPlugin('myName', () => null);
 
-            expect(plugin.identifier).to.equal('myName');
-        });
-    });
+    t.is(plugin.identifier, 'myName');
+});
 
-    describe('"define" utility', () => {
-        it('should return a curry function which defines the given identifier/value in the given target object.', () => {
-            const target = {};
-            const targetDefine = define(target);
+test(`
+    "define" utility should return a curry function which defines the given
+    identifier/value in the given target object.`, t => {
+    const target = {};
+    const targetDefine = define(target);
 
-            expect(targetDefine).to.be.an('function');
+    t.is(typeof (targetDefine), 'function');
 
-            targetDefine('myName', 'value');
+    targetDefine('myName', 'value');
 
-            expect(target.myName).to.exist; // eslint-disable-line
-        });
+    t.not(target.myName, undefined);
+});
 
-        it('should throw an error if the given identifier was already registered in the parent.', () => {
-            const target = {};
-            const targetDefine = define(target);
-            const fn = () => targetDefine('myName', 'value');
+test(`
+    "define" utility should throw an error if the given identifier was already
+    registered in the parent.`, t => {
+    const target = {};
+    const targetDefine = define(target);
+    const fn = () => targetDefine('myName', 'value');
 
-            targetDefine('myName', 'value');
+    targetDefine('myName', 'value');
 
-            expect(fn).to.throw();
-        });
-    });
+    t.throws(fn);
+});
 
-    it('should throw an error if no csrfToken was passed.', () => {
-        const fn = () => api({});
+test('should throw an error if no csrfToken was passed.', t => {
+    const fn = () => api({});
 
-        expect(fn).to.throw();
-    });
+    t.throws(fn);
+});
 
-    it('should place itself inside the specified parent object with the specified alias.', () => {
-        const win = {};
+test('should place itself inside the specified parent object with the specified alias.', t => {
+    const win = {};
+    const csrfToken = 'csrfToken';
+    const alias = 'test';
 
-        api(win, 'csrfToken', 'test');
+    api(win, {csrfToken, alias});
 
-        expect(win.test).to.exist; // eslint-disable-line
-    });
+    t.not(win.test, undefined);
+});
 
-    it('should throw an error if a value already exists under the given alias in parent.', () => {
-        const win = {};
-        const fn = () => api(win, 'csrfToken');
+test('should throw an error if a value already exists under the given alias in parent.', t => {
+    const win = {};
+    const csrfToken = 'csrfToken';
+    const fn = () => api(win, 'csrfToken');
 
-        api(win, 'csrfToken');
+    api(win, {csrfToken});
 
-        expect(fn).to.throw();
-    });
+    t.throws(fn);
+});
 
-    it('should have a fallback alias "neos" if none was specified.', () => {
-        const win = {};
+test('should have a fallback alias "neos" if none was specified.', t => {
+    const win = {};
+    const csrfToken = 'csrfToken';
 
-        api(win, 'csrfToken');
+    api(win, {csrfToken});
 
-        expect(win.neos).to.exist; // eslint-disable-line
-    });
+    t.not(win.neos, undefined);
+});
 
-    it('should not be writable.', () => {
-        const win = {};
-        const fn = () => {
-            win.neos = 'test';
-        };
+test('should not be writable.', t => {
+    const win = {};
+    const csrfToken = 'csrfToken';
+    const fn = () => {
+        win.neos = 'test';
+    };
 
-        api(win, 'csrfToken');
+    api(win, {csrfToken});
 
-        const oldApi = win.neos;
+    const oldApi = win.neos;
 
-        expect(fn).to.throw();
-        expect(win.neos).to.equal(oldApi);
-    });
+    t.throws(fn);
+    t.is(win.neos, oldApi);
+});
 
-    it('should expose the use method by default.', () => {
-        const win = {};
+test('should expose the use method by default.', t => {
+    const win = {};
+    const csrfToken = 'csrfToken';
 
-        api(win, 'csrfToken');
+    api(win, {csrfToken});
 
-        expect(win.neos.use).to.be.an('function');
-    });
+    t.not(win.neos.use, undefined);
+    t.is(typeof (win.neos.use), 'function');
 });

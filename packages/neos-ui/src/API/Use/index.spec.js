0da57eb4749c473cc54d79a556e8779a11ea8617
Wilhelm Behncke <behncke@sitegeist.de>
2016-10-19 15:17:50 +0200

    TASK: Turn api plugin mechanism specs into ava tests

--- packages/neos-ui/src/API/Use/index.spec.js
+++ packages/neos-ui/src/API/Use/index.spec.js
@@ -1,38 +1,36 @@
-import chai, {expect} from 'chai';
-import sinon from 'sinon/pkg/sinon.js';
-import sinonChai from 'sinon-chai';
+import test from 'ava';
+import sinon from 'sinon';
 import initializeUse from './index.js';
 
-chai.should();
-chai.use(sinonChai);
+test(`"api.use" should return a function when called.`, t => {
+    t.is(typeof (initializeUse()), 'function');
+});
 
-describe('"api.use"', () => {
-    it('should return a function when called', () => {
-        expect(initializeUse()).to.be.an('function');
-    });
+test(`
+    "api.use" should call the "addLibrary" function when initializing a plugin
+    factory called.`, t => {
+    const apiTarget = {};
+    const addLibrary = sinon.spy();
+    const use = initializeUse(addLibrary, apiTarget);
+    const pluginFactory = () => {};
+    pluginFactory.identifier = 'myPlugin';
 
-    it('should call the "addLibrary" function when initializing a plugin factory called', () => {
-        const apiTarget = {};
-        const addLibrary = sinon.spy();
-        const use = initializeUse(addLibrary, apiTarget);
-        const pluginFactory = () => {};
-        pluginFactory.identifier = 'myPlugin';
+    use(pluginFactory);
 
-        use(pluginFactory);
+    t.true(addLibrary.calledOnce);
+    t.true(addLibrary.calledWith('myPlugin', pluginFactory()));
+});
 
-        expect(addLibrary).to.have.callCount(1);
-        expect(addLibrary).to.have.been.calledWith('myPlugin', pluginFactory());
-    });
+test(`
+    "api.use" should call the "pluginFactory" when initializing a plugin
+    factory called.`, t => {
+    const apiTarget = {};
+    const addLibrary = () => {};
+    const use = initializeUse(addLibrary, apiTarget);
+    const pluginFactory = sinon.spy();
+    pluginFactory.identifier = 'myPlugin';
 
-    it('should call the "pluginFactory" when initializing a plugin factory called', () => {
-        const apiTarget = {};
-        const addLibrary = () => {};
-        const use = initializeUse(addLibrary, apiTarget);
-        const pluginFactory = sinon.spy();
-        pluginFactory.identifier = 'myPlugin';
+    use(pluginFactory);
 
-        use(pluginFactory);
-
-        expect(pluginFactory).to.have.callCount(1);
-    });
+    t.true(pluginFactory.calledOnce);
 });

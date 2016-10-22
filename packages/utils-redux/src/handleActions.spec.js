9ec25f4cc72f63446ff6d16996803780825592a4
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-10-22 16:02:20 +0200

    TASK: Ensure tests are green for @neos-project/utils-redux

--- packages/utils-redux/src/handleActions.spec.js
+++ packages/utils-redux/src/handleActions.spec.js
@@ -1,44 +1,47 @@
-import chai, {expect} from 'chai';
-import sinon from 'sinon/pkg/sinon.js';
-import sinonChai from 'sinon-chai';
-import handleActions from './HandleActions.js';
+import test from 'ava';
+import sinon from 'sinon';
 
-chai.should();
-chai.use(sinonChai);
+import handleActions from './handleActions';
 
-describe('"host.utilities.handleActions" ', () => {
-    it('should return a curry function when called.', () => {
-        expect(handleActions()).to.be.an('function');
-    });
+test(`should export a function`, t => {
+    t.is(typeof (handleActions), 'function');
+});
 
-    it('should check if the passed handlers contain the given action object name and return the state if none was found.', () => {
-        const handler = handleActions();
-        const state = {};
-        const action = {
-            type: 'test'
-        };
+test(`should return a curry function when called`, t => {
+    t.is(typeof (handleActions()), 'function');
+});
 
-        expect(handler(state, action)).to.equal(state);
-    });
+test(`
+    should check if the passed handlers contain the given action object name and return the
+    state if none was found.`, t => {
+    const handler = handleActions();
+    const state = {};
+    const action = {
+        type: 'test'
+    };
 
-    it('should call the associated handler of the action type with the payload and the returned curry function with the state.', () => {
-        const actionReducer = sinon.spy();
-        const handlers = {
-            test: sinon.spy(() => actionReducer)
-        };
-        const handler = handleActions(handlers);
-        const state = {};
-        const action = {
-            type: 'test',
-            payload: {}
-        };
+    t.is(handler(state, action), state);
+});
 
-        handler(state, action);
+test(`
+    should call the associated handler of the action type with the payload and the returned curry
+    function with the state.`, t => {
+    const actionReducer = sinon.spy();
+    const handlers = {
+        test: sinon.spy(() => actionReducer)
+    };
+    const handler = handleActions(handlers);
+    const state = {};
+    const action = {
+        type: 'test',
+        payload: {}
+    };
 
-        expect(handlers.test).to.have.callCount(1);
-        expect(handlers.test).to.have.been.calledWith(action.payload);
+    handler(state, action);
 
-        expect(actionReducer).to.have.callCount(1);
-        expect(actionReducer).to.have.been.calledWith(state);
-    });
+    t.true(handlers.test.calledOnce);
+    t.true(handlers.test.calledWith(action.payload));
+
+    t.true(actionReducer.calledOnce);
+    t.true(actionReducer.calledWith(state));
 });

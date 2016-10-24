1de5968e8b0ce5d8026fa9f8f632ed792b6c9959
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2016-10-24 21:05:49 +0200

    FEATURE: implement reloadIfChanged support for inspector

--- Classes/Neos/Neos/Ui/Domain/Model/AbstractChange.php
+++ Classes/Neos/Neos/Ui/Domain/Model/AbstractChange.php
@@ -1,6 +1,7 @@
 <?php
 namespace Neos\Neos\Ui\Domain\Model;
 
+use Neos\Neos\Ui\Domain\Model\Feedback\Operations\ReloadDocument;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 use Neos\Neos\Ui\TYPO3CR\Service\NodeService;
@@ -54,5 +55,23 @@ abstract class AbstractChange implements ChangeInterface
         );
 
         $this->feedbackCollection->add($updateWorkspaceInfo);
+    }
+
+    /**
+     * Inform the client to reload the currently-displayed document, because the rendering has changed.
+     *
+     * This method will be triggered if [nodeType].properties.[propertyName].ui.reloadIfChanged is TRUE.
+     *
+     * @return void
+     */
+    protected function reloadDocument()
+    {
+        $nodeService = new NodeService();
+        $reloadDocument = new ReloadDocument();
+        $reloadDocument->setDocument(
+            $nodeService->getClosestDocument($this->getSubject())
+        );
+
+        $this->feedbackCollection->add($reloadDocument);
     }
 }

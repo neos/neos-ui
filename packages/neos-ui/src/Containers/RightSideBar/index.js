a241395f1db68d2be6a413e9aafa0f62da542792
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2016-11-23 13:37:21 +0100

    TASK: use selector functions instead of $get directly

--- packages/neos-ui/src/Containers/RightSideBar/index.js
+++ packages/neos-ui/src/Containers/RightSideBar/index.js
@@ -5,14 +5,14 @@ import {connect} from 'react-redux';
 import {$transform, $get} from 'plow-js';
 import IconButton from '@neos-project/react-ui-components/lib/IconButton/';
 import SideBar from '@neos-project/react-ui-components/lib/SideBar/';
-import {actions} from '@neos-project/neos-ui-redux-store';
+import {actions, selectors} from '@neos-project/neos-ui-redux-store';
 
 import style from './style.css';
 
 import Inspector from './Inspector/index';
 
 @connect($transform({
-    isHidden: $get('ui.rightSideBar.isHidden'),
+    isHidden: selectors.UI.RightSideBar.isHidden,
     isFullScreen: $get('ui.fullScreen.isFullScreen')
 }), {
     toggleSidebar: actions.UI.RightSideBar.toggle

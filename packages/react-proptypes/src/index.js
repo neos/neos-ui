9b685bdf5d2ff39e602c20d5333981d0f884133a
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-11-24 15:43:25 +0100

    TASK: Add renderedNodeDomAddress prop type

--- packages/react-proptypes/src/index.js
+++ packages/react-proptypes/src/index.js
@@ -3,11 +3,13 @@ import documentNode from './documentNode';
 import node from './node';
 import nodeType from './nodeType';
 import propertyDefinition from './propertyDefinition';
+import renderedNodeDomAddress from './renderedNodeDomAddress';
 
 export {
     alohaConfiguration,
     documentNode,
     node,
     nodeType,
-    propertyDefinition
+    propertyDefinition,
+    renderedNodeDomAddress
 };

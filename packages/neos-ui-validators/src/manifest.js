14f121795501778d8a95b17360df47fc0a1df9a5
Wilhelm Behncke <behncke@sitegeist.de>
2016-12-07 09:21:43 +0100

    TASK: Rename validator references according to namespace change

--- packages/neos-ui-validators/src/manifest.js
+++ packages/neos-ui-validators/src/manifest.js
@@ -6,6 +6,6 @@ import manifest from '@neos-project/neos-ui-extensibility';
 manifest('validators', {}, globalRegistry => {
     const validatorRegistry = globalRegistry.get('validators');
 
-    validatorRegistry.add('TYPO3.Neos/Validation/NotEmptyValidator', NotEmpty);
-    validatorRegistry.add('TYPO3.Neos/Validation/StringLengthValidator', StringLength);
+    validatorRegistry.add('Neos.Neos/Validation/NotEmptyValidator', NotEmpty);
+    validatorRegistry.add('Neos.Neos/Validation/StringLengthValidator', StringLength);
 });

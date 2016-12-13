d6e3564fcf00854e383cba87e1db9ff3f0013fc5
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-12-13 09:49:42 +0100

    TASK: Use correct URIs for Media Browser

--- packages/neos-ui-editors/src/Image/Components/Secondary/MediaSelectionScreen/index.js
+++ packages/neos-ui-editors/src/Image/Components/Secondary/MediaSelectionScreen/index.js
@@ -12,7 +12,7 @@ const MediaSelectionScreen = props => {
 
     // TODO: hard-coded url
     return (
-        <iframe src="/neos/content/images.html" className={style.iframe}/>
+        <iframe src="/neos/module/media/browser/image.html" className={style.iframe}/>
     );
 };
 MediaSelectionScreen.propTypes = {

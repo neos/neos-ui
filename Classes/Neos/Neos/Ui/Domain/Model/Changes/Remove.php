52f9ea7ca894ae97e8063d5b30440b66acb67ad7
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-12-14 15:21:19 +0100

    TASK: Create Remove change

---
+++ Classes/Neos/Neos/Ui/Domain/Model/Changes/Remove.php
@@ -0,0 +1,77 @@
+<?php
+namespace Neos\Neos\Ui\Domain\Model\Changes;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Neos\Ui\Domain\Model\AbstractChange;
+use Neos\Neos\Ui\Domain\Model\ChangeInterface;
+use Neos\Neos\Ui\Domain\Model\Feedback\Operations\RemoveNode;
+
+/**
+ * Removes a node
+ */
+class Remove extends AbstractChange
+{
+    /**
+     * Checks whether this change can be merged with a subsequent change
+     *
+     * @param  ChangeInterface $subsequentChange
+     * @return boolean
+     */
+    public function canMerge(ChangeInterface $subsequentChange)
+    {
+        if ($subsequentChange->getSubject() !== $this->getSubject()) {
+            return false;
+        }
+
+        if ($subsequentChange->getPropertyName() !== $this->getPropertyName()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Merges this change with a subsequent change
+     *
+     * @param  ChangeInterface $subsequentChange
+     * @return ChangeInterface|null
+     */
+    public function merge(ChangeInterface $subsequentChange)
+    {
+        if ($this->canMerge($subsequentChange)) {
+            return $this;
+        }
+
+        return null;
+    }
+
+    /**
+     * Checks whether this change can be applied to the subject
+     *
+     * @return boolean
+     */
+    public function canApply()
+    {
+        return true;
+    }
+
+    /**
+     * Applies this change
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        if ($this->canApply()) {
+            $node = $this->getSubject();
+            $node->remove();
+
+            $this->updateWorkspaceInfo();
+
+            $removeNode = new RemoveNode();
+            $removeNode->setNode($node);
+
+            $this->feedbackCollection->add($removeNode);
+        }
+    }
+}

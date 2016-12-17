561c2ef37d723e88f3effc6ff2ca9ba92928b5f6
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-12-17 18:32:39 +0100

    TASK: Implement workflow-saga for copy&paste

--- packages/neos-ui/src/Sagas/CR/NodeOperations/index.js
+++ packages/neos-ui/src/Sagas/CR/NodeOperations/index.js
@@ -1,8 +1,8 @@
-import {takeLatest} from 'redux-saga';
-import {take, put, race, select} from 'redux-saga/effects';
+import {takeLatest, takeEvery} from 'redux-saga';
+import {take, put, race, select, call} from 'redux-saga/effects';
 import {$get} from 'plow-js';
 
-import {actions, actionTypes} from '@neos-project/neos-ui-redux-store';
+import {selectors, actions, actionTypes} from '@neos-project/neos-ui-redux-store';
 
 function * removeNodeIfConfirmed() {
     yield * takeLatest(actionTypes.CR.Nodes.COMMENCE_REMOVAL, function * waitForConfirmation() {
@@ -11,7 +11,7 @@ function * removeNodeIfConfirmed() {
             take(actionTypes.CR.Nodes.REMOVAL_ABORTED),
             take(actionTypes.CR.Nodes.REMOVAL_CONFIRMED)
         ]);
-        const nextAction = Object.keys(waitForNextAction).map(k => waitForNextAction[k])[0];
+        const nextAction = Object.values(waitForNextAction)[0];
 
         if (nextAction.type === actionTypes.CR.Nodes.REMOVAL_ABORTED) {
             return;
@@ -28,6 +28,94 @@ function * removeNodeIfConfirmed() {
     });
 }
 
+function * determineInsertMode(subjectContextPath, referenceContextPath, canBePastedAlongside, canBePastedInto) {
+    if (canBePastedInto && !canBePastedAlongside) {
+        return 'into';
+    }
+
+    yield put(actions.UI.InsertionModeModal.open(
+        subjectContextPath,
+        referenceContextPath,
+        canBePastedAlongside,
+        canBePastedInto
+    ));
+    const waitForNextAction = yield race([
+        take(actionTypes.UI.InsertionModeModal.CANCEL),
+        take(actionTypes.UI.InsertionModeModal.APPLY)
+    ]);
+    const nextAction = Object.values(waitForNextAction)[0];
+
+    if (nextAction.type === actionTypes.UI.InsertionModeModal.CANCEL) {
+        return;
+    }
+
+    return nextAction.payload;
+}
+
+function * copyAndPasteNode({globalRegistry}) {
+    const nodeTypesRegistry = globalRegistry.get('@neos-project/neos-ui-contentrepository');
+    const {
+        canBePastedAlongsideSelector,
+        canBePastedIntoSelector
+    } = selectors.CR.Nodes;
+    const calculateChangeTypeFromMode = mode => {
+        switch (mode) {
+            case 'prepend':
+                return 'Neos.Neos.Ui:CopyBefore';
+
+            case 'append':
+                return 'Neos.Neos.Ui:CopyAfter';
+
+            default:
+                return 'Neos.Neos.Ui:CopyInto';
+        }
+    };
+
+    yield * takeEvery(actionTypes.CR.Nodes.COPY, function * waitForPaste() {
+        const state = yield select();
+        const waitForNextAction = yield race([
+            take(actionTypes.CR.Nodes.COPY),
+            take(actionTypes.CR.Nodes.PASTE)
+        ]);
+        const nextAction = Object.values(waitForNextAction)[0];
+
+        if (nextAction.type === actionTypes.CR.Nodes.COPY) {
+            return;
+        }
+
+        if (nextAction.type === actionTypes.CR.Nodes.PASTE) {
+            const nodeToBePasted = $get('cr.nodes.clipboard', state);
+            const referenceNodeContextPath = nextAction.payload;
+            const canBePastedArguments = [nodeToBePasted, referenceNodeContextPath, nodeTypesRegistry];
+            const canBePastedAlongside = canBePastedAlongsideSelector(state)(...canBePastedArguments);
+            const canBePastedInto = canBePastedIntoSelector(state)(...canBePastedArguments);
+
+            const mode = yield call(
+                determineInsertMode,
+                nodeToBePasted,
+                referenceNodeContextPath,
+                canBePastedAlongside,
+                canBePastedInto
+            );
+
+            if (mode) {
+                yield put(actions.Changes.persistChange({
+                    type: calculateChangeTypeFromMode(mode),
+                    subject: nodeToBePasted,
+                    reference: referenceNodeContextPath,
+                    payload: {}
+                }));
+            }
+
+            //
+            // Keep the loop runnning
+            //
+            yield put(actions.CR.Nodes.copy(nodeToBePasted));
+        }
+    });
+}
+
 export const sagas = [
-    removeNodeIfConfirmed
+    removeNodeIfConfirmed,
+    copyAndPasteNode
 ];

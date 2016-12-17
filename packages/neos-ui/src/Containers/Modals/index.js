f32142cdbb6590db34a2b6fd47c999882744c74d
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-12-17 18:31:35 +0100

    TASk: Add InsertionModeModal to Modal container index

--- packages/neos-ui/src/Containers/Modals/index.js
+++ packages/neos-ui/src/Containers/Modals/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddNodeModal from './AddNode/index';
 import DeleteNodeModal from './DeleteNode/index';
+import InsertionModeModal from './InsertionMode/index';
 
 const Modals = () => (
     <div>
         <AddNodeModal/>
         <DeleteNodeModal/>
+        <InsertionModeModal/>
     </div>
 );
 

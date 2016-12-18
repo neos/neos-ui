0085c4caaa9138148adfc5376cd31c71363c5ad4
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2016-12-18 18:17:24 +0100

    TASK: Connect inline hide button with store

--- packages/neos-ui/src/Containers/ContentCanvas/InlineUI/NodeToolbar/Buttons/HideSelectedNode/index.js
+++ packages/neos-ui/src/Containers/ContentCanvas/InlineUI/NodeToolbar/Buttons/HideSelectedNode/index.js
@@ -1,40 +1,57 @@
 import React, {PureComponent, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {$transform, $get} from 'plow-js';
 import IconButton from '@neos-project/react-ui-components/lib/IconButton/';
 
+import {selectors, actions} from '@neos-project/neos-ui-redux-store';
+
+@connect($transform({
+    node: selectors.CR.Nodes.focusedSelector
+}), {
+    hideNode: actions.CR.Nodes.hide,
+    showNode: actions.CR.Nodes.show
+})
 export default class HideSelectedNode extends PureComponent {
     static propTypes = {
-        isDisabled: PropTypes.bool,
-        className: PropTypes.string
-    };
-
-    static defaultProps = {
-        isDisabled: true
+        node: PropTypes.object,
+        className: PropTypes.string,
+        hideNode: PropTypes.func.isRequired,
+        showNode: PropTypes.func.isRequired
     };
 
     constructor(props) {
         super(props);
 
-        this.handleHideSelectedNodeClick = this.hideSelectedNode.bind(this);
+        this.handleHideNode = this.handleHideNode.bind(this);
+        this.handleShowNode = this.handleShowNode.bind(this);
     }
 
     render() {
-        const {
-            isDisabled,
-            className
-        } = this.props;
+        const {className, node} = this.props;
+        const isDisabled = !node || $get('isAutoCreated', node);
+        const isHidden = $get('properties._hidden', node);
 
         return (
             <IconButton
                 className={className}
+                isActive={isHidden}
                 isDisabled={isDisabled}
-                onClick={this.handleHideSelectedNodeClick}
+                onClick={isHidden ? this.handleShowNode : this.handleHideNode}
                 icon="eye-slash"
                 hoverStyle="clean"
                 />
         );
     }
 
-    hideSelectedNode() {
-        console.log('hide selected node');
+    handleHideNode() {
+        const {node, hideNode} = this.props;
+
+        hideNode($get('contextPath', node));
+    }
+
+    handleShowNode() {
+        const {node, showNode} = this.props;
+
+        showNode($get('contextPath', node));
     }
 }

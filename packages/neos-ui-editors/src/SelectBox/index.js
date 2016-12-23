cf0d710dcb11c5abbc6eea6fe809a9519ad219c2
Dmitri Pisarev <dimaip@gmail.com>
2016-12-23 14:10:59 +0300

    TASK: improve SelectBoxEditor
    - Translate labels
    - Filter out empty values

--- packages/neos-ui-editors/src/SelectBox/index.js
+++ packages/neos-ui-editors/src/SelectBox/index.js
@@ -1,4 +1,5 @@
 import React, {PureComponent, PropTypes} from 'react';
+import I18n from '@neos-project/neos-ui-i18n';
 import SelectBox from '@neos-project/react-ui-components/lib/SelectBox/';
 
 class SelectBoxEditor extends PureComponent {
@@ -17,11 +18,13 @@ class SelectBoxEditor extends PureComponent {
     render() {
         const {commit, value} = this.props;
         const options = Object.keys(this.props.options.values)
-            .map(k => Object.assign(
+            // Filter out items without a label
+            .map(k => this.props.options.values[k].label && Object.assign(
                 {value: k},
-                this.props.options.values[k]
+                this.props.options.values[k],
+                {label: <I18n id={this.props.options.values[k].label}/>}
             )
-        );
+        ).filter(k => k);
         const onDelete = value ? this.handleDelete : null;
 
         return <SelectBox options={options} value={value} onSelect={commit} onDelete={onDelete}/>;

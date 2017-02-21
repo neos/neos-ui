3c2eae1859789b3887027a42dfe80e83cba7af24
Max Strübing <mxstrbng@gmail.com>
2017-02-21 11:01:42 +0100

    TASK: Add textValidator

--- packages/neos-ui-validators/src/manifest.js
+++ packages/neos-ui-validators/src/manifest.js
@@ -1,6 +1,7 @@
 import NotEmpty from './NotEmpty/index';
 import NumberRange from './NumberRange/index';
 import StringLength from './StringLength/index';
+import Text from './Text/index';
 import RegularExpression from './RegularExpression/index';
 
 import manifest from '@neos-project/neos-ui-extensibility';
@@ -11,5 +12,6 @@ manifest('validators', {}, globalRegistry => {
     validatorRegistry.add('Neos.Neos/Validation/NotEmptyValidator', NotEmpty);
     validatorRegistry.add('Neos.Neos/Validation/NumberRangeValidator', NumberRange);
     validatorRegistry.add('Neos.Neos/Validation/StringLengthValidator', StringLength);
+    validatorRegistry.add('Neos.Neos/Validation/TextValidator', Text);
     validatorRegistry.add('Neos.Neos/Validation/RegularExpressionValidator', RegularExpression);
 });

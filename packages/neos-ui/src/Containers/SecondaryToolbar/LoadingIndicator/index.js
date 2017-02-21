ab6f9cb048fedab3541b74148db6e6a3710b5dc0
Max Strübing <mxstrbng@gmail.com>
2017-02-21 16:51:31 +0100

    TASK: Add plain loading indicator

---
+++ packages/neos-ui/src/Containers/SecondaryToolbar/LoadingIndicator/index.js
@@ -0,0 +1,15 @@
+import React, {PureComponent} from 'react';
+import style from './style.css';
+
+export default class DimensionSwitcher extends PureComponent {
+    render() {
+        return (
+                <div className={style.loadingIndicator__container}>
+                    <div className={style.loadingIndicator}>
+                        <div className={style.loadingIndicator__bar}>
+                        </div>
+                    </div>
+                </div>
+        );
+    }
+}

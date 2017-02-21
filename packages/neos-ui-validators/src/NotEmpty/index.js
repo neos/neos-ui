47122589eb030484ff0ec3a13661fadd8229f438
Max Strübing <mxstrbng@gmail.com>
2017-02-21 12:07:33 +0100

    TASK: Adjust comments

--- packages/neos-ui-validators/src/NotEmpty/index.js
+++ packages/neos-ui-validators/src/NotEmpty/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import I18n from '@neos-project/neos-ui-i18n';
 
+/**
+ * Checks if a given value is not empty
+ */
 const NotEmpty = value => {
     if (value === null || value === '' || value === []) {
         return <I18n id='content.inspector.validators.notEmptyValidator.isEmpty' fallback='The value can't be empty'/>;
--- packages/neos-ui-validators/src/RegularExpression/index.js
+++ packages/neos-ui-validators/src/RegularExpression/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import I18n from '@neos-project/neos-ui-i18n';
 
+/**
+ * Checks if a given value matches a given regular expression
+ */
 const RegularExpression = (value, validatorOptions) => {
     const match = validatorOptions.regularExpression.match(new RegExp('^/(.*?)/([gimy]*)$'));
     const regularExpression = new RegExp(match[1], match[2]);

e99db048f0488b9e2503336d24c64eaa81278e7e
Max Strübing <mxstrbng@gmail.com>
2017-02-21 12:04:02 +0100

    TASK: Adjust comment

--- packages/neos-ui-validators/src/String/index.js
+++ packages/neos-ui-validators/src/String/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import I18n from '@neos-project/neos-ui-i18n';
 
 /**
- * Checks if the given value is string
+ * Checks if the given value is a string
  */
 const String = value => {
     if (typeof value !== 'string') {

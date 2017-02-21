bb71c0bb9a1fcbf46a881c5c6a4e1d3e22fe7b2d
Max Strübing <mxstrbng@gmail.com>
2017-02-21 10:33:28 +0100

    TASK: Add integer validator

---
+++ packages/neos-ui-validators/src/Integer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import I18n from '@neos-project/neos-ui-i18n';
+
+/**
+ * Checks if the given value is a valid Integer number (or can be cast to a number
+ * if an object is given)
+ */
+const Integer = value => {
+    const number = parseInt(value, 10);
+
+    if (!Number.isSafeInteger(number)) {
+        return <I18n id='content.inspector.validators.integerValidator.aValidIntegerNumberIsExpected'/>;
+    }
+    return null;
+};
+
+export default Integer;

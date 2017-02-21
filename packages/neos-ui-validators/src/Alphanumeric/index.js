b9dae60f36e2d58109ad9803486e431530471b50
Max Strübing <mxstrbng@gmail.com>
2017-02-21 09:52:56 +0100

    TASK: Add naive alphanumValidator implementation
    TODO: UTF8-character support

---
+++ packages/neos-ui-validators/src/Alphanumeric/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import I18n from '@neos-project/neos-ui-i18n';
+
+const Alphanumeric = value => {
+    const regularExpression = /^[\w\d]*$/u;
+
+    if (value.length === 0 || value.match(regularExpression) !== null) {
+        return null;
+    } else {
+        const pattern = regularExpression.toString();
+        return <I18n id="content.inspector.validators.alphanumericValidator"/>;
+    }
+};
+
+export default Alphanumeric;
--- packages/neos-ui-validators/src/manifest.js
+++ packages/neos-ui-validators/src/manifest.js
@@ -1,15 +1,17 @@
+import Alphanumeric from './Alphanumeric/index';
 import NotEmpty from './NotEmpty/index';
 import NumberRange from './NumberRange/index';
-import StringLength from './StringLength/index';
 import RegularExpression from './RegularExpression/index';
+import StringLength from './StringLength/index';
 
 import manifest from '@neos-project/neos-ui-extensibility';
 
 manifest('validators', {}, globalRegistry => {
     const validatorRegistry = globalRegistry.get('validators');
 
+    validatorRegistry.add('Neos.Neos/Validation/AlphanumericValidator', Alphanumeric);
     validatorRegistry.add('Neos.Neos/Validation/NotEmptyValidator', NotEmpty);
     validatorRegistry.add('Neos.Neos/Validation/NumberRangeValidator', NumberRange);
-    validatorRegistry.add('Neos.Neos/Validation/StringLengthValidator', StringLength);
     validatorRegistry.add('Neos.Neos/Validation/RegularExpressionValidator', RegularExpression);
+    validatorRegistry.add('Neos.Neos/Validation/StringLengthValidator', StringLength);
 });

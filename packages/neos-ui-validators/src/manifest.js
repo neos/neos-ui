af3310961dfdbf9f0553914324caaf1e5b998a99
Max Strübing <mxstrbng@gmail.com>
2017-02-21 14:37:25 +0100

    TASK: Add dateTimeValidator

--- packages/neos-ui-validators/src/manifest.js
+++ packages/neos-ui-validators/src/manifest.js
@@ -1,3 +1,4 @@
+import DateTime from './DateTime/index';
 import NotEmpty from './NotEmpty/index';
 import NumberRange from './NumberRange/index';
 import StringLength from './StringLength/index';
@@ -8,6 +9,7 @@ import manifest from '@neos-project/neos-ui-extensibility';
 manifest('validators', {}, globalRegistry => {
     const validatorRegistry = globalRegistry.get('validators');
 
+    validatorRegistry.add('Neos.Neos/Validation/DateTimeValidator', DateTime);
     validatorRegistry.add('Neos.Neos/Validation/NotEmptyValidator', NotEmpty);
     validatorRegistry.add('Neos.Neos/Validation/NumberRangeValidator', NumberRange);
     validatorRegistry.add('Neos.Neos/Validation/StringLengthValidator', StringLength);

3bfba25fb6577efebf2b7a35b9a51ac87ebbf967
Max Strübing <mxstrbng@gmail.com>
2017-02-22 21:27:25 +0100

    TASK: Add integer tests

---
+++ packages/neos-ui-validators/src/Integer/index.spec.js
@@ -0,0 +1,39 @@
+import test from 'ava';
+
+import integerValidator from './index';
+
+test('1 should be a valid integer', t => {
+    t.is(integerValidator(1), null);
+});
+
+test('"1" should be a valid integer', t => {
+    t.is(integerValidator('1'), null);
+});
+
+test('-1 should be a valid integer', t => {
+    t.is(integerValidator(-1), null);
+});
+
+test('"-1" should be a valid integer', t => {
+    t.is(integerValidator('-1'), null);
+});
+
+test('Number.MAX_SAFE_INTEGER should be a valid integer', t => {
+    t.is(integerValidator(Number.MAX_SAFE_INTEGER), null);
+});
+
+test('Number.MAX_SAFE_INTEGER + 1 should not be a valid integer', t => {
+    t.not(integerValidator(Number.MAX_SAFE_INTEGER + 1, null));
+});
+
+test('Number.MIN_SAFE_INTEGER should be a valid integer', t => {
+    t.is(integerValidator(Number.MIN_SAFE_INTEGER), null);
+});
+
+test('Number.MIN_SAFE_INTEGER - 1 should not be a valid integer', t => {
+    t.not(integerValidator(Number.MIN_SAFE_INTEGER - 1, null));
+});
+
+test('a should not be a valid integer', t => {
+    t.not(integerValidator('a'), null);
+});
--- packages/neos-ui-validators/src/NotEmpty/index.js
+++ packages/neos-ui-validators/src/NotEmpty/index.js
@@ -5,7 +5,7 @@ import I18n from '@neos-project/neos-ui-i18n';
  * Checks if a given value is not empty
  */
 const NotEmpty = value => {
-    if (value === null || value === '' || value === []) {
+    if (value === null || value === '' || value.length === 0) {
         return <I18n id="content.inspector.validators.notEmptyValidator.isEmpty" fallback="The value can not be empty"/>;
     }
     return null;

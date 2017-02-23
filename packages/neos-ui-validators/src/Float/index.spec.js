6f1bc84b444e8b164eed2072d63f95a572df4029
Max Strübing <mxstrbng@gmail.com>
2017-02-23 10:33:30 +0100

    TASK: Add floatValidator test

---
+++ packages/neos-ui-validators/src/Float/index.spec.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+
+import floatValidator from './index';
+
+test('"1.2" should be a valid float', t => {
+    t.is(floatValidator("1.2"), null);
+});
+
+test('1.2 should be a valid float', t => {
+    t.is(floatValidator(1.2), null);
+});
+
+test('".1" should be a valid float', t => {
+    t.is(floatValidator(".1"), null);
+});
+
+test('.1 should be a valid float', t => {
+    t.is(floatValidator(.1), null);
+});
+
+test('"1." should not be a valid float', t => {
+    t.not(floatValidator("1."), null);
+});
+
+test('1. should not be a valid float', t => {
+    t.not(floatValidator(1.), null);
+});
+
+test('"1" should not be a valid float', t => {
+    t.not(floatValidator("1"), null);
+});
+
+test('1 should not be a valid float', t => {
+    t.not(floatValidator(1), null);
+});

f2eeca52b8a86abcf5ebedee2e21bbf1afc9ef5e
Max Strübing <mxstrbng@gmail.com>
2017-02-23 09:28:20 +0100

    TASK: Add alphanum test

---
+++ packages/neos-ui-validators/src/Alphanumeric/index.spec.js
@@ -0,0 +1,23 @@
+import test from 'ava';
+
+import alphanumericValidator from './index';
+
+test('"a1b2" should be alphanumeric', t => {
+    t.is(alphanumericValidator('a1b2'), null);
+});
+
+test('"ÄÖÜß" should be alphanumeric', t => {
+    t.is(alphanumericValidator('ÄÖÜß'), null);
+});
+
+test('"!ÄÖÜß" should not be alphanumeric', t => {
+    t.not(alphanumericValidator('!ÄÖÜß'), null);
+});
+
+test('"--" should not be alphanumeric', t => {
+    t.not(alphanumericValidator('--'), null);
+});
+
+test('"ab c" should not be alphanumeric', t => {
+    t.not(alphanumericValidator('ab c'), null);
+});

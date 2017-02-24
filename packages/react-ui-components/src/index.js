797a2ec29fa15f9351f2dff8841307a3a5cafb73
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2017-02-24 10:42:27 +0100

    TASK: add public component API to react-ui-components

---
+++ packages/react-ui-components/src/index.js
@@ -0,0 +1,75 @@
+import Badge from './Badge/index';
+import Bar from './Bar/index';
+import Button from './Button/index';
+import ButtonGroup from './ButtonGroup/index';
+import CheckBox from './CheckBox/index';
+import DateInput from './DateInput/index';
+import Dialog from './Dialog/index';
+import DropDown from './DropDown/index';
+import Frame from './Frame/index';
+import Grid from './Grid/index';
+import Headline from './Headline/index';
+import Icon from './Icon/index';
+import IconButton from './IconButton/index';
+import IconButtonDropDown from './IconButtonDropDown/index';
+import Label from './Label/index';
+import SelectBox from './SelectBox/index';
+import SideBar from './SideBar/index';
+import Tabs from './Tabs/index';
+import TextArea from './TextArea/index';
+import TextInput from './TextInput/index';
+import ToggablePanel from './ToggablePanel/index';
+import Tooltip from './Tooltip/index';
+import Tree from './Tree/index';
+
+export {
+    Badge
+    Bar,
+    Button,
+    ButtonGroup,
+    CheckBox,
+    DateInput,
+    Dialog,
+    DropDown,
+    Frame,
+    Grid,
+    Headline,
+    Icon,
+    IconButton,
+    IconButtonDropDown,
+    Label,
+    SelectBox,
+    SideBar,
+    Tabs,
+    TextArea,
+    TextInput,
+    ToggablePanel,
+    Tooltip,
+    Tree
+};
+
+export default {
+    Badge,
+    Bar,
+    Button,
+    ButtonGroup,
+    CheckBox,
+    DateInput,
+    Dialog,
+    DropDown,
+    Frame,
+    Grid,
+    Headline,
+    Icon,
+    IconButton,
+    IconButtonDropDown,
+    Label,
+    SelectBox,
+    SideBar,
+    Tabs,
+    TextArea,
+    TextInput,
+    ToggablePanel,
+    Tooltip,
+    Tree
+};

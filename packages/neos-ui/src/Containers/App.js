52edfc9499a334fceceec1d500d4aba6dbbf96b6
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2017-02-24 20:45:38 +0100

    FEATURE: make full application themeable and replaceable

---
+++ packages/neos-ui/src/Containers/App.js
@@ -0,0 +1,39 @@
+import React, {PropTypes} from 'react';
+
+import FlashMessages from './FlashMessages/index';
+
+const App = ({globalRegistry, menu}) => {
+    const containerRegistry = globalRegistry.get('containers');
+
+    const Modals = containerRegistry.get('Modals');
+    const FullScreen = containerRegistry.get('FullScreen');
+    const PrimaryToolbar = containerRegistry.get('PrimaryToolbar');
+    const EditModePanel = containerRegistry.get('EditModePanel');
+    const SecondaryToolbar = containerRegistry.get('SecondaryToolbar');
+    const Drawer = containerRegistry.get('Drawer');
+    const LeftSideBar = containerRegistry.get('LeftSideBar');
+    const ContentCanvas = containerRegistry.get('ContentCanvas');
+    const RightSideBar = containerRegistry.get('RightSideBar');
+
+    return (
+        <div>
+            <div id="dialog"/>
+            <Modals/>
+            <FlashMessages/>
+            <FullScreen/>
+            <PrimaryToolbar/>
+            <EditModePanel/>
+            <SecondaryToolbar/>
+            <Drawer menuData={menu}/>
+            <LeftSideBar/>
+            <ContentCanvas/>
+            <RightSideBar/>
+        </div>
+    );
+};
+App.propTypes = {
+    globalRegistry: PropTypes.object.isRequired,
+    menu: PropTypes.object.isRequired
+};
+
+export default App;

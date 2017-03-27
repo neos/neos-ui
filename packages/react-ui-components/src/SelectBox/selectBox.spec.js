59793cb981055ba0a6e35b7ea78ea8e1bf671cb6
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2017-03-27 21:13:29 +0200

    fix tests

--- packages/react-ui-components/src/SelectBox/selectBox.spec.js
+++ packages/react-ui-components/src/SelectBox/selectBox.spec.js
@@ -29,12 +29,6 @@ test('should render passed options straight away', t => {
     t.is(selectBox.instance().getOptions(), props.options);
 });
 
-test('should have an initial value of \'\' (empty string)', t => {
-    const selectBox = shallow();
-
-    t.is(selectBox.instance().state.value, '');
-});
-
 test('should have the selected value', t => {
     const props = {
         options: [{value: 'foo', label: 'bar'}]
@@ -69,42 +63,4 @@ test('should not call "onSelect" on initial render when having a value set', t =
     selectBox.instance().componentDidMount();
 
     t.truthy(props.onSelect.notCalled);
-});
-
-test('should render a searchbox when explicitly enabled', t => {
-    const props = {
-        minimumResultsForSearch: 0
-    };
-    const selectBox = shallow(props);
-
-    t.truthy(selectBox.instance().isSearchEnabled());
-});
-
-test('should not render a searchbox when explicitly disabled', t => {
-    const props = {
-        minimumResultsForSearch: -1
-    };
-    const selectBox = shallow(props);
-
-    t.falsy(selectBox.instance().isSearchEnabled());
-});
-
-test('should render a delete icon if a `onDelete` prop was passed', t => {
-    const props = {
-        minimumResultsForSearch: -1,
-        onDelete: sinon.spy()
-    };
-    const selectBox = shallow(props);
-    const deleteAnchor = selectBox.find(DropDownComponent.Header).find('a');
-
-    t.is(deleteAnchor.length, 1);
-    t.is(deleteAnchor.find(defaultProps.IconComponent).length, 1);
-    t.is(deleteAnchor.find(defaultProps.IconComponent).prop('icon'), 'close');
-
-    deleteAnchor.simulate('click', {
-        preventDefault: () => null,
-        stopPropagation: () => null
-    });
-
-    t.is(props.onDelete.callCount, 1);
 });

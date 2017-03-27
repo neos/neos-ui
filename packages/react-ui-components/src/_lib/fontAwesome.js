923c29618169df0b91bef377738149cdb50d9b37
Max Strübing <mxstrbng@gmail.com>
2017-03-27 14:55:07 +0200

    BUGFIX: Check if -o suffix is needed

--- packages/react-ui-components/src/_lib/fontAwesome.js
+++ packages/react-ui-components/src/_lib/fontAwesome.js
@@ -5,9 +5,14 @@ export function makeValidateId(icons = {}) {
         //
         // Automatically prefix the passed id with fa regardless which prefix was passed
         //
-        const name = id.startsWith('fa-') ? id : (id.startsWith('icon-') ? id.replace(/^icon/, 'fa') : `fa-${id}`);
+        const tempName = id.startsWith('fa-') ? id : (id.startsWith('icon-') ? id.replace(/^icon/, 'fa') : `fa-${id}`);
 
-        const isValid = ICON_NAMES.includes(name);
+        //
+        // becuase e.g. picture is called picture-o in FA
+        //
+        const nameWithSuffix = `${tempName}-o`;
+        const isValid = ICON_NAMES.includes(tempName) || ICON_NAMES.includes(nameWithSuffix);
+        const name = ICON_NAMES.includes(tempName) ? tempName : nameWithSuffix;
 
         return {
             isValid,

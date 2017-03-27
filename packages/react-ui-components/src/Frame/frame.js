b97128059de3dfd88faa63d4f20f25d6465562a3
Christian Mueller <christian@flownative.com>
2017-03-27 13:37:10 +0200

    TASK: Refactor frame component

--- packages/react-ui-components/src/Frame/frame.js
+++ packages/react-ui-components/src/Frame/frame.js
@@ -1,101 +1,44 @@
 // 98997e98ea6d5ececc49db0de528151e40e272b7 from https://github.com/ryanseddon/react-frame-component/commit/98997e98ea6d5ececc49db0de528151e40e272b7 - and then modified!
-import React from 'react';
+import React, {PureComponent, PropTypes} from 'react';
 import omit from 'lodash.omit';
 import ReactDOM from 'react-dom';
-import assign from 'object-assign';
 
-const hasConsole = typeof window !== 'undefined' && window.console;
-const noop = function () {};
-let swallowInvalidHeadWarning = noop;
-let resetWarnings = noop;
-
-if (hasConsole) {
-    const originalError = console.error;
-    // Rendering a <head> into a body is technically invalid although it
-    // works. We swallow React's validateDOMNesting warning if that is the
-    // message to avoid confusion
-    swallowInvalidHeadWarning = () => {
-        console.error = msg => {
-            if (/<head>/.test(msg)) {
-                return;
-            }
-            originalError.call(console, msg);
-        };
-    };
-    resetWarnings = () => {
-        console.error = originalError;
-    };
-}
-
-const Frame = React.createClass({
-    // React warns when you render directly into the body since browser extensions
-    // also inject into the body and can mess up React. For this reason
-    // initialContent initialContent is expected to have a div inside of the body
-    // element that we render react into.
-    propTypes: {
-        style: React.PropTypes.object,
-        head: React.PropTypes.node,
-        initialContent: React.PropTypes.string,
-        mountTarget: React.PropTypes.string,
-        contentDidUpdate: React.PropTypes.func,
+export default class Frame extends PureComponent {
+    static propTypes = {
+        mountTarget: React.PropTypes.string.isRequired,
+        contentDidUpdate: React.PropTypes.func.isRequired,
         children: React.PropTypes.node
-    },
-    getDefaultProps() {
-        return {
-            initialContent: '<!DOCTYPE html><html><head></head><body><div></div></body></html>',
-            contentDidUpdate: () => null
-        };
-    },
+    };
+
     render() {
-        // The iframe isn't ready so we drop children from props here. #12, #17
         const rest = omit(this.props, [
             'mountTarget',
             'contentDidUpdate',
-            'contentDidMount',
             'theme',
-            'initialContent'
+            'children'
         ]);
 
-        return React.createElement('iframe', assign({}, rest, {children: undefined}));
-    },
+        return <iframe {...rest}/>
+    }
     componentWillMount() {
-        document.addEventListener('Neos.Content.Ready', () => this.renderFrameContents());
-    },
-    renderFrameContents() {
+        document.addEventListener('Neos.Neos.Ui.ContentReady', this.renderFrameContents);
+    }
+    renderFrameContents = () => {
+        console.log(this);
         const doc = ReactDOM.findDOMNode(this).contentDocument; // eslint-disable-line react/no-find-dom-node
         const win = ReactDOM.findDOMNode(this).contentWindow; // eslint-disable-line react/no-find-dom-node
-        // TODO: doc.readyState seems to be *always* true in Chrome at least; so we check whether the querySelectors exist.
-        const contents = React.createElement('div',
-            undefined,
-            this.props.head,
-            this.props.children
-        );
-
-        swallowInvalidHeadWarning();
-
-        // unstable_renderSubtreeIntoContainer allows us to pass this component as
-        // the parent, which exposes context to any child components.
-        const callback = this.props.contentDidUpdate;
-        let mountTarget;
-
-        if (this.props.mountTarget) {
-            mountTarget = doc.querySelector(this.props.mountTarget);
-        } else {
-            mountTarget = doc.body.children[0];
-        }
+        const mountTarget = doc.querySelector(this.props.mountTarget);
+        const contents = React.createElement('div', undefined, this.props.children);
 
         ReactDOM.unstable_renderSubtreeIntoContainer(this, contents, mountTarget, () => {
-            callback(win, doc, mountTarget);
+            this.props.contentDidUpdate(win, doc, mountTarget);
         });
-
-        resetWarnings();
-    },
+    }
     componentWillUnmount() {
         const doc = ReactDOM.findDOMNode(this).contentDocument; // eslint-disable-line react/no-find-dom-node
+        document.removeEventListener('Neos.Neos.Ui.ContentReady', this.renderFrameContents);
         if (doc) {
             ReactDOM.unmountComponentAtNode(doc.body);
         }
     }
-});
-
-module.exports = Frame;
+}

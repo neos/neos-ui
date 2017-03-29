81a93c588d099804c03aad6288ca7e758af28417
Gerhard Boden <gerhard.boden@gmail.com>
2017-03-29 21:11:24 +0200

    TASK: Remove unnecessary compress and merge methods

--- Classes/Neos/Neos/Ui/Domain/Model/Changes/AbstractCreate.php
+++ Classes/Neos/Neos/Ui/Domain/Model/Changes/AbstractCreate.php
@@ -116,38 +116,6 @@ abstract class AbstractCreate extends AbstractStructuralChange
     }
 
     /**
-     * Checks whether this change can be merged with a subsequent change
-     *
-     * @param  ChangeInterface $subsequentChange
-     * @return boolean
-     */
-    public function canMerge(ChangeInterface $subsequentChange)
-    {
-        if (!$subsequentChange instanceof AbstractCreate) {
-            return false;
-        }
-
-        if ($subsequentChange->getSubject() !== $this->getSubject()) {
-            return false;
-        }
-
-        return $subsequentChange->canApply();
-    }
-
-    /**
-     * Merges this change with a subsequent change
-     *
-     * @param  ChangeInterface $subsequentChange
-     * @return void
-     */
-    public function merge(ChangeInterface $subsequentChange)
-    {
-        if ($this->canMerge($subsequentChange)) {
-            return $subsequentChange;
-        }
-    }
-
-    /**
      * Creates a new node beneath $parent
      *
      * @param  NodeInterface $parent

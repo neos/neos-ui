03688f4b690903b50c32168fdb56fcefd1dec91b
Max Strübing <mxstrbng@gmail.com>
2017-04-20 13:27:48 +0200

    TASK: Add tests for striptagsEncoded

--- packages/utils-helpers/src/striptags.spec.js
+++ packages/utils-helpers/src/striptags.spec.js
@@ -1,4 +1,4 @@
-import striptags from './striptags';
+import {striptags, striptagsEncoded} from './striptags';
 
 test(`should strip html tags if string contains some`, () => {
     const string = '<p>hello world</p>';
@@ -8,4 +8,14 @@ test(`should strip html tags if string contains some`, () => {
 test(`should return the same string if string does not contain html tags`, () => {
     const string = 'hello world';
     expect(striptags(string)).toEqual(string);
+});
+
+test(`should strip html tags if string contains some`, () => {
+    const string = '&lt;p&gt;hello world&lt;/p&gt;';
+    expect(striptagsEncoded(string)).toEqual('hello world');
+});
+
+test(`should return the same string if string does not contain html tags`, () => {
+    const string = 'hello world';
+    expect(striptagsEncoded(string)).toEqual(string);
 });

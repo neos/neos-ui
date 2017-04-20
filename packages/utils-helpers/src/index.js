11ea4804e8bc2ba96058e9783bfceb30a4227ef5
Max Strübing <mxstrbng@gmail.com>
2017-04-20 13:27:28 +0200

    TASK: Add striptags for encoded html entities

--- packages/utils-helpers/src/index.js
+++ packages/utils-helpers/src/index.js
@@ -2,12 +2,13 @@ import delay from './delay';
 import discover from './discover';
 import isThenable from './isThenable';
 import loadScript from './loadScript';
-import striptags from './striptags';
+import {striptags, striptagsEncoded} from './striptags';
 
 export {
     delay,
     discover,
     isThenable,
     loadScript,
-    striptags
+    striptags,
+    striptagsEncoded
 };
--- packages/utils-helpers/src/striptags.js
+++ packages/utils-helpers/src/striptags.js
@@ -1,3 +1,4 @@
 const striptags = string => string.replace(/<\/?[^>]+(>|$)/g, '');
+const striptagsEncoded = string => string.replace(/&lt;\/?[^&gt;]+(&gt;|$)/g, '');
 
-export default striptags;
+export {striptags, striptagsEncoded};

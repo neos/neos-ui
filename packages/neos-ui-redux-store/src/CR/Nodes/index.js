c9a97d2c9a4bc2ab7288f46f62962d44f614854d
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-04-24 17:33:04 +0200

    TASK: Remove unnecessary  call from CR.Nodes reducer

--- packages/neos-ui-redux-store/src/CR/Nodes/index.js
+++ packages/neos-ui-redux-store/src/CR/Nodes/index.js
@@ -192,9 +192,7 @@ export const reducer = handleActions({
             )
         ))
     ),
-    [FOCUS]: ({contextPath, fusionPath}) => $all(
-        $set('cr.nodes.focused', new Map({contextPath, fusionPath}))
-    ),
+    [FOCUS]: ({contextPath, fusionPath}) => $set('cr.nodes.focused', new Map({contextPath, fusionPath})),
     [UNFOCUS]: () => $set('cr.nodes.focused', new Map({
         contextPath: '',
         fusionPath: ''

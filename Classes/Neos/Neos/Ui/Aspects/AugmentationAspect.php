4d1e0304c3a679a02e42e93280ae4b0334ff3f2f
Karsten Dambekalns <karsten@dambekalns.de>
2017-06-26 12:02:10 +0200

    TASK: Replace typoScript with Fusion in comment

--- Classes/Neos/Neos/Ui/Aspects/AugmentationAspect.php
+++ Classes/Neos/Neos/Ui/Aspects/AugmentationAspect.php
@@ -103,7 +103,7 @@ class AugmentationAspect
 
     /**
      * Hooks into standard content element wrapping to render those attributes needed for the package to identify
-     * nodes and typoScript paths
+     * nodes and Fusion paths
      *
      * @Flow\Around("method(Neos\Neos\Service\ContentElementWrappingService->wrapContentObject())")
      * @param JoinPointInterface $joinPoint the join point

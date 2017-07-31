1a6259226e7ee2f5d409d911d2e8b00054550e31
Torsten Blindert <info@by-torsten.com>
2017-07-31 14:43:06 +0200

    TASK: DateInput should accept both string and date

--- packages/neos-ui-editors/src/DateTime/index.js
+++ packages/neos-ui-editors/src/DateTime/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DateInput from '@neos-project/react-ui-components/src/DateInput/';
+import moment from 'moment';
 
 const DateTime = props => {
     const {value, commit} = props;
-    const mappedVal = value && value.length ? value : undefined;
+    const mappedValue = (typeof value === 'string' && value.length) ? moment(value).toDate() : (value || undefined);
 
     return <DateInput value={mappedVal} onChange={commit}/>;
 };
 DateTime.propTypes = {
-    value: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
     commit: PropTypes.func.isRequired
 };
 

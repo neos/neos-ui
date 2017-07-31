2b5d702dc7937fef2687d0c051b7f0cb7f19e2e9
Torsten Blindert <info@by-torsten.com>
2017-07-31 14:43:57 +0200

    TASK: Wrap onChange event of DatePicker to bring the date obect to the correct neos property format

--- packages/neos-ui-editors/src/DateTime/index.js
+++ packages/neos-ui-editors/src/DateTime/index.js
@@ -7,8 +7,13 @@ const DateTime = props => {
     const {value, commit} = props;
     const mappedValue = (typeof value === 'string' && value.length) ? moment(value).toDate() : (value || undefined);
 
-    return <DateInput value={mappedVal} onChange={commit}/>;
+    const onChange = date => {
+        commit(moment(date).format('YYYY-MM-DDTHH:MM:SSZ'));
+    };
+
+    return <DateInput value={mappedValue} onChange={onChange}/>;
 };
+
 DateTime.propTypes = {
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
     commit: PropTypes.func.isRequired

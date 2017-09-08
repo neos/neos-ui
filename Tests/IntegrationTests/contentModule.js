d15758beb78a0b69efa84d85676ee8d395b1d304
Dmitri Pisarev <dimaip@gmail.com>
2017-09-08 15:11:51 +0300

    TASK: write testcafe tests for PageTree search and filter

--- Tests/IntegrationTests/contentModule.js
+++ Tests/IntegrationTests/contentModule.js
@@ -1,6 +1,9 @@
 import {Selector, Role} from 'testcafe';
 import ReactSelector from 'testcafe-react-selectors';
 
+const section = name => console.log('\x1b[44m%s\x1b[0m', name);
+const subSection = name => console.log('\x1b[33m%s\x1b[0m', ' - ' + name);
+
 /* global fixture:true */
 /* eslint babel/new-cap: 0 */
 
@@ -13,14 +16,43 @@ const adminUser = Role(adminUrl, async t => {
         .click('button.neos-login-btn');
 }, {preserveUrl: true});
 
-fixture `ContentCanvas`
+fixture `Content Module`
     .beforeEach(async t => {
         await t.useRole(adminUser);
     });
 
-// TODO: split tests when it's possible to skip authentication
 test('All tests at once', async t => {
-    console.log('Can toggle leftSideBar');
+    section('PageTree search and filter');
+    subSection('Search the page tree');
+    const nodeTreeSearchInput = ReactSelector('NodeTreeSearchInput');
+    const treeNode = ReactSelector('Node').find('span');
+    const nodeTreeFilter = ReactSelector('NodeTreeFilter');
+    const shortcutFilter = ReactSelector('NodeTreeFilter').find('li').withText('Shortcut');
+    await t
+        .typeText(nodeTreeSearchInput, 'Download')
+        .expect(treeNode.withText('Download').count).eql(2, 'Two "Download" nodes should be found, on shortcut and one normal page')
+        .expect(treeNode.withText('Try me').exists).notOk('Top level "Try me" page should be hidden ');
+    subSection('Set the Shortcut filter');
+    await t
+        .click(nodeTreeFilter)
+        .click(shortcutFilter)
+        .expect(treeNode.withText('Download').count).eql(1, 'Only one "Download" page should be found, of type Shortcut')
+        .expect(treeNode.withText('Shortcut to child node').exists).notOk('No matching "Shortcut" pages should be hidden')
+        .expect(treeNode.withText('Try me').exists).notOk('Top level "Try me" page should still be hidden');
+    subSection('Clear search');
+    const clearSearch = ReactSelector('NodeTreeSearchInput IconButton');
+    await t
+        .click(clearSearch)
+        .expect(treeNode.withText('Shortcut to child node').exists).ok('All "Shortcut" pages should be found')
+        .expect(treeNode.withText('Try me').exists).notOk('Top level "Try me" page should still be hidden');
+    subSection('Clear filter');
+    const clearFilter = ReactSelector('NodeTreeFilter Button');
+    await t
+        .click(clearFilter)
+        .expect(treeNode.withText('Shortcut to child node').exists).notOk('Deep Shortcut pages should be hidden')
+        .expect(treeNode.withText('Try me').exists).ok('Top level "Try me" page should shown again');
+
+    section('Can toggle leftSideBar');
     const leftSideBarToggler = ReactSelector('LeftSideBarToggler Button');
     const leftSideBar = ReactSelector('LeftSideBar');
     await t
@@ -30,7 +62,7 @@ test('All tests at once', async t => {
         .click(leftSideBarToggler)
         .expect(leftSideBar.getReact(({props}) => props.isHidden)).eql(false);
 
-    console.log('Can toggle rightSideBar');
+    section('Can toggle rightSideBar');
     const rightSideBarToggler = ReactSelector('RightSideBar Button');
     const rightSideBar = ReactSelector('RightSideBar');
     await t
@@ -40,7 +72,7 @@ test('All tests at once', async t => {
         .click(rightSideBarToggler)
         .expect(rightSideBar.getReact(({props}) => props.isHidden)).eql(false);
 
-    console.log('Can create a new page');
+    section('Can create a new page');
     const SelectNodeTypeModal = ReactSelector('SelectNodeType');
     const newPageTitle = 'TestPage';
     await t
@@ -59,7 +91,7 @@ test('All tests at once', async t => {
         .expect(Selector('li').withText(newPageTitle).exists).ok()
         .switchToMainWindow();
 
-    console.log('Can create content node from inside InlineUI');
+    section('Can create content node from inside InlineUI');
     const headlineTitle = 'Helloworld!';
     await t
         .switchToIframe('[name="neos-content-main"]')

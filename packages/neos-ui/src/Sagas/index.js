c594908198a9cc5c8dc420e2a8f32707127468bd
Dmitri Pisarev <dimaip@gmail.com>
2017-09-14 23:54:00 +0300

    BUGFIX: remove history.pushState
    No need to do this, browser itself detects changes to iframe's src and
    pushes them to history. Otherwise history entries are duplicated.

--- packages/neos-ui/src/Sagas/index.js
+++ packages/neos-ui/src/Sagas/index.js
@@ -1,4 +1,3 @@
-import {sagas as BrowserSagas} from './Browser/index';
 import {sagas as ChangesSagas} from './Changes/index';
 import {sagas as CRSagas} from './CR/index';
 import {sagas as PublishSagas} from './Publish/index';
@@ -6,7 +5,6 @@ import {sagas as ServerFeedbackSagas} from './ServerFeedback/index';
 import {sagas as UISagas} from './UI/index';
 
 export default [
-    ...BrowserSagas,
     ...ChangesSagas,
     ...CRSagas,
     ...PublishSagas,

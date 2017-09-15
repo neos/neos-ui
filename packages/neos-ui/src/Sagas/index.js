15a04f8ad82eba9200091aaf194d825297ad70fe
Dmitri Pisarev <dimaip@gmail.com>
2017-09-15 21:23:15 +0300

    Revert "BUGFIX: remove history.pushState"
    This reverts commit c594908198a9cc5c8dc420e2a8f32707127468bd.

--- packages/neos-ui/src/Sagas/index.js
+++ packages/neos-ui/src/Sagas/index.js
@@ -1,3 +1,4 @@
+import {sagas as BrowserSagas} from './Browser/index';
 import {sagas as ChangesSagas} from './Changes/index';
 import {sagas as CRSagas} from './CR/index';
 import {sagas as PublishSagas} from './Publish/index';
@@ -5,6 +6,7 @@ import {sagas as ServerFeedbackSagas} from './ServerFeedback/index';
 import {sagas as UISagas} from './UI/index';
 
 export default [
+    ...BrowserSagas,
     ...ChangesSagas,
     ...CRSagas,
     ...PublishSagas,

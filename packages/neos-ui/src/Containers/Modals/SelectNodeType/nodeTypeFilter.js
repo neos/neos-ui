075135238264c5738ed6d842c1a2df91ac11b2a9
Johannes Steu <hello@johannessteu.de>
2017-09-18 13:39:36 +0200

    TASK: Add reset filter button

--- packages/neos-ui/src/Containers/Modals/SelectNodeType/nodeTypeFilter.js
+++ packages/neos-ui/src/Containers/Modals/SelectNodeType/nodeTypeFilter.js
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import TextInput from '@neos-project/react-ui-components/src/TextInput/';
+import IconButton from '@neos-project/react-ui-components/src/IconButton/';
 import Icon from '@neos-project/react-ui-components/src/Icon/';
 import {neos} from '@neos-project/neos-ui-decorators';
 import style from './style.css';
@@ -11,6 +12,7 @@ import style from './style.css';
 class NodeTypeFilter extends PureComponent {
     static propTypes = {
         onChange: PropTypes.func.isRequired,
+        filterSearchTerm: PropTypes.string,
         i18nRegistry: PropTypes.object.isRequired
     };
 
@@ -18,21 +20,34 @@ class NodeTypeFilter extends PureComponent {
         super(props);
 
         this.handleValueChange = this.handleValueChange.bind(this);
+        this.handleResetFilter = this.handleResetFilter.bind(this);
     }
 
     render() {
-        const {i18nRegistry} = this.props;
+        const {i18nRegistry, filterSearchTerm} = this.props;
         const label = i18nRegistry.translate('filter', 'Filter', {}, 'Neos.Neos', 'Main');
 
         return (
             <div className={style.nodeTypeDialogHeader__filter}>
-                <Icon icon="filter" padded="right"/>
+
+                {filterSearchTerm ? (
+                    <IconButton icon="close" onClick={this.handleResetFilter}/>
+                ) : (
+                    <Icon icon="filter" padded="right"/>
+                ) }
+
                 <TextInput
+                    value={filterSearchTerm}
                     onChange={this.handleValueChange}
                     placeholder={label}
                     />
             </div>
         );
+    }
+
+    handleResetFilter() {
+        const {onChange} = this.props;
+        onChange('');
     }
 
     handleValueChange(filterSearchTerm) {

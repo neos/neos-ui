neos/neos-ui

commit 9ca09aa6d80bfd2b45ca61684dc5f15bbe585a48

BUGFIX: only allow to create nodes of the same role as the reference node

packages/neos-ui-contentrepository/src/registry/NodeTypesRegistry.js
@@ -47,15 +47,19 @@ export default class NodeTypesRegistry extends SynchronousRegistry {
         return Object.keys(result || []).filter(key => result[key]);
     }
 
-    getAllowedNodeTypesTakingAutoCreatedIntoAccount(isSubjectNodeAutocreated, referenceParentName, referenceParentNodeType, referenceGrandParentNodeType) {
+    getAllowedNodeTypesTakingAutoCreatedIntoAccount(isSubjectNodeAutocreated, referenceParentName, referenceParentNodeType, referenceGrandParentNodeType, role) {
+        let result;
         if (isSubjectNodeAutocreated) {
             if (!referenceGrandParentNodeType) {
                 return [];
             }
-            return this.getAllowedGrandChildNodeTypes(referenceGrandParentNodeType, referenceParentName);
+            result = this.getAllowedGrandChildNodeTypes(referenceGrandParentNodeType, referenceParentName);
         }
 
-        return this.getAllowedChildNodeTypes(referenceParentNodeType);
+        result = this.getAllowedChildNodeTypes(referenceParentNodeType);
+        // If role is provided, filter by role, e.g. only "content" or "document" ndoetypes
+        console.log(role);
+        return role ? result.filter(nodeTypeName => this.hasRole(nodeTypeName, role)) : result;
     }
 
     getGroupedNodeTypeList(nodeTypeFilter) {

packages/neos-ui-redux-store/src/CR/Nodes/selectors.js
@@ -177,7 +177,7 @@ export const clipboardIsEmptySelector = createSelector(
     clipboardNodePath => Boolean(clipboardNodePath)
 );
 
-const getPathInNode = (state, contextPath, propertyPath) => {
+export const getPathInNode = (state, contextPath, propertyPath) => {
     const node = $get(['cr', 'nodes', 'byContextPath', contextPath], state);
 
     return $get(propertyPath, node);
@@ -188,7 +188,8 @@ export const makeGetAllowedChildNodeTypesSelector = (nodeTypesRegistry, elevator
         (state, {reference}) => getPathInNode(state, elevator(reference), 'isAutoCreated'),
         (state, {reference}) => getPathInNode(state, elevator(reference), 'name'),
         (state, {reference}) => getPathInNode(state, elevator(reference), 'nodeType'),
-        (state, {reference}) => getPathInNode(state, elevator(parentNodeContextPath(reference)), 'nodeType')
+        (state, {reference}) => getPathInNode(state, elevator(parentNodeContextPath(reference)), 'nodeType'),
+        (state, {role}) => role
     ],
     (...args) => nodeTypesRegistry.getAllowedNodeTypesTakingAutoCreatedIntoAccount(...args)
 );

packages/neos-ui/src/Containers/Modals/SelectNodeType/index.js
@@ -34,8 +34,10 @@ const calculateInitialMode = (allowedSiblingNodeTypes, allowedChildNodeTypes) =>
 
     return state => {
         const reference = $get('ui.selectNodeTypeModal.referenceNodeContextPath', state);
-        const allowedSiblingNodeTypes = nodeTypesRegistry.getGroupedNodeTypeList(getAllowedSiblingNodeTypesSelector(state, {reference}));
-        const allowedChildNodeTypes = nodeTypesRegistry.getGroupedNodeTypeList(getAllowedChildNodeTypesSelector(state, {reference}));
+        const referenceNodeType = selectors.CR.Nodes.getPathInNode(state, reference, 'nodeType');
+        const role = nodeTypesRegistry.hasRole(referenceNodeType, 'document') ? 'document' : 'content';
+        const allowedSiblingNodeTypes = nodeTypesRegistry.getGroupedNodeTypeList(getAllowedSiblingNodeTypesSelector(state, {reference, role}));
+        const allowedChildNodeTypes = nodeTypesRegistry.getGroupedNodeTypeList(getAllowedChildNodeTypesSelector(state, {reference, role}));
 
         return {
             isOpen: $get('ui.selectNodeTypeModal.isOpen', state),

commit 18d9c21cf14a4d83e929f6ce6b8079d260e39fc1

BUGFIX: fix constraints with auto-created child nodes
This is broken since 9ca09aa6d80bfd2b45ca61684dc5f15bbe585a48 - but
fortunately easy to fix :)

packages/neos-ui-contentrepository/src/registry/NodeTypesRegistry.js
@@ -54,9 +54,10 @@ export default class NodeTypesRegistry extends SynchronousRegistry {
                 return [];
             }
             result = this.getAllowedGrandChildNodeTypes(referenceGrandParentNodeType, referenceParentName);
+        } else {
+            result = this.getAllowedChildNodeTypes(referenceParentNodeType);
         }
 
-        result = this.getAllowedChildNodeTypes(referenceParentNodeType);
         // If role is provided, filter by role, e.g. only "content" or "document" ndoetypes
         return role ? result.filter(nodeTypeName => this.hasRole(nodeTypeName, role)) : result;
     }
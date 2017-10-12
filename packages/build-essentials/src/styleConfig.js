c0e974f1f3e20636d513eebf35a638a0890b111e
Dustin Chabrowski <dustin.duske@gmail.com>
2017-10-12 21:36:04 +0200

    TASK: use arrays to describe z-indices

--- packages/build-essentials/src/styleConfig.js
+++ packages/build-essentials/src/styleConfig.js
@@ -3,89 +3,27 @@ const upperFirst = require('lodash.upperfirst');
 // Global CSS variables for Neos.Ui
 const config = {
     zIndex: {
-        secondaryToolbar: {
-            linkIconButtonFlyout: '1'
-        },
-        flashMessageContainer: {
-            context: '1'
-        },
-        secondaryInspector: {
-            context: '1',
-            close: '2'
-        },
-        secondaryInspectorElevated: {
-            context: '1',
-            dropdownContents: '2'
-        },
-        dialog: {
-            context: '1'
-        },
-        fullScreenClose: {
-            context: '1'
-        },
-        drawer: {
-            context: '1'
-        },
-        bar: {
-            context: '1'
-        },
-        primaryToolbar: {
-            context: '1'
-        },
-        checkboxInput: {
-            context: '1'
-        },
-        dropdownContents: {
-            context: '1'
-        },
-        selectBoxContents: {
-            context: '1'
-        },
-        selectBoxLoadingIcon: {
-            context: '1'
-        },
-        selectBoxBtnDelete: {
-            context: '1'
-        },
-        notInlineEditableOverlay: {
-            context: '1'
-        },
-        calendarFakeInputMirror: {
-            context: '1'
-        },
-        rdtPicker: {
-            context: '1'
-        },
-        sideBar: {
-            dropTargetBefore: '1',
-            dropTargetAfter: '2'
-        },
-        wrapperDropdown: {
-            context: '1'
-        },
-        flashMessage: '999999', //
-        linkIconButton: '1000', //
-        aspectRatioDropdownContents: '10', // secondaryInspectorElevated.dropdownContents
-        nodeToolBar: '2147483647',
-        notInlineEditableOverlay: '10000', // notInlineEditableOverlay.context
-        secondaryInspectorElevated: '10001', // secondaryInspectorElevated.context
-        secondaryInspectorClose: '10', // secondaryInspector.close
-        drawer: '8000', // drawer.context
-        fullScreenClose: '1', // fullScreenClose.context
-        primaryToolbar: '9999', // primaryToolbar.context
-        unappliedChangesOverlay: '10000',
-        bar: '1', // bar.context
-        checkboxInput: '1', // checkoboxInput.context
-        calendarFakeInputMirror: '1', // calendarFakeInputMirror.context
-        rdtPicker: '99999 !important', // rdtPicker.context
-        dialog: '999', // dialog.context
-        dropdownContents: '1', // dropdownContents.context
-        wrapperDropdown: '1', //
-        selectBoxLoadingIcon: '1', // selectBoxLoadingIcon.context
-        selectBoxBtnDelete: '1', //
-        selectBoxContents: '10', // selectBoxContents.context
-        dropTargetBefore: '1', // sideBar.dropTargetBefore
-        dropTargetAfter: '2' // sidebar.dropTargetAfter
+        secondaryToolbar: ['linkIconButtonFlyout'],
+        flashMessageContainer: ['context'],
+        secondaryInspector: ['context', 'close'],
+        secondaryInspectorElevated: ['context', 'dropdownContents'],
+        dialog: ['context'],
+        fullScreenClose: ['context'],
+        drawer: ['context'],
+        bar: ['context'],
+        primaryToolbar: ['context'],
+        checkboxInput: ['context'],
+        dropdownContents: ['context'],
+        selectBoxContents: ['context'],
+        selectBoxLoadingIcon: ['context'],
+        selectBoxBtnDelete: ['context'],
+        notInlineEditableOverlay: ['context'],
+        calendarFakeInputMirror: ['context'],
+        rdtPicker: ['context'],
+        sideBar: ['dropTargetBefore', 'dropTargetAfter'],
+        wrapperDropdown: ['context'],
+        unappliedChangesOverlay: ['context'],
+        nodeToolBar: '2147483647'
     }
 };
 

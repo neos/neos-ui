3723f05bb643bb8610c5493e4beb3b9a7b64c3db
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-11-16 14:06:47 +0100

    FEATURE: Add policy info for EditNodePrivilege

--- Classes/Neos/Neos/Ui/Aspects/PolicyAspect.php
+++ Classes/Neos/Neos/Ui/Aspects/PolicyAspect.php
@@ -13,11 +13,11 @@ use Neos\ContentRepository\Domain\Service\NodeTypeManager;
 use Neos\ContentRepository\Security\Authorization\Privilege\Node\NodePrivilegeSubject;
 use Neos\ContentRepository\Security\Authorization\Privilege\Node\CreateNodePrivilegeSubject;
 use Neos\Neos\Security\Authorization\Privilege\NodeTreePrivilege;
-use Neos\Neos\Security\Authorization\Privilege\EditNodePrivilege;
 use Neos\Neos\Security\Authorization\Privilege\ReadNodePrivilege;
 use Neos\Neos\Security\Authorization\Privilege\EditNodePropertyPrivilege;
 use Neos\ContentRepository\Security\Authorization\Privilege\Node\CreateNodePrivilege;
 use Neos\ContentRepository\Security\Authorization\Privilege\Node\RemoveNodePrivilege;
+use Neos\ContentRepository\Security\Authorization\Privilege\Node\EditNodePrivilege;
 
 /**
  * Add information to rendered nodes relevant to enforce the following privileges
@@ -108,19 +108,20 @@ class PolicyAspect
     }
 
     /**
-     * @Flow\Around("method()")
-     * @return void
+     * @Flow\Around("method(Neos\Neos\Ui\Fusion\Helper\NodeInfoHelper->renderNode())")
+     * @return array
      */
     public function enforceEditNodePrivilege(JoinPointInterface $joinPoint)
     {
-    }
+        $node = $joinPoint->getMethodArgument('node');
+        $nodeInfo = $joinPoint->getAdviceChain()->proceed($joinPoint);
 
-    /**
-     * @Flow\Around("method()")
-     * @return void
-     */
-    public function enforceReadNodePrivilege(JoinPointInterface $joinPoint)
-    {
+        $canEdit = $this->privilegeManager->isGranted(EditNodePrivilege::class, new NodePrivilegeSubject($node));
+
+        $nodeInfo['policy'] = array_key_exists('policy', $nodeInfo) ? $nodeInfo['policy'] : [];
+        $nodeInfo['policy']['canEdit'] = $canEdit;
+
+        return $nodeInfo;
     }
 
     /**

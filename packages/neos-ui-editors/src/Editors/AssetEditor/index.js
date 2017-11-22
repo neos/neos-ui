697897eba4fdd9135dc13047b08e43741ed338d7
Dmitri Pisarev <dimaip@gmail.com>
2017-11-22 19:26:26 +0300

    BUGFIX: fix AssetEditor to support single assets

--- packages/neos-ui-editors/src/Editors/AssetEditor/index.js
+++ packages/neos-ui-editors/src/Editors/AssetEditor/index.js
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import MultiSelectBox from '@neos-project/react-ui-components/src/MultiSelectBox/';
+import SelectBox from '@neos-project/react-ui-components/src/SelectBox/';
 import AssetOption from '@neos-project/neos-ui-ckeditor-bindings/src/EditorToolbar/AssetOption';
 import {dndTypes} from '@neos-project/neos-ui-constants';
 import {neos} from '@neos-project/neos-ui-decorators';
@@ -33,22 +34,22 @@ export default class AssetEditor extends PureComponent {
     };
 
     static propTypes = {
-        value: PropTypes.arrayOf(PropTypes.string),
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.object, PropTypes.arrayOf(PropTypes.string)]),
         options: PropTypes.object,
         searchOptions: PropTypes.array,
         highlight: PropTypes.bool,
         placeholder: PropTypes.string,
-        displayLoadingIndicator: PropTypes.bool,
         onSearchTermChange: PropTypes.func,
         commit: PropTypes.func.isRequired,
         i18nRegistry: PropTypes.object.isRequired,
         assetLookupDataLoader: PropTypes.shape({
+            resolveValue: PropTypes.func.isRequired,
             resolveValues: PropTypes.func.isRequired,
             search: PropTypes.func.isRequired
         }).isRequired,
         siteNodePath: PropTypes.string.isRequired,
         secondaryEditorsRegistry: PropTypes.object.isRequired,
-        renderSecondaryInspector: PropTypes.object.isRequired
+        renderSecondaryInspector: PropTypes.func.isRequired
     };
 
     componentDidMount() {
@@ -61,11 +62,20 @@ export default class AssetEditor extends PureComponent {
         }
     }
 
+    getValue() {
+        const value = this.props.value;
+        if (value.__identity) {
+            // Needed to handle Image assets
+            return value.__identity;
+        }
+        return value;
+    }
+
     resolveValue = () => {
         if (this.props.value) {
             this.setState({isLoading: true});
-            const resolver = this.props.assetLookupDataLoader.resolveValues.bind(this.props.assetLookupDataLoader);
-            resolver({}, this.props.value)
+            const resolver = this.props.options.multiple ? this.props.assetLookupDataLoader.resolveValues.bind(this.props.assetLookupDataLoader) : this.props.assetLookupDataLoader.resolveValue.bind(this.props.assetLookupDataLoader);
+            resolver({}, this.getValue())
                 .then(options => {
                     this.setState({
                         isLoading: false,
@@ -106,10 +116,13 @@ export default class AssetEditor extends PureComponent {
 
     handleMediaSelected = assetIdentifier => {
         const {value} = this.props;
-        const values = value ? value.slice() : [];
-
-        values.push(assetIdentifier);
-        this.handleValueChange(values);
+        if (this.props.options.multiple) {
+            const values = value ? value.slice() : [];
+            values.push(assetIdentifier);
+            this.handleValueChange(values);
+        } else {
+            this.handleValueChange(assetIdentifier);
+        }
     }
 
     handleChooseFile = () => {
@@ -117,26 +130,42 @@ export default class AssetEditor extends PureComponent {
     }
 
     handleUpload = files => {
-        const index = files.length;
-        const {value} = this.props;
-        const values = value ? value.slice() : [];
-
-        this.uploadFile(index, values, files);
+        this.setState({
+            isLoading: true
+        });
+        if (this.props.options.multiple) {
+            const index = files.length;
+            const {value} = this.props;
+            const values = value ? value.slice() : [];
+            this.uploadFile(index, values, files);
+        } else {
+            const {uploadAsset} = backend.get().endpoints;
+            const {siteNodePath} = this.props;
+            const siteNodeName = siteNodePath.match(/\/sites\/([^/@]*)/)[1];
+            uploadAsset(files[0], siteNodeName, 'Asset').then(res => {
+                this.handleValueChange(res.assetUuid);
+                this.setState({
+                    isLoading: false
+                });
+            });
+        }
     }
 
     uploadFile(index, values, files) {
         index--;
         const {uploadAsset} = backend.get().endpoints;
         const {siteNodePath} = this.props;
-
         const siteNodeName = siteNodePath.match(/\/sites\/([^/@]*)/)[1];
 
         if (index < 0) {
             this.handleValueChange(values);
+            this.setState({
+                isLoading: false
+            });
             return;
         }
-        uploadAsset(files[index], siteNodeName).then(res => {
-            values.push(res.object.__identity);
+        uploadAsset(files[index], siteNodeName, 'Asset').then(res => {
+            values.push(res.assetUuid);
             this.uploadFile(index, values, files);
         });
     }
@@ -149,7 +178,7 @@ export default class AssetEditor extends PureComponent {
                 onDropAccepted={this.handleUpload}
                 className={style.assetEditor}
                 >
-                <MultiSelectBox
+                {this.props.options.multiple ? (<MultiSelectBox
                     dndType={dndTypes.MULTISELECT}
                     optionValueField="identifier"
                     loadingLabel={this.props.i18nRegistry.translate('Neos.Neos:Main:loading')}
@@ -157,14 +186,28 @@ export default class AssetEditor extends PureComponent {
                     optionComponent={AssetOption}
                     placeholder={this.props.i18nRegistry.translate(this.props.placeholder)}
                     options={this.state.options || []}
-                    values={this.props.value}
+                    values={this.getValue()}
                     highlight={this.props.highlight}
                     onValuesChange={this.handleValueChange}
-                    displayLoadingIndicator={this.props.displayLoadingIndicator}
+                    displayLoadingIndicator={this.state.isLoading}
                     searchTerm={this.state.searchTerm}
                     searchOptions={this.state.searchOptions}
                     onSearchTermChange={this.handleSearchTermChange}
-                    />
+                    />) : (<SelectBox
+                        optionValueField="identifier"
+                        loadingLabel={this.props.i18nRegistry.translate('Neos.Neos:Main:loading')}
+                        displaySearchBox={true}
+                        optionComponent={AssetOption}
+                        placeholder={this.props.i18nRegistry.translate(this.props.placeholder)}
+                        options={this.state.options || []}
+                        value={this.getValue()}
+                        highlight={this.props.highlight}
+                        onValueChange={this.handleValueChange}
+                        displayLoadingIndicator={this.state.isLoading}
+                        searchTerm={this.state.searchTerm}
+                        searchOptions={this.state.searchOptions}
+                        onSearchTermChange={this.handleSearchTermChange}
+                        />)}
                 <Controls
                     onChooseFromMedia={this.handleChooseFromMedia}
                     onChooseFromLocalFileSystem={this.handleChooseFile}

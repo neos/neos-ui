3c5dda228ff934f6362bc9757d56098d1c5eee71
Dmitri Pisarev <dimaip@gmail.com>
2017-11-27 22:46:59 +0300

    BUGFIX: fix changing nodetype of a site node

--- packages/neos-ui-editors/src/Editors/NodeType/index.js
+++ packages/neos-ui-editors/src/Editors/NodeType/index.js
@@ -22,8 +22,9 @@ import style from './style.css';
             subject: focusedNodeContextPath,
             reference: focusedNodeContextPath
         });
+        const isSiteNode = $get('cr.nodes.siteNode', state) === focusedNodeContextPath;
 
-        return {focusedNode, allowedSiblingNodeTypesForFocusedNode};
+        return {focusedNode, allowedSiblingNodeTypesForFocusedNode, isSiteNode};
     };
 })
 export default class NodeType extends PureComponent {
@@ -32,6 +33,7 @@ export default class NodeType extends PureComponent {
         commit: PropTypes.func.isRequired,
         highlight: PropTypes.bool,
 
+        isSiteNode: PropTypes.bool,
         focusedNode: PropTypes.object,
         allowedSiblingNodeTypesForFocusedNode: PropTypes.array,
         nodeTypesRegistry: PropTypes.object.isRequired,
@@ -49,14 +51,16 @@ export default class NodeType extends PureComponent {
     }
 
     render() {
-        const {value, commit, nodeTypesRegistry, allowedSiblingNodeTypesForFocusedNode, i18nRegistry, highlight, focusedNode} = this.props;
+        const {value, commit, nodeTypesRegistry, allowedSiblingNodeTypesForFocusedNode, i18nRegistry, highlight, focusedNode, isSiteNode} = this.props;
 
         // auto-created child nodes cannot change type
         if ($get('isAutoCreated', focusedNode) === true) {
             return this.renderNoOptionsAvailable();
         }
 
-        const options = allowedSiblingNodeTypesForFocusedNode
+        // Show all nodetypes if is site root node
+        const rawOptions = isSiteNode ? nodeTypesRegistry.getSubTypesOf(nodeTypesRegistry.getRole('document')) : allowedSiblingNodeTypesForFocusedNode;
+        const options = rawOptions
             // Filter out system nodetypes (i.e. without groups)
             // ToDo: move this logic to some more generic place, maybe nodeTypesRegistry
             .filter(nodeType => $get('ui.group', nodeTypesRegistry.get(nodeType)))

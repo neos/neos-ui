2acefdd19d4cfaf5dd60a5fd8154762192d926de
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-11-28 16:03:58 +0100

    BUGFIX: Remove beforeunload handling from content canvas

--- packages/neos-ui/src/Containers/ContentCanvas/index.js
+++ packages/neos-ui/src/Containers/ContentCanvas/index.js
@@ -132,7 +132,7 @@ export default class ContentCanvas extends PureComponent {
     }
 
     handleFrameAccess = iframe => {
-        const {startLoading, requestRegainControl, requestLogin} = this.props;
+        const {requestRegainControl, requestLogin} = this.props;
 
         try {
             if (iframe) {
@@ -145,19 +145,6 @@ export default class ContentCanvas extends PureComponent {
                     requestLogin();
                     return;
                 }
-
-                iframe.contentWindow.addEventListener('beforeunload', event => {
-                    //
-                    // If we cannot guess the link that is responsible for
-                    // the unload, we should better hide the frame, until we're
-                    // sure that it ends up in a consistent state.
-                    //
-                    if (!event.target.activeElement.getAttribute('href')) {
-                        this.setState({isVisible: false});
-                    }
-
-                    startLoading();
-                });
 
                 this.setState({
                     isVisible: true,

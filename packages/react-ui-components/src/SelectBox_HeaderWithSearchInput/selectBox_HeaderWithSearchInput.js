91b98015b3054cfc970451a59175ba8d2b3b4f90
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2017-11-30 13:45:48 +0100

    TASK: adjust CSS

--- packages/react-ui-components/src/SelectBox_HeaderWithSearchInput/selectBox_HeaderWithSearchInput.js
+++ packages/react-ui-components/src/SelectBox_HeaderWithSearchInput/selectBox_HeaderWithSearchInput.js
@@ -4,22 +4,28 @@ import mergeClassNames from 'classnames';
 
 const Fragment = props => props.children;
 
-// TODO: document component usage && check code in detail
+/**
+ * **SelectBox_HeaderWithSearchInput is an internal implementation detail of SelectBox**, meant to improve code quality.
+ * 
+ * It is used inside SelectBox as the header component when no value is selected, and a filter/search box is shown.
+ */
 export default class SelectBox_HeaderWithSearchInput extends PureComponent {
     static propTypes = {
+        // For explanations of the PropTypes, see SelectBox.js
         searchTerm: PropTypes.string.isRequired,
         onSearchTermChange: PropTypes.func.isRequired,
         placeholder: PropTypes.string.isRequired,
         displayLoadingIndicator: PropTypes.bool,
         setFocus: PropTypes.bool,
 
+        /*****************************
+         * Theme & Dependencies
+         *****************************/
         theme: PropTypes.shape({
-            selectBox__searchInputContainer: PropTypes.string.isRequired,
-            selectBox__searchInput: PropTypes.string.isRequired,
-            selectBox__loadingIcon: PropTypes.string.isRequired
+            selectBoxHeaderWithSearchInput__inputContainer: PropTypes.string.isRequired,
+            selectBoxHeaderWithSearchInput__input: PropTypes.string.isRequired,
+            selectBoxHeaderWithSearchInput__loader: PropTypes.string.isRequired
         }).isRequired,
-
-        // dependency injection
         Icon: PropTypes.any.isRequired,
         TextInput: PropTypes.any.isRequired
     }
@@ -42,8 +48,8 @@ export default class SelectBox_HeaderWithSearchInput extends PureComponent {
                     icon="search"
                     />
                 <TextInput
-                    containerClassName={theme.selectBox__searchInputContainer}
-                    className={theme.selectBox__searchInput}
+                    containerClassName={theme.selectBoxHeaderWithSearchInput__inputContainer}
+                    className={theme.selectBoxHeaderWithSearchInput__input}
                     value={searchTerm}
                     onChange={onSearchTermChange}
                     placeholder={placeholder}
@@ -51,7 +57,7 @@ export default class SelectBox_HeaderWithSearchInput extends PureComponent {
                     type="search"
                     />
                 {displayLoadingIndicator ?
-                    <Icon className={theme.selectBox__loadingIcon} spin={true} icon="spinner"/> :
+                    <Icon className={theme.selectBoxHeaderWithSearchInput__loader} spin={true} icon="spinner"/> :
                     null
                 }
             </Fragment>

8800a48f719125dc3a281655d0d416d58dca8e3f
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2017-12-05 15:17:07 +0100

    FEATURE: make reducers configurable through Manifest

---
+++ packages/neos-ui/src/DelegatingReducer.js
@@ -0,0 +1,24 @@
+let _innerReducer = null;
+
+export default class DelegatingReducer {
+
+    reducer() {
+        return (state, action) => {
+            if (action.type === '@@INIT') {
+                return state;
+            }
+            if (!_innerReducer) {
+                console.error(action);
+                throw new Error("NO inner reducer supplied so far, but received action just shown");
+            }
+            return _innerReducer(state, action);
+        }
+    }
+
+    setReducer(reducer) {
+        if (_innerReducer) {
+            throw new Error("Inner Reducer not allowed to be changed");
+        }
+        _innerReducer = reducer;
+    }
+}

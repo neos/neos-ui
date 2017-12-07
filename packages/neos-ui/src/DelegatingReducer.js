5ea38c5a5593aa8814b1917f0adce9dbc8a49c8d
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2017-12-07 12:35:51 +0100

    BUGFIX: fix e2e tests

--- packages/neos-ui/src/DelegatingReducer.js
+++ packages/neos-ui/src/DelegatingReducer.js
@@ -4,12 +4,11 @@ export default class DelegatingReducer {
 
     reducer() {
         return (state, action) => {
-            if (action.type === '@@INIT') {
+            if (action.type === '@@INIT' || action.type === '@@redux/INIT') {
                 return state;
             }
             if (!_innerReducer) {
-                console.error(action);
-                throw new Error('NO inner reducer supplied so far, but received action just shown');
+                throw new Error('NO inner reducer supplied so far, but received action ' + action.type);
             }
             return _innerReducer(state, action);
         };

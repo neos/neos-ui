c8328f40ff8f6e8ae28b9fba80601ca5fd944d0c
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2017-12-07 08:22:27 +0100

    TASK: fix eslint errors

--- packages/neos-ui/src/DelegatingReducer.js
+++ packages/neos-ui/src/DelegatingReducer.js
@@ -9,15 +9,15 @@ export default class DelegatingReducer {
             }
             if (!_innerReducer) {
                 console.error(action);
-                throw new Error("NO inner reducer supplied so far, but received action just shown");
+                throw new Error('NO inner reducer supplied so far, but received action just shown');
             }
             return _innerReducer(state, action);
-        }
+        };
     }
 
     setReducer(reducer) {
         if (_innerReducer) {
-            throw new Error("Inner Reducer not allowed to be changed");
+            throw new Error('Inner Reducer not allowed to be changed');
         }
         _innerReducer = reducer;
     }

bdb1cf4dd46088ebb4719255ef7ee7073658f119
Markus Günther <markus.guenther@noerdisch.de>
2018-01-19 23:49:47 +0100

    TASK: Fix linting issues

--- packages/utils-helpers/src/isUri.js
+++ packages/utils-helpers/src/isUri.js
@@ -1,6 +1,6 @@
 const isUri = uri => {
-  const trimedUri = String(uri).trim();
-  return trimedUri === '' ? false : Boolean(trimedUri.match('^(https?://|news://|ftp://|mailto:|tel:|#)'));
+    const trimedUri = String(uri).trim();
+    return trimedUri === '' ? false : Boolean(trimedUri.match('^(https?://|news://|ftp://|mailto:|tel:|#)'));
 };
 
 export {isUri};

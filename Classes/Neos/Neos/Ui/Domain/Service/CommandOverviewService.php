8bbecac7d544cf64066b0dd31d20c8c5e6ea40d4
Max Strübing <mxstrbng@gmail.com>
2018-03-01 14:20:29 +0100

    TASK: Add CommandOverviewService

---
+++ Classes/Neos/Neos/Ui/Domain/Service/CommandOverviewService.php
@@ -0,0 +1,61 @@
+<?php
+namespace Neos\Neos\Ui\Domain\Service;
+
+/*
+ * This file is part of the Neos.Neos.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\ContentRepository\Domain\Context\ContentStream\ContentStreamCommandHandler;
+use Neos\ContentRepository\Domain\Projection\Workspace\WorkspaceFinder;
+use Neos\EventSourcing\EventStore\EventStoreManager;
+use Neos\ContentRepository\Domain\ValueObject\WorkspaceName;
+use Neos\Flow\Annotations as Flow;
+
+class CommandOverviewService
+{
+    /**
+     * @Flow\Inject
+     * @var EventStoreManager
+     */
+    protected $eventStoreManager;
+
+    /**
+     * @Flow\Inject
+     * @var WorkspaceFinder
+     */
+    protected $workspaceFinder;
+
+    public function getUnpublishedCommandsForCurrentWorkspace(): array
+    {
+        // Get the current workspace
+        $personalWorkspaceName = $this->userService->getPersonalWorkspaceName();
+        $workspace = $this->workspaceFinder->findOneByName(new WorkspaceName($personalWorkspaceName));
+
+        // Get the workspaceContentStream
+        $workspaceContentStreamName = ContentStreamCommandHandler::getStreamNameForContentStream($workspace->getCurrentContentStreamIdentifier());
+        $eventStore = $this->eventStoreManager->getEventStoreForStreamName($workspaceContentStreamName);
+        $workspaceContentStream = $eventStore->get(new StreamNameFilter($workspaceContentStreamName));
+
+        // Build array to show in frontend
+        $unpublishedCommands = [];
+
+        foreach ($workspaceContentStream as $eventAndRawEvent) {
+            $identifier = $eventAndRawEvent->getSequenceNumber();
+            $unpublishedCommands[$identifier]['type'] = $eventAndRawEvent->gettype();
+
+            $metadata = $eventAndRawEvent->getRawEvent()->getMetadata();
+            if (isset($metadata['commandClass'])) {
+                $unpublishedCommands[$identifier]['payload'] = $metadata['commandPayload'];
+            }
+        }
+
+        return $unpublishedCommands;
+    }
+}
+

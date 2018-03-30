c89890ead0bbc11210075eeca6dbaf0cc3906955
Dmitri Pisarev <dimaip@gmail.com>
2018-03-30 17:01:35 +0300

    BUGFIX: don't allow to publish/discard while publishing/discarding/saving

--- packages/neos-ui/src/Containers/PrimaryToolbar/PublishDropDown/index.js
+++ packages/neos-ui/src/Containers/PrimaryToolbar/PublishDropDown/index.js
@@ -90,6 +90,7 @@ export default class PublishDropDown extends PureComponent {
             publishableNodes,
             publishableNodesInDocument,
             isSaving,
+            isPublishing,
             isAutoPublishingEnabled,
             isWorkspaceReadOnly,
             toggleAutoPublishing,
@@ -102,8 +103,8 @@ export default class PublishDropDown extends PureComponent {
         const workspaceModuleUri = $get('routes.core.modules.workspaces', neos);
         const allowedWorkspaces = $get('configuration.allowedTargetWorkspaces', neos);
         const baseWorkspaceTitle = $get([baseWorkspace, 'title'], allowedWorkspaces);
-        const canPublishLocally = publishableNodesInDocument && (publishableNodesInDocument.count() > 0);
-        const canPublishGlobally = publishableNodes && (publishableNodes.count() > 0);
+        const canPublishLocally = !isSaving && !isPublishing && publishableNodesInDocument && (publishableNodesInDocument.count() > 0);
+        const canPublishGlobally = !isSaving && !isPublishing && publishableNodes && (publishableNodes.count() > 0);
         const changingWorkspaceAllowed = !canPublishGlobally;
         const autoPublishWrapperClassNames = mergeClassNames({
             [style.dropDown__item]: true,

78cc095311100e4e9e22545e0cb0cd06ca64916e
heilmann <heilmann@mindscreen.de>
2018-04-11 12:06:32 +0200

    BUGFIX: Remove obsolete hotkey registry

--- packages/neos-ui/src/Registry/index.js
+++ packages/neos-ui/src/Registry/index.js
@@ -1,7 +1,5 @@
 import RichTextToolbarRegistry from './RichTextToolbarRegistry';
-import HotkeyRegistry from './HotkeyRegistry';
 
 export {
-    RichTextToolbarRegistry,
-    HotkeyRegistry
+    RichTextToolbarRegistry
 };

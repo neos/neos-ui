cb107bf05ff1ab4ab3970032184ecad9058bb62b
Max Strübing <mxstrbng@gmail.com>
2018-04-28 12:45:05 +0200

    FEATURE: Read keyboardShortcuts from Settings.yaml

--- packages/neos-ui/src/Sagas/UI/Hotkeys/index.js
+++ packages/neos-ui/src/Sagas/UI/Hotkeys/index.js
@@ -1,20 +1,38 @@
 import Mousetrap from 'mousetrap';
 import {take, race} from 'redux-saga/effects';
-import {actionTypes} from '@neos-project/neos-ui-redux-store';
+import {actionTypes, actions} from '@neos-project/neos-ui-redux-store';
 import {getGuestFrameDocument} from '@neos-project/neos-ui-guest-frame/src/dom';
 
 export function * handleHotkeys({globalRegistry, store}) {
     yield take(actionTypes.System.READY);
 
-    const hotkeyRegistry = globalRegistry.get('hotkeys');
-    const items = hotkeyRegistry.getAllAsList();
+    const keyboardShortcuts = globalRegistry.get('frontendConfiguration').get('keyboardShortcuts');
     let mousetrapPaused = false;
 
-    items.forEach(item => {
-        Mousetrap.bind(item.keys, () => {
-            store.dispatch(item.action());
-        });
-    });
+    const getActualAction = actionAsString => {
+        const actionStringParts = actionAsString.split('.');
+
+        const actualAction = actionStringParts.reduce((acc, curr) => {
+            if (acc[curr]) {
+                return acc[curr];
+            }
+
+            return null;
+        }, actions);
+
+        return actualAction;
+    };
+
+    for (const keyboardShortcut in keyboardShortcuts) {
+        if (Object.prototype.hasOwnProperty.call(keyboardShortcuts, keyboardShortcut)) {
+            const shortcut = keyboardShortcuts[keyboardShortcut];
+            const actualAction = getActualAction(shortcut.action.substr(shortcut.action.indexOf('.') + 1));
+
+            Mousetrap.bind(shortcut.keys, () => {
+                store.dispatch(actualAction());
+            });
+        }
+    }
 
     while (true) {
         const waitForGuestFrameInteraction = yield race([
@@ -26,13 +44,21 @@ export function * handleHotkeys({globalRegistry, store}) {
         // Bind mousetrap to guest frame after content canvas stopped loading
         if (action.type === actionTypes.UI.ContentCanvas.STOP_LOADING) {
             const mousetrapGuest = new Mousetrap(getGuestFrameDocument());
-            items.forEach(item => {
-                mousetrapGuest.bind(item.keys, () => {
-                    if (mousetrapPaused === false) {
-                        store.dispatch(item.action());
-                    }
-                });
-            });
+
+            for (const keyboardShortcut in keyboardShortcuts) {
+                if (Object.prototype.hasOwnProperty.call(keyboardShortcuts, keyboardShortcut)) {
+                    const shortcut = keyboardShortcuts[keyboardShortcut];
+                    mousetrapGuest.bind(shortcut.keys, () => {
+                        if (mousetrapPaused === false) {
+                            const actualAction = getActualAction(shortcut.action.substr(shortcut.action.indexOf('.') + 1));
+
+                            Mousetrap.bind(shortcut.keys, () => {
+                                store.dispatch(actualAction());
+                            });
+                        }
+                    });
+                }
+            }
         }
 
         // Pause mousetrap during inline editing

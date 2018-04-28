4a171e54f3a8c20eb34dd9efd863150deafca5b8
Max Strübing <mxstrbng@gmail.com>
2018-04-28 14:06:32 +0200

    TASK: Throw flashmessage when action does not exists and minimize code

--- packages/neos-ui/src/Sagas/UI/Hotkeys/index.js
+++ packages/neos-ui/src/Sagas/UI/Hotkeys/index.js
@@ -1,5 +1,5 @@
 import Mousetrap from 'mousetrap';
-import {take, race} from 'redux-saga/effects';
+import {take, race, put} from 'redux-saga/effects';
 import {actionTypes, actions} from '@neos-project/neos-ui-redux-store';
 import {getGuestFrameDocument} from '@neos-project/neos-ui-guest-frame/src/dom';
 
@@ -9,29 +9,27 @@ export function * handleHotkeys({globalRegistry, store}) {
     const keyboardShortcuts = globalRegistry.get('frontendConfiguration').get('keyboardShortcuts');
     let mousetrapPaused = false;
 
-    const getActualAction = actionAsString => {
-        const actionStringParts = actionAsString.split('.');
-
-        const actualAction = actionStringParts.reduce((acc, curr) => {
-            if (acc[curr]) {
-                return acc[curr];
-            }
-
-            return null;
-        }, actions);
-
-        return actualAction;
-    };
-
-    for (const keyboardShortcut in keyboardShortcuts) {
-        if (Object.prototype.hasOwnProperty.call(keyboardShortcuts, keyboardShortcut)) {
-            const shortcut = keyboardShortcuts[keyboardShortcut];
-            const actualAction = getActualAction(shortcut.action.substr(shortcut.action.indexOf('.') + 1));
-
-            Mousetrap.bind(shortcut.keys, () => {
-                store.dispatch(actualAction());
-            });
+    // We get the action as a string from settings
+    // and this needs to be converted into a real action
+    const getActualAction = actionAsString => actionAsString.split('.').reduce((action, actionStringPart) => {
+        if (action && actionStringPart in action) {
+            return action[actionStringPart];
         }
+
+        return null;
+    }, actions);
+
+    for (const keyboardShortcut of Object.values(keyboardShortcuts)) {
+        const actualAction = getActualAction(keyboardShortcut.action.substr(keyboardShortcut.action.indexOf('.') + 1));
+
+        if (actualAction === null) {
+            yield put(actions.UI.FlashMessages.add('configuration error', 'You configured a keyboardShortcut which action doesnt exists', 'error'));
+            continue;
+        }
+
+        Mousetrap.bind(keyboardShortcut.keys, () => {
+            store.dispatch(actualAction());
+        });
     }
 
     while (true) {
@@ -45,19 +43,23 @@ export function * handleHotkeys({globalRegistry, store}) {
         if (action.type === actionTypes.UI.ContentCanvas.STOP_LOADING) {
             const mousetrapGuest = new Mousetrap(getGuestFrameDocument());
 
-            for (const keyboardShortcut in keyboardShortcuts) {
-                if (Object.prototype.hasOwnProperty.call(keyboardShortcuts, keyboardShortcut)) {
-                    const shortcut = keyboardShortcuts[keyboardShortcut];
-                    mousetrapGuest.bind(shortcut.keys, () => {
-                        if (mousetrapPaused === false) {
-                            const actualAction = getActualAction(shortcut.action.substr(shortcut.action.indexOf('.') + 1));
+            for (const keyboardShortcut of Object.values(keyboardShortcuts)) {
+                mousetrapGuest.bind(keyboardShortcut.keys, () => {
+                    if (mousetrapPaused === false) {
+                        // strip the preceding 'action.' from string
+                        // as we return null if the action is not there we didn't need to check this further
+                        const actualAction = getActualAction(keyboardShortcut.action.substr(keyboardShortcut.action.indexOf('.') + 1));
 
-                            Mousetrap.bind(shortcut.keys, () => {
-                                store.dispatch(actualAction());
-                            });
+                        if (actualAction === null) {
+                            store.dispatch(actions.UI.FlashMessages.add('configuration error', 'You configured a keyboardShortcut which action doesnt exists', 'error'));
+                            return;
                         }
-                    });
-                }
+
+                        Mousetrap.bind(keyboardShortcut.keys, () => {
+                            store.dispatch(actualAction());
+                        });
+                    }
+                });
             }
         }
 

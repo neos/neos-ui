bfc55fb6ee73556ee170b848a3ce44c2e9dde74d
Max Strübing <mxstrbng@gmail.com>
2018-06-29 14:05:23 +0200

    TASK: Implement ButtongroupItem as stateless component

--- packages/react-ui-components/src/ButtonGroup/buttonGroupItem.js
+++ packages/react-ui-components/src/ButtonGroup/buttonGroupItem.js
@@ -1,26 +1,21 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import omit from 'lodash.omit';
 
-export default class ButtonGroupItem extends PureComponent {
-    static propTypes = {
-        id: PropTypes.string.isRequired,
-        onClick: PropTypes.func.isRequired,
-        element: PropTypes.any.isRequired
-    };
+const ButtonGroupItem = props => {
+    const {element, ...restProps} = props;
+    const rest = omit(restProps, ['onClick']);
+    return (
+        React.cloneElement(element, {
+            ...rest,
+            onClick: this.handleButtonClick
+        })
+    );
+};
+ButtonGroupItem.propTypes = {
+    id: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    element: PropTypes.any.isRequired
+};
 
-    handleButtonClick = () => {
-        this.props.onClick(this.props.id);
-    }
-
-    render() {
-        const {element, ...restProps} = this.props;
-        const rest = omit(restProps, ['onClick']);
-        return (
-            React.cloneElement(element, {
-                ...rest,
-                onClick: this.handleButtonClick
-            })
-        );
-    }
-}
+export default ButtonGroupItem;

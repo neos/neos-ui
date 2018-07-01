fdab6ee4700d1a347a5fb91c358e94e1fb718606
Dmitri Pisarev <dimaip@gmail.com>
2018-07-01 10:30:25 +0300

    Debug test

--- Build/TravisCi/InstallScripts/IntegrationTests.sh
+++ Build/TravisCi/InstallScripts/IntegrationTests.sh
@@ -59,6 +59,7 @@ cd Packages/Application/Neos.Neos.Ui
 # Since all environments depend on the node dependencies, install and
 # afterwards prune them to remove extranous packages from previous/cached runs.
 make install
+ls node_modules
 make build-production
 
 # Deactivate the previous enabled handling of hidden files with the `mv` command.

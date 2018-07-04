98b07a50e6a85a2efcfcc6e802a17c66068c703a
Max Strübing <mxstrbng@gmail.com>
2018-07-04 09:54:46 +0200

    TASK: Convert SelectBox_Header to statelessComponent

--- packages/react-ui-components/src/SelectBox_Header/selectBox_Header.js
+++ packages/react-ui-components/src/SelectBox_Header/selectBox_Header.js
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase, react/jsx-pascal-case */
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 /**
@@ -7,82 +7,81 @@ import PropTypes from 'prop-types';
  *
  * It is used inside SelectBox as the header component which displays the currently selected value.
  */
-export default class SelectBox_Header extends PureComponent {
-    static propTypes = {
-        // API with SelectBox
-        option: PropTypes.shape({
-            icon: PropTypes.string,
-            label: PropTypes.string.isRequired
-        }),
-        placeholder: PropTypes.string,
-        placeholderIcon: PropTypes.string,
-        showResetButton: PropTypes.bool.isRequired,
-        onReset: PropTypes.func,
-        displayLoadingIndicator: PropTypes.bool,
-        disabled: PropTypes.bool,
+const SelectBox_Header = props => {
+    const {
+        option,
+        showResetButton,
+        theme,
+        IconButton,
+        placeholder,
+        placeholderIcon,
+        displayLoadingIndicator,
+        Icon,
+        ListPreviewElement,
+        disabled
+    } = props;
 
-        /* ------------------------------
-         * Theme & Dependencies
-         * ------------------------------ */
-        theme: PropTypes.shape({
-            selectBoxHeader: PropTypes.string.isRequired,
-            selectBoxHeader__icon: PropTypes.string.isRequired,
-            selectBoxHeader__label: PropTypes.string.isRequired
-        }).isRequired,
-        Icon: PropTypes.any.isRequired,
-        IconButton: PropTypes.any.isRequired,
-        ListPreviewElement: PropTypes.any.isRequired
-    }
+    const label = option ? option.label : placeholder;
+    const icon = option && option.icon ? option.icon : placeholderIcon;
+    const onClick = event => disabled ? null : props.onReset(event);
 
-    render() {
-        const {
-            option,
-            showResetButton,
-            theme,
-            IconButton,
-            placeholder,
-            placeholderIcon,
-            displayLoadingIndicator,
-            Icon,
-            ListPreviewElement,
-            disabled
-        } = this.props;
+    const resetButton = () => {
+        if (showResetButton) {
+            return (
+                <span>
+                    <IconButton className={theme.selectBoxHeader__icon} disabled={disabled} icon="times" onClick={onClick}/>
+                    <span className={theme.selectBoxHeader__seperator}/>
+                </span>
+            );
+        }
 
-        const label = option ? option.label : placeholder;
-        const icon = option && option.icon ? option.icon : placeholderIcon;
-        const onClick = event => disabled ? null : this.props.onReset(event);
+        return '';
+    };
 
-        const resetButton = () => {
-            if (showResetButton) {
-                return (
-                    <span>
-                        <IconButton className={theme.selectBoxHeader__icon} disabled={disabled} icon="times" onClick={onClick}/>
-                        <span className={theme.selectBoxHeader__seperator}/>
-                    </span>
-                );
-            }
+    return (
+        <div className={theme.selectBoxHeader}>
+            {displayLoadingIndicator ? (
+                <span className={theme.selectBoxHeader__wrapperIconWrapper}>
+                    <Icon className={theme.selectBoxHeader__icon} spin={true} icon="spinner"/>
+                </span>
+            ) : (
+                <div className={theme.selectBoxHeader__innerPreview}>
+                    {option ? <ListPreviewElement
+                        {...props}
+                        label={label}
+                        icon={icon}
+                        disabled={disabled}
+                        /> : <div className={theme.selectBoxHeader__label}>{label}</div>}
+                </div>
+            )}
+            {resetButton()}
+        </div>
+    );
+};
+SelectBox_Header.propTypes = {
+    // API with SelectBox
+    option: PropTypes.shape({
+        icon: PropTypes.string,
+        label: PropTypes.string.isRequired
+    }),
+    placeholder: PropTypes.string,
+    placeholderIcon: PropTypes.string,
+    showResetButton: PropTypes.bool.isRequired,
+    onReset: PropTypes.func,
+    displayLoadingIndicator: PropTypes.bool,
+    disabled: PropTypes.bool,
 
-            return '';
-        };
+    /* ------------------------------
+     * Theme & Dependencies
+     * ------------------------------ */
+    theme: PropTypes.shape({
+        selectBoxHeader: PropTypes.string.isRequired,
+        selectBoxHeader__icon: PropTypes.string.isRequired,
+        selectBoxHeader__label: PropTypes.string.isRequired
+    }).isRequired,
+    Icon: PropTypes.any.isRequired,
+    IconButton: PropTypes.any.isRequired,
+    ListPreviewElement: PropTypes.any.isRequired
+};
 
-        return (
-            <div className={theme.selectBoxHeader}>
-                {displayLoadingIndicator ? (
-                    <span className={theme.selectBoxHeader__wrapperIconWrapper}>
-                        <Icon className={theme.selectBoxHeader__icon} spin={true} icon="spinner"/>
-                    </span>
-                ) : (
-                    <div className={theme.selectBoxHeader__innerPreview}>
-                        {option ? <ListPreviewElement
-                            {...this.props}
-                            label={label}
-                            icon={icon}
-                            disabled={disabled}
-                            /> : <div className={theme.selectBoxHeader__label}>{label}</div>}
-                    </div>
-                )}
-                {resetButton()}
-            </div>
-        );
-    }
-}
+export default SelectBox_Header;

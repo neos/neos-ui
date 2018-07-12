fe40e0f46254d45c945c2b1bea3b46fdc3af58a3
Dmitri Pisarev <dimaip@gmail.com>
2018-07-12 23:10:07 +0300

    Adjust checkPropTypes.js

--- Tests/checkPropTypes.js
+++ Tests/checkPropTypes.js
@@ -2,7 +2,7 @@ import {t} from 'testcafe';
 
 export default async function () {
     const {error} = await t.getBrowserConsoleMessages();
-    if (error) {
+    if (error[0]) {
         console.log('These console errors were the cause of the failed test:', error);
     }
     await t.expect(error[0]).notOk();

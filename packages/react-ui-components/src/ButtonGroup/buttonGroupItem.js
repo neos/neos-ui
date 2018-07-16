2b78748139163d88feb0753ed37e7e1dd82c1552
Max Strübing <mxstrbng@gmail.com>
2018-07-16 06:29:32 +0200

    TASK: Convert ButtonGroupItem to PureComponent

--- packages/react-ui-components/src/ButtonGroup/buttonGroupItem.js
+++ packages/react-ui-components/src/ButtonGroup/buttonGroupItem.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import omit from 'lodash.omit';
 
-const ButtonGroupItem = props => {
-    const handleButtonClick = () => {
-        props.onClick(props.id);
+class ButtonGroupItem extends PureComponent {
+    static propTypes = {
+        id: PropTypes.string.isRequired,
+        onClick: PropTypes.func.isRequired,
+        element: PropTypes.any.isRequired
     };
 
-    const {element, ...restProps} = props;
-    const rest = omit(restProps, ['onClick']);
-    return (
-        React.cloneElement(element, {
-            ...rest,
-            onClick: handleButtonClick
-        })
-    );
-};
-ButtonGroupItem.propTypes = {
-    id: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
-    element: PropTypes.any.isRequired
-};
+    handleButtonClick() {
+        const {onClick, id} = this.props;
+        onClick(id);
+    }
+
+    render() {
+        const {element, ...restProps} = this.props;
+        const rest = omit(restProps, ['onClick']);
+        return (
+            React.cloneElement(element, {
+                ...rest,
+                onClick: this.handleButtonClick
+            })
+        );
+    }
+}
 
 export default ButtonGroupItem;

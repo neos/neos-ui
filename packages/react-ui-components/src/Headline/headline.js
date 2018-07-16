b2e62af10a32c61ab2d7ae767b90633c97db2824
Max Strübing <mxstrbng@gmail.com>
2018-07-16 07:03:00 +0200

    TASK: Convert Headline to PureComponent

--- packages/react-ui-components/src/Headline/headline.js
+++ packages/react-ui-components/src/Headline/headline.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import mergeClassNames from 'classnames';
 
@@ -11,81 +11,84 @@ const types = [
     'h6'
 ];
 
-const Headline = props => {
-    const {
-        type,
-        className,
-        children,
-        theme,
-        ...rest
-    } = props;
-    const classNames = mergeClassNames({
-        [theme.heading]: true,
-        [theme['heading--h1']]: true,
-        [className]: className && className.length
-    });
-    let heading;
+class Headline extends PureComponent {
+    static propTypes = {
+        /**
+        * The contents to be rendered.
+        */
+        children: PropTypes.any.isRequired,
 
-    switch (type) {
-        case 'h1':
-            heading = <h1 {...rest} className={classNames}>{children}</h1>;
-            break;
+        /**
+         * The semantic tag type of the headline.
+         */
+        type: PropTypes.oneOf(types).isRequired,
 
-        case 'h2':
-            heading = <h2 {...rest} className={classNames}>{children}</h2>;
-            break;
+        /**
+         * Optional style identifier, this enables the possibility to diff the
+         * semantic value of the UI to the displayed style.
+         */
+        style: PropTypes.oneOf(types),
 
-        case 'h3':
-            heading = <h3 {...rest} className={classNames}>{children}</h3>;
-            break;
+        /**
+         * An optional `className` to attach to the wrapper.
+         */
+        className: PropTypes.string,
 
-        case 'h4':
-            heading = <h4 {...rest} className={classNames}>{children}</h4>;
-            break;
+        /**
+        * An optional css theme to be injected.
+        */
+        theme: PropTypes.shape({
+            'heading': PropTypes.string,
+            'heading--h1': PropTypes.string
+        }).isRequired
+    };
+    static defaultProps = {
+        type: 'h1'
+    };
 
-        case 'h5':
-            heading = <h5 {...rest} className={classNames}>{children}</h5>;
-            break;
+    render() {
+        const {
+            type,
+            className,
+            children,
+            theme,
+            ...rest
+        } = this.props;
+        const classNames = mergeClassNames({
+            [theme.heading]: true,
+            [theme['heading--h1']]: true,
+            [className]: className && className.length
+        });
+        let heading;
 
-        default:
-            heading = <h6 {...rest} className={classNames}>{children}</h6>;
-            break;
+        switch (type) {
+            case 'h1':
+                heading = <h1 {...rest} className={classNames}>{children}</h1>;
+                break;
+
+            case 'h2':
+                heading = <h2 {...rest} className={classNames}>{children}</h2>;
+                break;
+
+            case 'h3':
+                heading = <h3 {...rest} className={classNames}>{children}</h3>;
+                break;
+
+            case 'h4':
+                heading = <h4 {...rest} className={classNames}>{children}</h4>;
+                break;
+
+            case 'h5':
+                heading = <h5 {...rest} className={classNames}>{children}</h5>;
+                break;
+
+            default:
+                heading = <h6 {...rest} className={classNames}>{children}</h6>;
+                break;
+        }
+
+        return heading;
     }
-
-    return heading;
-};
-Headline.propTypes = {
-    /**
-    * The contents to be rendered.
-    */
-    children: PropTypes.any.isRequired,
-
-    /**
-     * The semantic tag type of the headline.
-     */
-    type: PropTypes.oneOf(types).isRequired,
-
-    /**
-     * Optional style identifier, this enables the possibility to diff the
-     * semantic value of the UI to the displayed style.
-     */
-    style: PropTypes.oneOf(types),
-
-    /**
-     * An optional `className` to attach to the wrapper.
-     */
-    className: PropTypes.string,
-
-    /**
-    * An optional css theme to be injected.
-    */
-    theme: PropTypes.shape({
-        'heading': PropTypes.string,
-        'heading--h1': PropTypes.string
-    }).isRequired
-};
-Headline.defaultProps = {
-    type: 'h1'
-};
+}
 
 export default Headline;

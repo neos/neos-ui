cddcf470faefb850728cb3e8d093bcf73cab65f4
Max Strübing <mxstrbng@gmail.com>
2018-07-16 07:06:41 +0200

    TASK: Convert IconButton to PureComponent

--- packages/react-ui-components/src/IconButton/iconButton.js
+++ packages/react-ui-components/src/IconButton/iconButton.js
@@ -1,69 +1,73 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import mergeClassNames from 'classnames';
 
-const IconButton = props => {
-    const {
-        IconComponent,
-        ButtonComponent,
-        className,
-        theme,
-        icon,
-        size,
-        disabled,
-        ...rest
-    } = props;
-    const finalClassName = mergeClassNames({
-        [className]: className && className.length,
-        [theme.iconButton]: true,
-        [theme[`size-${size}`]]: true,
-        [theme['iconButton--disabled']]: disabled
-    });
+class IconButton extends PureComponent {
+    static propTypes = {
+        /**
+         * The icon key which gets passed to the Icon Component.
+         */
+        icon: PropTypes.string.isRequired,
 
-    return (
-        <ButtonComponent {...rest} size={size} className={finalClassName}>
-            <IconComponent icon={icon}/>
-        </ButtonComponent>
-    );
-};
-IconButton.propTypes = {
-    /**
-     * The icon key which gets passed to the Icon Component.
-     */
-    icon: PropTypes.string.isRequired,
+        /**
+         * An optional `className` to attach to the wrapper.
+         */
+        className: PropTypes.string,
 
-    /**
-     * An optional `className` to attach to the wrapper.
-     */
-    className: PropTypes.string,
+        /**
+         * Defines the size of the icon button.
+         */
+        size: PropTypes.oneOf(['small', 'regular']),
 
-    /**
-     * Defines the size of the icon button.
-     */
-    size: PropTypes.oneOf(['small', 'regular']),
+        /**
+        * An optional css theme to be injected.
+        */
+        theme: PropTypes.shape({/* eslint-disable quote-props */
+            'iconButton': PropTypes.string
+        }).isRequired, /* eslint-enable quote-props */
 
-    /**
-    * An optional css theme to be injected.
-    */
-    theme: PropTypes.shape({/* eslint-disable quote-props */
-        'iconButton': PropTypes.string
-    }).isRequired, /* eslint-enable quote-props */
+        /**
+         * Static component dependencies which are injected from the outside (index.js)
+         */
+        IconComponent: PropTypes.any.isRequired,
+        ButtonComponent: PropTypes.any.isRequired,
 
-    /**
-     * Static component dependencies which are injected from the outside (index.js)
-     */
-    IconComponent: PropTypes.any.isRequired,
-    ButtonComponent: PropTypes.any.isRequired,
+        /**
+         * Optional disabled flag
+         */
+        disabled: PropTypes.bool
+    };
 
-    /**
-     * Optional disabled flag
-     */
-    disabled: PropTypes.bool
-};
-IconButton.defaultProps = {
-    size: 'regular',
-    style: 'transparent',
-    hoverStyle: 'brand'
-};
+    static defaultProps = {
+        size: 'regular',
+        style: 'transparent',
+        hoverStyle: 'brand'
+    };
+
+    render() {
+        const {
+            IconComponent,
+            ButtonComponent,
+            className,
+            theme,
+            icon,
+            size,
+            disabled,
+            ...rest
+        } = this.props;
+        const finalClassName = mergeClassNames({
+            [className]: className && className.length,
+            [theme.iconButton]: true,
+            [theme[`size-${size}`]]: true,
+            [theme['iconButton--disabled']]: disabled
+        });
+
+        return (
+            <ButtonComponent {...rest} size={size} className={finalClassName}>
+                <IconComponent icon={icon}/>
+            </ButtonComponent>
+        );
+    }
+}
 
 export default IconButton;

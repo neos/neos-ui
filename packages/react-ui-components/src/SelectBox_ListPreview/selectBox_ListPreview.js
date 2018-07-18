7881106b4f5fd88ca5ae65c88bf4644bb22b9b88
Max Strübing <mxstrbng@gmail.com>
2018-07-18 07:09:08 +0200

    TASK: Convert SelectBox_ListPreview to PureComponent

--- packages/react-ui-components/src/SelectBox_ListPreview/selectBox_ListPreview.js
+++ packages/react-ui-components/src/SelectBox_ListPreview/selectBox_ListPreview.js
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase, react/jsx-pascal-case */
-import React, {Fragment} from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import SelectBox_Option_SingleLine from '../SelectBox_Option_SingleLine/index';
 
@@ -8,65 +8,68 @@ import SelectBox_Option_SingleLine from '../SelectBox_Option_SingleLine/index';
  *
  * It is used inside SelectBox as the dropdown part.
  */
-const SelectBox_ListPreview = props => {
-    const {
-        options,
-        searchTermLeftToType,
-        noMatchesFound,
-        noMatchesFoundLabel,
-        SelectBox_CreateNew,
-        SelectBox_ListPreviewFlat,
-        SelectBox_ListPreviewGrouped,
-        searchBoxLeftToTypeLabel,
-        theme
-    } = props;
+class SelectBox_ListPreview extends PureComponent {
+    static propTypes = {
+        // For explanations of the PropTypes, see SelectBox.js
+        options: PropTypes.arrayOf(
+            PropTypes.shape({
+            }),
+        ),
+        // Number of characters left to type before search
+        searchTermLeftToType: PropTypes.number,
+        noMatchesFound: PropTypes.bool,
+        searchBoxLeftToTypeLabel: PropTypes.string,
+        noMatchesFoundLabel: PropTypes.string,
+        theme: PropTypes.object,
 
-    const ListPreviewComponent = options.some(option => option.group) ? SelectBox_ListPreviewGrouped : SelectBox_ListPreviewFlat;
+        // Dependency injection
+        SelectBox_CreateNew: PropTypes.any.isRequired,
+        SelectBox_ListPreviewFlat: PropTypes.any.isRequired,
+        SelectBox_ListPreviewGrouped: PropTypes.any.isRequired
+    }
 
-    // TODO: replace horible self-made I18n replace
-    return (
-        <Fragment>
-            {searchTermLeftToType > 0 ? (
+    render() {
+        const {
+            options,
+            searchTermLeftToType,
+            noMatchesFound,
+            noMatchesFoundLabel,
+            SelectBox_CreateNew,
+            SelectBox_ListPreviewFlat,
+            SelectBox_ListPreviewGrouped,
+            searchBoxLeftToTypeLabel,
+            theme
+        } = this.props;
+
+        const ListPreviewComponent = options.some(option => option.group) ? SelectBox_ListPreviewGrouped : SelectBox_ListPreviewFlat;
+
+        // TODO: replace horible self-made I18n replace
+        return (
+            <Fragment>
+                {searchTermLeftToType > 0 ? (
+                    <div className={theme.selectBox__item}>
+                        <SelectBox_Option_SingleLine
+                            option={{label: `${searchBoxLeftToTypeLabel && searchBoxLeftToTypeLabel.replace('###CHARACTERS###', searchTermLeftToType)}`, icon: 'ellipsis-h'}}
+                            key={'___leftToType'}
+                            />
+                    </div>
+                ) : (
+                    <ListPreviewComponent {...this.props}/>
+                )}
+                {noMatchesFound && (
+                    <div className={theme.selectBox__item}>
+                        <SelectBox_Option_SingleLine
+                            option={{label: noMatchesFoundLabel, icon: 'ban'}}
+                            key={'___noResults'}
+                            />
+                    </div>
+                )}
                 <div className={theme.selectBox__item}>
-                    <SelectBox_Option_SingleLine
-                        option={{label: `${searchBoxLeftToTypeLabel && searchBoxLeftToTypeLabel.replace('###CHARACTERS###', searchTermLeftToType)}`, icon: 'ellipsis-h'}}
-                        key={'___leftToType'}
-                        />
+                    <SelectBox_CreateNew {...this.props}/>
                 </div>
-            ) : (
-                <ListPreviewComponent {...props}/>
-            )}
-            {noMatchesFound && (
-                <div className={theme.selectBox__item}>
-                    <SelectBox_Option_SingleLine
-                        option={{label: noMatchesFoundLabel, icon: 'ban'}}
-                        key={'___noResults'}
-                        />
-                </div>
-            )}
-            <div className={theme.selectBox__item}>
-                <SelectBox_CreateNew {...props}/>
-            </div>
-        </Fragment>
-    );
-};
-SelectBox_ListPreview.propTypes = {
-    // For explanations of the PropTypes, see SelectBox.js
-    options: PropTypes.arrayOf(
-        PropTypes.shape({
-        }),
-    ),
-    // Number of characters left to type before search
-    searchTermLeftToType: PropTypes.number,
-    noMatchesFound: PropTypes.bool,
-    searchBoxLeftToTypeLabel: PropTypes.string,
-    noMatchesFoundLabel: PropTypes.string,
-    theme: PropTypes.object,
-
-    // Dependency injection
-    SelectBox_CreateNew: PropTypes.any.isRequired,
-    SelectBox_ListPreviewFlat: PropTypes.any.isRequired,
-    SelectBox_ListPreviewGrouped: PropTypes.any.isRequired
-};
+            </Fragment>
+        );
+    }
+}
 
 export default SelectBox_ListPreview;

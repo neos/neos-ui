b3ed1eacaae8971d2f1178ddefc6006cf2bb4a18
Max Strübing <mxstrbng@gmail.com>
2018-07-18 07:43:45 +0200

    TASK: Convert SideBar to PureComponent

--- packages/react-ui-components/src/SideBar/sideBar.js
+++ packages/react-ui-components/src/SideBar/sideBar.js
@@ -1,52 +1,55 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import mergeClassNames from 'classnames';
 
-const SideBar = props => {
-    const {
-        position,
-        className,
-        children,
-        theme,
-        ...rest
-    } = props;
-    const classNames = mergeClassNames({
-        [className]: className && className.length,
-        [theme.sideBar]: true,
-        [theme['sideBar--left']]: position === 'left',
-        [theme['sideBar--right']]: position === 'right'
-    });
+class SideBar extends PureComponent {
+    static propTypes = {
+        /**
+         * This prop controls the absolute positioning of the SideBar.
+         */
+        position: PropTypes.oneOf(['left', 'right']).isRequired,
 
-    return (
-        <div {...rest} className={classNames}>
-            {children}
-        </div>
-    );
-};
-SideBar.propTypes = {
-    /**
-     * This prop controls the absolute positioning of the SideBar.
-     */
-    position: PropTypes.oneOf(['left', 'right']).isRequired,
+        /**
+         * An optional className to render on the div node.
+         */
+        className: PropTypes.string,
 
-    /**
-     * An optional className to render on the div node.
-     */
-    className: PropTypes.string,
+        /**
+         * The children to render within the div node.
+         */
+        children: PropTypes.any.isRequired,
 
-    /**
-     * The children to render within the div node.
-     */
-    children: PropTypes.any.isRequired,
+        /**
+         * An optional css theme to be injected.
+         */
+        theme: PropTypes.shape({// eslint-disable-line quote-props
+            'sideBar': PropTypes.string,
+            'sideBar--left': PropTypes.string,
+            'sideBar--right': PropTypes.string
+        }).isRequired
+    }
 
-    /**
-     * An optional css theme to be injected.
-     */
-    theme: PropTypes.shape({// eslint-disable-line quote-props
-        'sideBar': PropTypes.string,
-        'sideBar--left': PropTypes.string,
-        'sideBar--right': PropTypes.string
-    }).isRequired
-};
+    render() {
+        const {
+            position,
+            className,
+            children,
+            theme,
+            ...rest
+        } = this.props;
+        const classNames = mergeClassNames({
+            [className]: className && className.length,
+            [theme.sideBar]: true,
+            [theme['sideBar--left']]: position === 'left',
+            [theme['sideBar--right']]: position === 'right'
+        });
+
+        return (
+            <div {...rest} className={classNames}>
+                {children}
+            </div>
+        );
+    }
+}
 
 export default SideBar;

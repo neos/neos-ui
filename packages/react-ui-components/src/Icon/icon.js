18d4f8ddeafc807e740e051df8bebac4c931aca6
Max Strübing <mxstrbng@gmail.com>
2018-08-21 12:15:27 +0200

    TASK: Update to FontAwesome 5.2 (#2060)

--- packages/react-ui-components/src/Icon/icon.js
+++ packages/react-ui-components/src/Icon/icon.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import mergeClassNames from 'classnames';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import mapper from './mapper';
 
 class Icon extends PureComponent {

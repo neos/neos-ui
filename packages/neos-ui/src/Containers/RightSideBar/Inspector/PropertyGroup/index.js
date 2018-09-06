5a37838859c33aa5fd2046edd21d1887bf89aee4
Dennis Irmscher <dennis.irmscher@interlutions.de>
2018-09-06 13:54:11 +0200

    BUGFIX: Also takes views into account for dynamically hiding

--- packages/neos-ui/src/Containers/RightSideBar/Inspector/PropertyGroup/index.js
+++ packages/neos-ui/src/Containers/RightSideBar/Inspector/PropertyGroup/index.js
@@ -48,7 +48,10 @@ export default class PropertyGroup extends PureComponent {
                         const itemType = $get('type', item);
                         const label = $get('label', item) || '';
 
-                        if (itemType === 'editor' && $get('hidden', item) !== true) {
+                        if ($get('hidden', item) !== true) {
+                            return null;
+                        }
+                        if (itemType === 'editor') {
                             return (
                                 <InspectorEditorEnvelope
                                     key={$get('contextPath', node) + itemId}

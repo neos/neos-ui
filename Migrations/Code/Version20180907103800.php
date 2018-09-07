a403ce26514be2ed62d1eec52f81b9fb28c42089
Dmitri Pisarev <dimaip@gmail.com>
2018-09-07 17:34:32 +0300

    TASK: add legacy aloha format migration

---
+++ Migrations/Code/Version20180907103800.php
@@ -0,0 +1,94 @@
+<?php
+namespace Neos\Neos\Ui\Core\Migrations;
+
+/*
+ * This file is part of the Neos.Neos.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+use Neos\Flow\Configuration\ConfigurationManager;
+use Neos\Flow\Core\Migrations\AbstractMigration;
+
+/**
+ * Migrate RTE formatting configuration to new format
+ */
+class Version20180907103800 extends AbstractMigration
+{
+    /**
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return 'Neos.Neos-20180907103800';
+    }
+
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        $this->processConfiguration(
+            'NodeTypes',
+            function (&$configuration) {
+                foreach ($configuration as &$nodeType) {
+                    if (isset($nodeType['properties'])) {
+                        foreach ($nodeType['properties'] as &$propertyConfiguration) {
+                            if (isset($propertyConfiguration['ui']['aloha'])) {
+                                $propertyConfiguration['ui']['inline']['editorOptions'] = $this->transformAlohaFormat($propertyConfiguration['ui']['aloha']);
+                                unset($propertyConfiguration['ui']['aloha']);
+                            }
+                        }
+                    }
+                }
+            },
+            true
+        );
+    }
+
+    /**
+     * Takes legacy aloha formatting and return editorOptions
+     * 
+     * @param array $aloha
+     * @return array $editorOptions
+     */
+    protected function transformAlohaFormat($aloha)
+    {
+        $editorOptions = [
+            'formatting' => []
+        ];
+        if (isset($aloha['format']) && is_array($aloha['format'])) {
+            $editorOptions['formatting'] = array_merge($editorOptions['formatting'], $aloha['format']);
+        }
+        if (isset($aloha['table']) && is_array($aloha['table'])) {
+            $editorOptions['formatting'] = array_merge($editorOptions['formatting'], $aloha['table']);
+        }
+        if (isset($aloha['link']) && is_array($aloha['link'])) {
+            $editorOptions['formatting'] = array_merge($editorOptions['formatting'], $aloha['link']);
+        }
+        if (isset($aloha['list']) && is_array($aloha['list'])) {
+            $editorOptions['formatting'] = array_merge($editorOptions['formatting'], $aloha['list']);
+        }
+        if (isset($aloha['alignment']) && is_array($aloha['alignment'])) {
+            $editorOptions['formatting'] = array_merge($editorOptions['formatting'], $aloha['alignment']);
+        }
+        if (isset($aloha['autoparagraph'])) {
+            $editorOptions['autoparagraph'] = $aloha['autoparagraph'];
+        }
+        if (isset($aloha['placeholder'])) {
+            $editorOptions['placeholder'] = $aloha['placeholder'];
+        }
+        if (isset($editorOptions['formatting']['b'])) {
+            $editorOptions['formatting']['strong'] = true;
+            unset($editorOptions['formatting']['b']);
+        }
+        if (isset($editorOptions['formatting']['i'])) {
+            $editorOptions['formatting']['em'] = true;
+            unset($editorOptions['formatting']['i']);
+        }
+        return $editorOptions;
+    }
+}

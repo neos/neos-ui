1fef9285f064dd49cd644c5562a8fc4a55483f84
Dmitri Pisarev <dimaip@gmail.com>
2018-09-09 12:17:25 +0300

    Linter

--- packages/react-ui-components/src/Icon/icon.spec.js
+++ packages/react-ui-components/src/Icon/icon.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
-import Icon, {iconPropValidator} from './icon.js';
 
 describe('<Icon/>', () => {
     let props;

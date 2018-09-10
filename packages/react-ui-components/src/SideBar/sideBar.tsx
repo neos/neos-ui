a09f15641b1bc67b6e705d9bb510a15811075c8f
Robert Baruck <robert.baruck@sandstorm-media.de>
2018-09-10 21:12:28 +0200

    FEATURE: sidebar in typescript

---
+++ packages/react-ui-components/src/SideBar/sideBar.tsx
@@ -0,0 +1,60 @@
+import mergeClassNames from 'classnames';
+import React, {PureComponent} from 'react';
+
+type SideBarPosition = 'left' | 'right';
+
+interface ISideBarTheme {
+    readonly 'sideBar': string;
+    readonly 'sideBar--left': string;
+    readonly 'sideBar--right': string;
+}
+
+interface ISideBarProps {
+    /**
+     * This prop controls the absolute positioning of the SideBar.
+     */
+    readonly position: SideBarPosition;
+
+    /**
+     * An optional className to render on the div node.
+     */
+    readonly className?: string;
+
+    /**
+     * The children to render within the div node.
+     */
+    readonly children: any; // TODO
+
+    /**
+     * An optional css theme to be injected.
+     */
+    readonly theme?: ISideBarTheme;
+}
+
+class SideBar extends PureComponent<ISideBarProps> {
+    public render(): JSX.Element {
+        const {
+            position,
+            className,
+            children,
+            theme,
+            ...rest
+        } = this.props;
+        const classNames = mergeClassNames(
+            className,
+            theme!.sideBar,
+            {
+                [theme!['sideBar--left']]: position === 'left',
+                [theme!['sideBar--right']]: position === 'right'
+            }
+        );
+
+        return (
+            <div {...rest} className={classNames}>
+                {children}
+            </div>
+        );
+    }
+}
+
+export default SideBar;

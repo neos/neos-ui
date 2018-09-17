5719ce16d94d247fb8793318ab8a34ff85392350
Max Strübing <mxstrbng@gmail.com>
2018-09-17 06:29:09 +0200

    TASK: Remove 111:26  warning  Unnecessary return statement  no-useless-return

--- packages/neos-ui/src/Sagas/CR/NodeOperations/addNode.js
+++ packages/neos-ui/src/Sagas/CR/NodeOperations/addNode.js
@@ -107,8 +107,6 @@ export default function * addNode({globalRegistry}) {
                         }
                     }]));
                 }
-
-                default: return;
             }
         });
     });

004072f5a21781516b7c70147b0802822796f6c7
Max Strübing <mxstrbng@gmail.com>
2018-09-18 09:24:46 +0200

    TASK: reduce linter noise (#2134)

--- packages/neos-ui/src/Sagas/CR/NodeOperations/addNode.js
+++ packages/neos-ui/src/Sagas/CR/NodeOperations/addNode.js
@@ -108,7 +108,7 @@ export default function * addNode({globalRegistry}) {
                     }]));
                 }
 
-                default: return;
+                default: return null;
             }
         });
     });

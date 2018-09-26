6eed65fc771b5f993d30bcb597e3ff80301659ba
Heinz <info@h1-im.de>
2018-09-26 19:35:22 +0200

    TASK: Rework Help Message in SelectNodeType Dialog (#2105)
    * Translation and Thumbnail support
    
    * scroll if help message is hidden

--- packages/neos-ui/src/Containers/Modals/SelectNodeType/index.js
+++ packages/neos-ui/src/Containers/Modals/SelectNodeType/index.js
@@ -2,13 +2,10 @@ import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {$get} from 'plow-js';
-import ReactMarkdown from 'react-markdown';
 
 import {neos} from '@neos-project/neos-ui-decorators';
 import {actions, selectors} from '@neos-project/neos-ui-redux-store';
 
-import Icon from '@neos-project/react-ui-components/src/Icon/';
-import IconButton from '@neos-project/react-ui-components/src/IconButton/';
 import Button from '@neos-project/react-ui-components/src/Button/';
 import Dialog from '@neos-project/react-ui-components/src/Dialog/';
 import I18n from '@neos-project/neos-ui-i18n';
@@ -70,6 +67,7 @@ export default class SelectNodeType extends PureComponent {
             this.props.allowedSiblingNodeTypes,
             this.props.allowedChildNodeTypes
         ),
+        activeHelpMessageGroupPanel: '',
         showHelpMessageFor: ''
     };
 
@@ -101,6 +99,19 @@ export default class SelectNodeType extends PureComponent {
 
         apply(insertMode, nodeType);
     };
+
+    handleCloseHelpMessage = () => {
+        this.setState({
+            showHelpMessageFor: ''
+        });
+    }
+
+    handleHelpMessage = (nodeType, groupPanel) => {
+        this.setState({
+            showHelpMessageFor: nodeType === this.state.showHelpMessageFor ? '' : nodeType,
+            activeHelpMessageGroupPanel: groupPanel
+        });
+    }
 
     getAllowedNodeTypesByCurrentInsertMode() {
         const {insertMode} = this.state;
@@ -154,48 +165,8 @@ export default class SelectNodeType extends PureComponent {
 
     handleNodeTypeFilterChange = filterSearchTerm => this.setState({filterSearchTerm});
 
-    renderHelpMessage = () => {
-        const {showHelpMessageFor} = this.state;
-        const {nodeTypesRegistry} = this.props;
-
-        const nodeType = nodeTypesRegistry.getNodeType(showHelpMessageFor);
-        const message = $get('ui.help.message', nodeType);
-
-        const icon = $get('ui.icon', nodeType);
-        const label = $get('ui.label', nodeType);
-
-        return (
-            <div className={style.helpMessage__wrapper}>
-                <div className={style.helpMessage}>
-                    <span className={style.helpMessage__label}>
-                        {icon && <Icon icon={icon} className={style.nodeType__icon} padded="right"/>}
-                        <I18n id={label} fallback={label}/>
-                    </span>
-                    <ReactMarkdown source={message} />
-                </div>
-
-                <IconButton className={style.helpMessage__closeButton} icon="times" onClick={() => this.handleCloseHelpMessage()} />
-            </div>
-        );
-    }
-
-    handleCloseHelpMessage = () => {
-        this.setState({
-            showHelpMessageFor: ''
-        });
-    }
-
-    handleHelpMessage = nodeType => {
-        this.setState({
-            showHelpMessageFor: nodeType === this.state.showHelpMessageFor ? '' : nodeType
-        });
-    }
-
     render() {
         const {isOpen} = this.props;
-        const {showHelpMessageFor} = this.state;
-
-        const showHelpMessage = showHelpMessageFor !== '';
 
         if (!isOpen) {
             return null;
@@ -203,7 +174,6 @@ export default class SelectNodeType extends PureComponent {
 
         return (
             <Dialog
-                className={showHelpMessage ? style.dialog__padded : ''}
                 actions={[this.renderCancelAction()]}
                 title={[this.renderSelectNodeTypeDialogHeader()]}
                 onRequestClose={this.handleCancel}
@@ -217,11 +187,13 @@ export default class SelectNodeType extends PureComponent {
                             group={group}
                             filterSearchTerm={this.state.filterSearchTerm}
                             onSelect={this.handleApply}
+                            showHelpMessageFor ={this.state.showHelpMessageFor}
+                            activeHelpMessageGroupPanel ={this.state.activeHelpMessageGroupPanel}
                             onHelpMessage={this.handleHelpMessage}
+                            onCloseHelpMessage={this.handleCloseHelpMessage}
                             />
                     </div>
                 ))}
-                {showHelpMessage ? this.renderHelpMessage() : null}
             </Dialog>
         );
     }

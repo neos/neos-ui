431bb820ad5cbdb8a16ed51b86cf97ad6cd47369
Johannes Steu <hello@johannessteu.de>
2018-11-06 15:18:47 +0100

    Revert

--- packages/neos-ui-editors/src/Editors/Link/index.js
+++ packages/neos-ui-editors/src/Editors/Link/index.js
@@ -1,20 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import {neos} from '@neos-project/neos-ui-decorators';
-import {connect} from 'react-redux';
-import {selectors} from '@neos-project/neos-ui-redux-store';
-import {$get, $transform} from 'plow-js';
-
 import LinkInput from '@neos-project/neos-ui-editors/src/Library/LinkInput';
 
-@connect(
-    $transform({
-        focusedNodeType: selectors.CR.Nodes.focusedNodeTypeSelector
-    })
-)
-@neos(globalRegistry => ({
-    nodeTypesRegistry: globalRegistry.get('@neos-project/neos-ui-contentrepository')
-}))
 class LinkEditor extends PureComponent {
     static propTypes = {
         identifier: PropTypes.string.isRequired,
@@ -25,22 +12,19 @@ class LinkEditor extends PureComponent {
             nodeTypes: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
             placeholder: PropTypes.string,
             disabled: PropTypes.bool
-        }),
-        focusedNodeType: PropTypes.string.isRequired,
-        nodeTypesRegistry: PropTypes.object.isRequired
-    };
-
-    computeLinkOptions = () => {
-        const {focusedNodeType, identifier, nodeTypesRegistry, options} = this.props;
-        const linkingOptions = $get('linking', nodeTypesRegistry.getInlineEditorOptionsForProperty(focusedNodeType, identifier));
-
-        return Object.assign(options, linkingOptions);
+        })
     };
 
     render() {
-        const {value} = this.props;
+        const {value, options} = this.props;
 
-        return <LinkInput linkValue={value} onLinkChange={this.props.commit} options={this.computeLinkOptions()} />;
+        return (
+            <LinkInput
+                linkValue={value}
+                onLinkChange={this.props.commit}
+                options={options}
+                />
+        );
     }
 }
 

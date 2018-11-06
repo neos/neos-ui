07ade3900daed5b68fe09a143809d197b8cc37b6
Dmitri Pisarev <dimaip@gmail.com>
2018-11-06 17:16:06 +0100

    TASK: refactor System reducer to use TypeScript (#2215)

--- packages/neos-ui-redux-store/src/index.js
+++ packages/neos-ui-redux-store/src/index.js
@@ -7,8 +7,27 @@ import * as System from './System/index';
 import * as UI from './UI/index';
 import * as User from './User/index';
 import * as ServerFeedback from './ServerFeedback/index';
+import {$set, $get} from 'plow-js';
 
 const all = {Changes, CR, System, UI, User, ServerFeedback};
+
+// TODO: we'll get rid of untyped reducers soonish, this is just for transition period
+const untypedReducersSubparts = {Changes, CR, UI, User, ServerFeedback};
+const untypedReducers = map(k => untypedReducersSubparts[k].reducer, keys(untypedReducersSubparts));
+
+// We do something similar to `combineReducers` from redux here to pass a subpart of the state to a child reducer
+// Key in this object is the substate path, and the value is the reducer
+const typedReducersSubparts = {
+    system: System.reducer
+};
+const typedReducers = map(
+    k => (state, action) => $set(
+        k,
+        typedReducersSubparts[k]($get(k, state), action),
+        state
+    ),
+    keys(typedReducersSubparts)
+);
 
 //
 // Export the actionTypes
@@ -23,7 +42,7 @@ export const actions = map(a => a.actions, all);
 //
 // Export the reducer
 //
-export const reducer = handleActions(map(k => all[k].reducer, keys(all)));
+export const reducer = handleActions([...untypedReducers, ...typedReducers]);
 
 //
 // Export the selectors

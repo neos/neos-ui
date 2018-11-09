7a91cec9bb7261bb13af63e35c9f52ff56650147
Robert Baruck <robert.baruck@sandstorm-media.de>
2018-11-09 13:37:22 +0100

    TASK: fix tests

--- packages/react-ui-components/src/TextArea/textArea.spec.tsx
+++ packages/react-ui-components/src/TextArea/textArea.spec.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import toJson from 'enzyme-to-json';
 import TextareaAutosize from 'react-textarea-autosize';
 
-import TextArea, {TextAreaProps, defaultProps} from './textArea';
+import TextArea, {TextAreaProps} from './textArea';
 
 describe('<TextArea/>', () => {
-
     const props: TextAreaProps = {
-        ...defaultProps
+        theme: {
+            textArea: 'textAreaClassName',
+            'textArea--disabled': 'disabledClassName',
+        }
     };
 
     it('should render correctly.', () => {
-        const wrapper = shallow(<TextArea {...props}/>);
+        const wrapper = mount(<TextArea {...props} />);
 
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
     it('should allow the propagation of "className" with the "className" prop.', () => {
-        const wrapper = shallow(<TextArea {...props} className="fooClassName"/>);
+        const wrapper = mount(<TextArea {...props} className="fooClassName"/>);
         const input = wrapper.find(TextareaAutosize);
         expect(input.prop('className')).toContain('fooClassName');
     });
 
     it('should call the passed "onChange" prop with the value of the input when changing it.', () => {
         const onChange = jest.fn();
-        const wrapper = shallow(<TextArea {...props} onChange={onChange}/>);
+        const wrapper = mount(<TextArea {...props} onChange={onChange}/>);
         const input = wrapper.find(TextareaAutosize);
 
         input.simulate('change', {
--- packages/react-ui-components/src/TextArea/textArea.tsx
+++ packages/react-ui-components/src/TextArea/textArea.tsx
@@ -2,6 +2,7 @@ import React, {PureComponent, ChangeEvent} from 'react';
 import mergeClassNames from 'classnames';
 import TextareaAutosize from 'react-textarea-autosize';
 import enhanceWithClickOutside from 'react-click-outside';
+
 import { PickDefaultProps } from '../../types';
 
 export interface TextAreaProps {
@@ -41,30 +42,65 @@ export interface TextAreaProps {
         readonly expandedRows?: number;
 }
 
+interface TextAreaTheme {
+    readonly textArea: string;
+    readonly 'textArea--disabled': string;
+}
+
 export const defaultProps: PickDefaultProps<TextAreaProps, 'minRows' | 'expandedRows'> = {
     minRows: 2,
     expandedRows: 6
 };
 
-interface TextAreaTheme {
-    readonly textArea: string;
-    readonly 'textArea--invalid': string;
-    readonly 'textArea--disabled': string;
+interface TextAreaState {
+    readonly isFocused: boolean;
 }
 
-class TextArea extends PureComponent<TextAreaProps> {
-    public state = {
-        isFocused: false
-    };
+const intitialState: TextAreaState = {
+    isFocused: false,
+};
 
+export class TextArea extends PureComponent<TextAreaProps> {
     public static defaultProps = defaultProps;
+    public state = intitialState;
+
+    public render(): JSX.Element {
+        const {
+            placeholder,
+            className,
+            theme,
+            disabled,
+            minRows,
+            expandedRows,
+        } = this.props;
+        const classNames = mergeClassNames(
+            className,
+            theme!.textArea,
+            {
+                [theme!['textArea--disabled']]: disabled
+            }
+        );
+
+        return (
+            <TextareaAutosize
+                className={classNames}
+                role="textbox"
+                aria-multiline="true"
+                aria-disabled={disabled ? 'true' : 'false'}
+                placeholder={placeholder}
+                disabled={disabled}
+                onChange={this.handleValueChange}
+                onClick={this.handleOnClick}
+                minRows={this.state.isFocused ? expandedRows : minRows}
+            />
+        );
+    }
 
     private readonly handleValueChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        const {value} = e.target;
         const {onChange} = this.props;
 
         if (onChange) {
-            onChange(value);
+            onChange(e.target.value);
         }
     }
 
@@ -79,48 +115,9 @@ class TextArea extends PureComponent<TextAreaProps> {
             isFocused: false
         });
     }
-
-    public render(): JSX.Element {
-        const {
-            placeholder,
-            className,
-            theme,
-            disabled,
-            minRows,
-            expandedRows,
-            ...rest
-        } = this.props;
-        const classNames = mergeClassNames(
-            theme!.textArea,
-            className,
-            {
-                [theme!['textArea--disabled']]: disabled
-            }
-        );
-
-        return (
-            <div>
-                <TextareaAutosize
-                    {...rest}
-                    className={classNames}
-                    role="textbox"
-                    aria-multiline="true"
-                    aria-disabled={disabled ? 'true' : 'false'}
-                    placeholder={placeholder}
-                    disabled={disabled}
-                    onChange={this.handleValueChange}
-                    onClick={this.handleOnClick}
-                    minRows={this.state.isFocused ? expandedRows : minRows}
-                    />
-            </div>
-        );
-    }
 }
 
 //
 // Add the click-outside functionality to the TextArea component.
 //
-const EnhancedTextArea = enhanceWithClickOutside(TextArea);
-
-export default EnhancedTextArea;
-export const undecorated = TextArea;
+export default enhanceWithClickOutside(TextArea);

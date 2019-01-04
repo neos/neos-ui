ae28770cb7b2d8b3a1fb5b51ff4ff20e1a54a7f0
Robert Baruck <robert.baruck@sandstorm-media.de>
2019-01-04 12:38:54 +0100

    stage

--- packages/react-ui-components/src/Tree/node.tsx
+++ packages/react-ui-components/src/Tree/node.tsx
@@ -1,7 +1,8 @@
-import React, {PureComponent} from 'react';
+// tslint:disable:max-classes-per-file
+import React, {PureComponent, ReactNode} from 'react';
 import PropTypes from 'prop-types';
-import {DragSource, DropTarget} from 'react-dnd';
-import omit from 'lodash.omit';
+import {DragSource, DropTarget, DndComponent} from 'react-dnd';
+import {omit} from 'lodash';
 import mergeClassNames from 'classnames';
 
 const spec = {
@@ -19,7 +20,11 @@ const collect = (connect, monitor) => ({
     isOver: monitor.isOver()
 });
 
-export const Node = props => {
+interface NodeProps extends Readonly<{
+    children: ReactNode;
+}> {}
+
+export const Node = (props: NodeProps) => {
     const {children, ...restProps} = props;
     const rest = omit(restProps, ['theme']);
 
@@ -29,32 +34,56 @@ export const Node = props => {
         </div>
     );
 };
-Node.propTypes = {
-    children: PropTypes.node
-};
 
-@DropTarget(({nodeDndType}) => nodeDndType, spec, collect)
-class NodeDropTarget extends PureComponent {
-    static propTypes = {
-        connectDropTarget: PropTypes.func.isRequired,
-        nodeDndType: PropTypes.string.isRequired,
-        canDrop: PropTypes.bool.isRequired,
-        isOver: PropTypes.bool,
-        theme: PropTypes.object,
-        mode: PropTypes.string.isRequired
-    };
+interface NodeDropTargetProps extends Readonly<{
+    connectDropTarget: (node: ReactNode) => ReactNode;
+    nodeDndType: string;
+    canDrop: boolean;
+    isOver?: boolean;
+    theme?: NodeDropTargetTheme;
+    mode: string;
+    dragAndDropContext: any; // TODO
+}> {}
 
-    render() {
+interface NodeDropTargetTheme extends Readonly<{
+    'dropTarget': string;
+    'dropTarget--before': string;
+    'dropTarget--after': string;
+    'dropTarget__inner': string;
+    'dropTarget__inner--acceptsDrop': string;
+}> {}
+
+@DropTarget<NodeDropTargetProps>(
+    ({nodeDndType}) => nodeDndType,
+    {
+        canDrop: ({dragAndDropContext, mode}) => dragAndDropContext.accepts(mode || 'into'),
+        drop: ({dragAndDropContext, mode}) => {
+            dragAndDropContext.onDrop(mode || 'into');
+        },
+    },
+    (connect, monitor) => ({
+        connectDropTarget: connect.dropTarget(),
+        canDrop: monitor.canDrop(),
+        isOver: monitor.isOver(),
+    }),
+)
+class NodeDropTarget extends PureComponent<NodeDropTargetProps> {
+    public render(): ReactNode {
         const {connectDropTarget, isOver, canDrop, mode, theme} = this.props;
-        const classNames = mergeClassNames({
-            [theme.dropTarget]: true,
-            [theme['dropTarget--before']]: mode === 'before',
-            [theme['dropTarget--after']]: mode === 'after'
-        });
-        const classNamesInner = mergeClassNames({
-            [theme.dropTarget__inner]: true,
-            [theme['dropTarget__inner--acceptsDrop']]: isOver && canDrop
-        });
+        const classNames = mergeClassNames(
+            theme!.dropTarget,
+            {
+                [theme!['dropTarget--before']]: mode === 'before',
+                [theme!['dropTarget--after']]: mode === 'after'
+            }
+        );
+        const classNamesInner = mergeClassNames(
+            theme!.dropTarget__inner,
+            {
+                [theme!['dropTarget__inner--acceptsDrop']]: isOver && canDrop
+            }
+        );
+
         return connectDropTarget(
             <div className={classNames}>
                 <div className={classNamesInner}/>
@@ -63,84 +92,100 @@ class NodeDropTarget extends PureComponent {
     }
 }
 
-@DragSource(({nodeDndType}) => nodeDndType, {
-    beginDrag(props) {
-        props.dragAndDropContext.onDrag();
-        return {
-            contextPath: props.id
-        };
+interface HeaderProps extends Readonly<{
+    id?: string;
+    labelIdentifier?: string;
+    nodeDndType: string;
+    hasChildren: boolean;
+    isLastChild?: boolean;
+    isCollapsed: boolean;
+    isActive: boolean;
+    isFocused: boolean;
+    isLoading: boolean;
+    isHidden?: boolean;
+    isDirty?: boolean;
+    isHiddenInIndex?: boolean;
+    hasError: boolean;
+    label: string;
+    icon?: string;
+    customIconComponent?: ReactNode; // TODO or ReactElement<SomeIconComponentSpecificProps>
+    iconLabel?: string;
+    level: number;
+    dragAndDropContext?: any; // TODO
+    connectDragSource: (node: ReactNode) => ReactNode;
+    connectDropTarget: (node: ReactNode) => ReactNode;
+    canDrop: boolean;
+    isDragging?: boolean;
+    isOver?: boolean;
+    dragForbidden?: boolean;
+
+    onToggle?: () => void; // TODO
+    onClick?: () => void; // TODO
+    onLabelClick?: () => void;
+    theme?: HeaderTheme;
+}> {}
+
+interface HeaderTheme extends Readonly<{
+    'header': string,
+    'header__data': string,
+    'header__data--isActive': string,
+    'header__data--isFocused': string,
+    'header__data--isLastChild': string;
+    'header__data--isHiddenInIndex': string;
+    'header__data--isHidden': string;
+    'header__data--isDirty': string;
+    'header__data--isDragging': string;
+    'header__data--acceptsDrop': string;
+    'header__data--deniesDrop': string;
+    'header__labelWrapper': string,
+    'header__label': string,
+    'header__chevron': string,
+    'header__chevron--isCollapsed': string,
+    'header__chevron--isLoading': string,
+    'header__iconWrapper': string,
+    'dropZone': string,
+    'dropZone--accepts': string,
+    'dropZone--denies': string
+}> {}
+
+@DragSource<HeaderProps>(
+    ({nodeDndType}) => nodeDndType,
+    {
+        beginDrag: (props) => {
+            props.dragAndDropContext.onDrag();
+            return {
+                contextPath: props.id
+            }
+        },
+        canDrag: ({dragForbidden}) => {
+            return !dragForbidden;
+        }
     },
-    canDrag({dragForbidden}) {
-        return !dragForbidden;
-    }
-}, (connect, monitor) => ({
-    connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging()
-}))
-@DropTarget(({nodeDndType}) => nodeDndType, spec, collect)
-export class Header extends PureComponent {
-    static propTypes = {
-        id: PropTypes.string,
-        labelIdentifier: PropTypes.string,
-        nodeDndType: PropTypes.string.isRequired,
-        hasChildren: PropTypes.bool.isRequired,
-        isLastChild: PropTypes.bool,
-        isCollapsed: PropTypes.bool.isRequired,
-        isActive: PropTypes.bool.isRequired,
-        isFocused: PropTypes.bool.isRequired,
-        isLoading: PropTypes.bool.isRequired,
-        isHidden: PropTypes.bool,
-        isDirty: PropTypes.bool,
-        isHiddenInIndex: PropTypes.bool,
-        hasError: PropTypes.bool.isRequired,
-        label: PropTypes.string.isRequired,
-        icon: PropTypes.string,
-        customIconComponent: PropTypes.node,
-        iconLabel: PropTypes.string,
-        level: PropTypes.number.isRequired,
-        dragAndDropContext: PropTypes.shape({
-            accepts: PropTypes.func.isRequired,
-            onDrag: PropTypes.func.isRequired,
-            onDrop: PropTypes.func.isRequired
-        }),
-        connectDragSource: PropTypes.func.isRequired,
-        connectDropTarget: PropTypes.func.isRequired,
-        canDrop: PropTypes.bool.isRequired,
-        isDragging: PropTypes.bool,
-        isOver: PropTypes.bool,
-        dragForbidden: PropTypes.bool,
-
-        onToggle: PropTypes.func,
-        onClick: PropTypes.func,
-        onLabelClick: PropTypes.func,
-        theme: PropTypes.shape({/* eslint-disable quote-props */
-            'header__data': PropTypes.string,
-            'header__data--isActive': PropTypes.string,
-            'header__data--isFocused': PropTypes.string,
-            'header': PropTypes.string,
-            'header__labelWrapper': PropTypes.string,
-            'header__label': PropTypes.string,
-            'header__chevron': PropTypes.string,
-            'header__chevron--isCollapsed': PropTypes.string,
-            'header__chevron--isLoading': PropTypes.string,
-            'header__iconWrapper': PropTypes.string,
-            'dropZone': PropTypes.string,
-            'dropZone--accepts': PropTypes.string,
-            'dropZone--denies': PropTypes.string
-        }).isRequired, /* eslint-enable quote-props */
-
-        //
-        // Static component dependencies which are injected from the outside (index.js)
-        //
-        IconComponent: PropTypes.any.isRequired
-    };
-
-    render() {
+    (connect, monitor) => ({
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    }),
+)
+@DropTarget<any>(
+    ({nodeDndType}) => nodeDndType,
+    {
+        canDrop: ({dragAndDropContext, mode}) => dragAndDropContext.accepts(mode || 'into'),
+        drop: ({dragAndDropContext, mode}) => {
+            dragAndDropContext.onDrop(mode || 'into');
+        },
+    },
+    (connect, monitor) => ({
+        connectDropTarget: connect.dropTarget(),
+        canDrop: monitor.canDrop(),
+        isOver: monitor.isOver(),
+    }),
+)
+export class Header extends PureComponent<HeaderProps> {
+    public render(): ReactNode {
         const {
             id,
             labelIdentifier,
             nodeDndType,
-            IconComponent,
             hasChildren,
             isLastChild,
             isActive,
@@ -166,18 +211,20 @@ export class Header extends PureComponent {
             ...restProps
         } = this.props;
         const rest = omit(restProps, ['onToggle', 'isCollapsed', 'hasError', 'isDragging', 'dragForbidden']);
-        const dataClassNames = mergeClassNames({
-            [theme.header__data]: true,
-            [theme['header__data--isActive']]: isActive,
-            [theme['header__data--isFocused']]: isFocused,
-            [theme['header__data--isLastChild']]: isLastChild,
-            [theme['header__data--isHiddenInIndex']]: isHiddenInIndex,
-            [theme['header__data--isHidden']]: isHidden,
-            [theme['header__data--isDirty']]: isDirty,
-            [theme['header__data--isDragging']]: isDragging,
-            [theme['header__data--acceptsDrop']]: isOver && canDrop,
-            [theme['header__data--deniesDrop']]: isOver && !canDrop
-        });
+        const dataClassNames = mergeClassNames(
+            theme!.header__data,
+            {
+                [theme!['header__data--isActive']]: isActive,
+                [theme!['header__data--isFocused']]: isFocused,
+                [theme!['header__data--isLastChild']]: isLastChild,
+                [theme!['header__data--isHiddenInIndex']]: isHiddenInIndex,
+                [theme!['header__data--isHidden']]: isHidden,
+                [theme!['header__data--isDirty']]: isDirty,
+                [theme!['header__data--isDragging']]: isDragging,
+                [theme!['header__data--acceptsDrop']]: isOver && canDrop,
+                [theme!['header__data--deniesDrop']]: isOver && !canDrop,
+            },
+        );
 
         return connectDragSource(
             <div>
@@ -231,7 +278,7 @@ export class Header extends PureComponent {
         );
     }
 
-    renderCollapseControl() {
+    private renderCollapseControl() {
         const {
             IconComponent,
             isLoading,

f765222884cf6e0c3e9bab613e75574fed81a0e6
Daniel Lienert <daniel@lienert.cc>
2019-02-14 11:45:57 +0100

    BUGFIX: Use backend validators if available
    This change resolves the according flow validator to a configured
    property validator. If it exists, it validates the result.
    This is non braking, as it returns true, if the validator is not
    found so custom validators are not affected.

---
+++ Classes/Service/NodePropertyValidationService.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the Neos.Neos.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+namespace Neos\Neos\Ui\Service;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Validation\Validator\ValidatorInterface;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class NodePropertyValidationService
+{
+
+    /**
+     * @param $value
+     * @param string $validatorName
+     * @param array $validatorConfiguration
+     * @return bool
+     */
+    public function validate($value, string $validatorName, array $validatorConfiguration): bool
+    {
+        $validator = $this->resolveValidator($validatorName, $validatorConfiguration);
+
+        if ($validator === null) {
+            return true;
+        }
+
+        $result = $validator->validate($value);
+        return !$result->hasErrors();
+    }
+
+    /**
+     * @param string $validatorName
+     * @param array $validatorConfiguration
+     * @return ValidatorInterface|null
+     */
+    protected function resolveValidator(string $validatorName, array $validatorConfiguration): ?ValidatorInterface
+    {
+        $nameParts = explode('/', $validatorName);
+        if (!$nameParts[0] === 'Neos.Neos') {
+            return null;
+        }
+
+        $fullQualifiedValidatorClassName = '\\Neos\\Flow\\Validation\\Validator\\' . end($nameParts);
+
+        if (!class_exists($fullQualifiedValidatorClassName)) {
+            return null;
+        }
+
+        return new $fullQualifiedValidatorClassName($validatorConfiguration);
+    }
+}
---
+++ Tests/Functional/Service/NodePropertyValidationServiceTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Neos\Neos\Ui\Tests\Functional\Service;
+
+/*
+ * This file is part of the Neos.Neos.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Tests\FunctionalTestCase;
+use Neos\Flow\Validation\Validator\NotEmptyValidator;
+use Neos\Neos\Ui\Service\NodePropertyValidationService;
+
+class NodePropertyValidationServiceTest extends FunctionalTestCase
+{
+
+    /**
+     * @var NodePropertyValidationService
+     */
+    protected $nodePropertyValidationService;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $nodePropertyValidatorServiceProxy = $this->buildAccessibleProxy(NodePropertyValidationService::class);
+        $this->nodePropertyValidationService = $this->objectManager->get($nodePropertyValidatorServiceProxy);
+    }
+
+    /**
+     * @test
+     */
+    public function resolveValidator()
+    {
+        $validator = $this->nodePropertyValidationService->_call('resolveValidator', 'Neos.Neos/Validation/NotEmptyValidator', []);
+        $this->assertInstanceOf(NotEmptyValidator::class, $validator);
+    }
+
+    /**
+     * @test
+     */
+    public function validate()
+    {
+        $result = $this->nodePropertyValidationService->validate(
+            'test',
+            'Neos.Neos/Validation/StringLengthValidator',
+            ['minimum' => 1, 'maximum' => 255]);
+
+        $this->assertTrue($result);
+    }
+}

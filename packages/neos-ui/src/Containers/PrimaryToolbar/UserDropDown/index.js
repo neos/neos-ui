7ba6db54af735b9913eb9fd4759ebf43818f7545
Daniel Kestler <daniel.kestler@medienreaktor.de>
2019-02-17 08:28:13 +0100

    TASK: Remove legacy UI switch (#2360)

--- packages/neos-ui/src/Containers/PrimaryToolbar/UserDropDown/index.js
+++ packages/neos-ui/src/Containers/PrimaryToolbar/UserDropDown/index.js
@@ -6,46 +6,21 @@ import Icon from '@neos-project/react-ui-components/src/Icon/';
 import DropDown from '@neos-project/react-ui-components/src/DropDown/';
 
 import I18n from '@neos-project/neos-ui-i18n';
-import {neos} from '@neos-project/neos-ui-decorators';
 
 import style from './style.css';
 
 @connect($transform({
     userName: $get('user.name.fullName')
 }))
-@neos(globalRegistry => ({
-    enableLegacyUiSwitch: $get('enableUiSwitch', globalRegistry.get('frontendConfiguration').get('legacy'))
-}))
 export default class UserDropDown extends PureComponent {
     static propTypes = {
-        userName: PropTypes.string.isRequired,
-        neos: PropTypes.object.isRequired,
-        enableLegacyUiSwitch: PropTypes.bool
+        userName: PropTypes.string.isRequired
     };
 
     render() {
         const logoutUri = $get('routes.core.logout', this.props.neos);
         const userSettingsUri = $get('routes.core.modules.userSettings', this.props.neos);
         const {csrfToken} = document.getElementById('appContainer').dataset;
-
-        const legacyUiSwitch = () => {
-            const {enableLegacyUiSwitch} = this.props;
-
-            // Don't show legacy ui switch only if
-            // explicitly set to false
-            if (enableLegacyUiSwitch === false) {
-                return null;
-            }
-
-            return (
-                <li className={style.dropDown__item}>
-                    <a title="User Settings" href="/neos/legacy">
-                        <Icon icon="far frown" aria-hidden="true" className={style.dropDown__itemIcon}/>
-                        <I18n id="userSettings_swtichUi" sourceName="Modules" fallback="Switch to old UI"/>
-                    </a>
-                </li>
-            );
-        };
 
         return (
             <div className={style.wrapper}>
@@ -64,7 +39,6 @@ export default class UserDropDown extends PureComponent {
                                 </button>
                             </form>
                         </li>
-                        {legacyUiSwitch()}
                         <li className={style.dropDown__item}>
                             <a title="User Settings" href={userSettingsUri}>
                                 <Icon icon="wrench" aria-hidden="true" className={style.dropDown__itemIcon}/>

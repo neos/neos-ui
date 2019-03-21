4a6595340346e630dc97a2fffb3dfa72ad4f8d7e
Dmitri Pisarev <dimaip@gmail.com>
2019-03-21 10:02:16 +0100

    Use symlinks instead of cp

--- Tests/IntegrationTests/e2e.sh
+++ Tests/IntegrationTests/e2e.sh
@@ -10,9 +10,10 @@ mv DistributionPackages/Neos.TestSite DummyTestPackage
 for fixture in Packages/Application/Neos.Neos.Ui/Tests/IntegrationTests/Fixtures/*/; do
     echo "$fixture"
 
-    cp -r "${fixture}SitePackage" DistributionPackages/Neos.TestSite
+    ln -s "../${fixture}SitePackage" DistributionPackages/Neos.TestSite
 
-    FLOW_CONTEXT=Production ./flow flow:cache:flush
+    FLOW_CONTEXT=Production ./flow flow:cache:flushone Neos_Neos_Fusion
+    FLOW_CONTEXT=Production ./flow flow:cache:flushone Neos_Fusion_Content
     FLOW_CONTEXT=Production ./flow site:prune
     FLOW_CONTEXT=Production ./flow site:import --package-key=Neos.TestSite
 
@@ -20,7 +21,7 @@ for fixture in Packages/Application/Neos.Neos.Ui/Tests/IntegrationTests/Fixtures
     yarn run testcafe chrome "../../../${fixture}*.e2e.js" \
             --selector-timeout=10000 --assertion-timeout=30000
     cd ../../..
-    rm -rf DistributionPackages/Neos.TestSite
+    rm -f DistributionPackages/Neos.TestSite
 
 done
 

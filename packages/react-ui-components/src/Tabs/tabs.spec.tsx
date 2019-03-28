973d575154b48c8b0f5593abd0010abd656aafdc
Philipp Maas <mail@pmaas.com>
2019-03-28 09:07:41 +0100

    Fixed typos

--- packages/react-ui-components/src/Tabs/tabs.spec.tsx
+++ packages/react-ui-components/src/Tabs/tabs.spec.tsx
@@ -16,8 +16,8 @@ describe('<Tabs/>', () => {
         ...tabsDefaultProps,
         theme: {
             'tabs': 'baseTabsClassName',
-            'tabs__content': 'basTabsContentsClassName',
-            'tabs__panel': 'basTabsPanelsClassName',
+            'tabs__content': 'baseTabsContentsClassName',
+            'tabs__panel': 'baseTabsPanelsClassName',
             'tabNavigation': 'baseTabsNavigationClassName',
             'tabNavigation__item': 'baseTabsNavigationItemClassName',
             'tabNavigation__item--isActive': 'activeTabsNavigationItemClassName',

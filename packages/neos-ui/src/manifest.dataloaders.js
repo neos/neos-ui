f89d4b468bf3140da16895f3821668876e82fc0d
mireo91 <mireo91@gmail.com>
2019-05-23 11:43:14 +0200

    FEATURE: Make caching data source in lru cache optional
    Add the property dataSourceDisableCaching to be able to disable the lru cache for
    data sources.

--- packages/neos-ui/src/manifest.dataloaders.js
+++ packages/neos-ui/src/manifest.dataloaders.js
@@ -247,6 +247,7 @@ manifest('main.dataloaders', {}, globalRegistry => {
                 - contextNodePath: ...
                 - dataSourceIdentifier: The data source to load. Either this or dataSourceUri is required.
                 - dataSourceUri: The data source URL to load.
+                - dataSourceDisableCaching: Disable default _lru caching option.
                 - dataSourceAdditionalData: Additional data to send to the server
         `,
 
@@ -272,7 +273,9 @@ manifest('main.dataloaders', {}, globalRegistry => {
             const params = Object.assign({node: options.contextNodePath}, options.dataSourceAdditionalData || {});
             const resultPromise = dataSource(options.dataSourceIdentifier, options.dataSourceUri, params);
 
-            this._lru().set(cacheKey, resultPromise);
+            if( !options.dataSourceDisableCaching ) {
+                this._lru().set(cacheKey, resultPromise);
+            }
             return resultPromise;
         },
 

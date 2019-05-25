9ed9cc1645e669e232e037191e1f003e6478cb9a
Daniel Kestler <daniel.kestler@medienreaktor.de>
2019-05-25 15:22:56 +0200

    Change node context by rebuilding node information to include removed nodes

--- Classes/Fusion/Helper/NodeInfoHelper.php
+++ Classes/Fusion/Helper/NodeInfoHelper.php
@@ -21,6 +21,7 @@ use Neos\Neos\Domain\Service\ContentContext;
 use Neos\Neos\Service\LinkingService;
 use Neos\Neos\Service\Mapping\NodePropertyConverterService;
 use Neos\Neos\TypeConverter\EntityToIdentityConverter;
+use Neos\Neos\Ui\ContentRepository\Service\NodeService;
 use Neos\Neos\Ui\Domain\Service\UserLocaleService;
 use Neos\Neos\Ui\Service\NodePolicyService;
 
@@ -34,6 +35,12 @@ class NodeInfoHelper implements ProtectedContextAwareInterface
      * @var NodePolicyService
      */
     protected $nodePolicyService;
+
+    /**
+     * @Flow\Inject
+     * @var NodeService
+     */
+    protected $nodeService;
 
     /**
      * @Flow\Inject
@@ -226,6 +233,9 @@ class NodeInfoHelper implements ProtectedContextAwareInterface
      */
     protected function renderChildrenInformation(NodeInterface $node, string $nodeTypeFilterString): array
     {
+        // rebuild node to change context to include removed nodes
+        $node = $this->nodeService->getNodeFromContextPath($node->getContextPath(), null, null, true);
+
         $documentChildNodes = $node->getChildNodes($nodeTypeFilterString);
         // child nodes for content tree, must not include those nodes filtered out by `baseNodeType`
         $contentChildNodes = $node->getChildNodes($this->buildContentChildNodeFilterString());

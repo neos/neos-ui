022ef606518b867f80d7de7ccf5f78cb19593bbe
Sebastian Helzle <sebastian@helzle.it>
2019-11-27 12:14:08 +0100

    FEATURE: Parse html and json errors before output
    This prevents the fullscreen red flash messages
    or flash messages with json code by
    checking the message content and parsing it.

--- packages/neos-ui/src/index.js
+++ packages/neos-ui/src/index.js
@@ -152,6 +152,30 @@ function * application() {
     });
 
     fetchWithErrorHandling.registerGeneralErrorHandler((message = 'unknown error') => {
+        // Check if the message is a Flow exception response
+        if (message.indexOf('Flow-Debug-Exception-Header') >= 0) {
+            const htmlContainer = document.createElement('div');
+            htmlContainer.innerHTML = message;
+            const exceptionHeader = htmlContainer.querySelector('.Flow-Debug-Exception-Header');
+            if (exceptionHeader && exceptionHeader.textContent) {
+                const exceptionSubject = exceptionHeader.querySelector('.ExceptionSubject');
+                message = exceptionSubject.textContent + ' - Check the logs for details';
+            } else {
+                message = 'Unknown error from unexpected HTML response. Check the logs for details.';
+            }
+        }
+        // Check if the message is a JSON string
+        else if (message.indexOf('{') >= 0) {
+            try {
+                const jsonMessage = JSON.parse(message);
+                if (jsonMessage.error && jsonMessage.error.message) {
+                    message = jsonMessage.error.message + ' - Reference code "' + jsonMessage.error.referenceCode + '"';
+                } else {
+                    message = 'Unknown error from unexpected JSON response. Check the logs for details.';
+                }
+            } catch (e) {}
+        }
+
         store.dispatch(actions.UI.FlashMessages.add('fetch error', message, 'error'));
     });
 

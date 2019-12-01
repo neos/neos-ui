4128e536182aad79b774049b8b24637df869b6c5
Dmitri Pisarev <dimaip@gmail.com>
2019-12-01 16:39:05 +0300

    Lint...

--- packages/neos-ui-redux-store/src/combineReducers.ts
+++ packages/neos-ui-redux-store/src/combineReducers.ts
@@ -5,37 +5,37 @@ type FunctionReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
 type CombinedState<T extends object> = { [K in keyof T]: FunctionReturnType<T[K]> };
 
 type ActionFromReducer<T extends object> = T[keyof T] extends (
-  state: any,
-  action: infer A,
-  ...args: any[]
+    state: any,
+    action: infer A,
+    ...args: any[]
 ) => any
-  ? A
-  : never;
+    ? A
+    : never;
 
 interface Reducers {
-  [key: string]: any;
+    [ key: string]: any;
 }
 
 type ReducerReturnValueRoot<T extends object> = (
-  state: CombinedState<T>,
-  action: ActionFromReducer<T>,
-  globalState: any
+    state: CombinedState<T>,
+    action: ActionFromReducer<T>,
+    globalState: any
 ) => CombinedState<T>;
 
 export function combineReducers<T extends Reducers>(reducers: T): ReducerReturnValueRoot<T> {
-  type S = CombinedState<T>;
-  const reducerProps = Object.keys(reducers);
+    type S = CombinedState<T>;
+    const reducerProps = Object.keys(reducers);
 
-  return (state: S = {} as S, action: any, globalState: any): S => {
-    let hasChanged = false;
-    const nextState: S = {} as S;
-    for (const prop of reducerProps) {
-      const reducer = reducers[prop];
-      const previousStateForKey = state[prop];
-      const nextStateForKey = reducer(previousStateForKey, action, globalState || state);
-      nextState[prop] = nextStateForKey;
-      hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
-    }
-    return hasChanged ? nextState : state;
-  };
+    return (state: S = {} as S, action: any, globalState: any): S => {
+        let hasChanged = false;
+        const nextState: S = {} as S;
+        for (const prop of reducerProps) {
+            const reducer = reducers[prop];
+            const previousStateForKey = state[prop];
+            const nextStateForKey = reducer(previousStateForKey, action, globalState || state);
+            nextState[prop] = nextStateForKey;
+            hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
+        }
+        return hasChanged ? nextState : state;
+    };
 }

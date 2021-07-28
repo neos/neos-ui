14892243551d664876368db7557a967afa265503
Sebastian Helzle <sebastian@helzle.it>
2021-07-28 10:51:16 +0200

    FEATURE: Support preview uris in datasource selectboxeditor (#2930)

---
+++ packages/neos-ui-editors/src/Library/PreviewOption.js
@@ -0,0 +1,22 @@
+/* eslint-disable camelcase, react/jsx-pascal-case */
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import SelectBox_Option_MultiLineWithThumbnail from '@neos-project/react-ui-components/src/SelectBox_Option_MultiLineWithThumbnail';
+
+export default class PreviewOption extends PureComponent {
+    static propTypes = {
+        option: PropTypes.shape({
+            label: PropTypes.string.isRequired,
+            icon: PropTypes.string,
+            preview: PropTypes.string
+        })
+    };
+
+    render() {
+        const {option} = this.props;
+
+        return (
+            <SelectBox_Option_MultiLineWithThumbnail {...this.props} imageUri={option.preview} icon={option.icon} label={option.label}/>
+        );
+    }
+}

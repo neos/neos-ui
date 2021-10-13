7dc78727289822bb78a3d0a9ca01274b66a75135
Daniel Lienert <daniel@lienert.cc>
2021-10-13 10:04:11 +0200

    BUGFIX: Use setHttpHeader instead of deprecated setComponentParameter (#2922)
    * BUGFIX: Use setHttpHeader instead of deprecated setComponentParameter
    
    * TASK: Replace deprecated guzzle version
    
    * Update Classes/Fusion/ExceptionHandler/PageExceptionHandler.php
    
    Co-authored-by: Wilhelm Behncke <2522299+grebaldi@users.noreply.github.com>
    
    Co-authored-by: Markus Günther <info@unikka.de>
    Co-authored-by: Wilhelm Behncke <2522299+grebaldi@users.noreply.github.com>

--- Classes/Fusion/ExceptionHandler/PageExceptionHandler.php
+++ Classes/Fusion/ExceptionHandler/PageExceptionHandler.php
@@ -11,7 +11,7 @@ namespace Neos\Neos\Ui\Fusion\ExceptionHandler;
  * source code.
  */
 
-use function GuzzleHttp\Psr7\str;
+use GuzzleHttp\Psr7\Message;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Exception;
 use Neos\Flow\Mvc\View\ViewInterface;
@@ -85,7 +85,7 @@ class PageExceptionHandler extends AbstractRenderingExceptionHandler
             ->withBody($this->contentFactory->createStream($bodyContent))
             ->withHeader('Cache-Control', 'no-store');
 
-        return str($response);
+        return Message::toString($response);
     }
 
     /**

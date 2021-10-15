33866f5221001e9705514e7b3f459c3eb452681f
Markus Günther <info@unikka.de>
2021-10-15 18:16:44 +0200

    BUGFIX: Close help message when closing dialog
    The help message closes when the create dialog getting closed.
    Before, the help message was still open when the create dialog reopens.
    
    Resolves: #2576

--- packages/neos-ui/src/Containers/Modals/SelectNodeType/index.js
+++ packages/neos-ui/src/Containers/Modals/SelectNodeType/index.js
@@ -118,6 +118,7 @@ export default class SelectNodeType extends PureComponent {
         this.setState({
             filterSearchTerm: ''
         });
+        this.handleCloseHelpMessage();
         cancel();
     }
 

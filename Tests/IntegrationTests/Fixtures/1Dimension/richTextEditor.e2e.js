03c80e0090f230e74ef50d65817d33380ba04280
Markus Günther <info@unikka.de>
2021-10-20 13:33:29 +0200

    TASK: Skip test for Safari

--- Tests/IntegrationTests/Fixtures/1Dimension/richTextEditor.e2e.js
+++ Tests/IntegrationTests/Fixtures/1Dimension/richTextEditor.e2e.js
@@ -5,24 +5,31 @@ import {Page} from './../../pageModel';
 
 /* global fixture:true */
 
-fixture`Rich text editor`
-    .beforeEach(beforeEach)
-    .afterEach(() => checkPropTypes());
+const featureDisabled = t.browser.name === 'Safari';
 
-test('Can crop an image', async t => {
-    const testContent = 'Test RTE content';
-    await Page.waitForIframeLoading(t);
+if(featureDisabled) {
+    fixture`Rich text editor`
+        .beforeEach(beforeEach)
+        .afterEach(() => checkPropTypes());
 
-    const rteInspectorEditor = await ReactSelector('InspectorEditorEnvelope').withProps('id', 'rte');
-    const ckeContent = await Selector('.ck-content p');
-    await t
-        .click(rteInspectorEditor.findReact('Button'));
-    await t
-        .typeText(ckeContent, testContent)
-        .wait(400)
-        .click(Selector('#neos-Inspector-Apply'));
-    await Page.waitForIframeLoading(t);
-    await t.switchToIframe('[name="neos-content-main"]');
-    await t.expect(Selector('.test-page-rte').innerText).eql(testContent, 'Entered RTE content should have persisted');
-    await t.switchToMainWindow();
-});
+    test('Can crop an image', async t => {
+
+        const testContent = 'Test RTE content';
+        await Page.waitForIframeLoading(t);
+
+        const rteInspectorEditor = await ReactSelector('InspectorEditorEnvelope').withProps('id', 'rte');
+        const ckeContent = await Selector('.ck-content p');
+        await t
+            .click(rteInspectorEditor.findReact('Button'));
+        await t
+            .typeText(ckeContent, testContent)
+            .wait(400)
+            .click(Selector('#neos-Inspector-Apply'));
+        await Page.waitForIframeLoading(t);
+        await t.switchToIframe('[name="neos-content-main"]');
+        await t.expect(Selector('.test-page-rte').innerText).eql(testContent, 'Entered RTE content should have persisted');
+        await t.switchToMainWindow();
+    });
+} else {
+    fixture.skip`Rich text editor`
+}

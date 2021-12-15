bcbd773e024b94b91a0d104f16fc4ba471db131e
Markus Günther <info@unikka.de>
2021-12-15 21:16:35 +0100

    TASK: Rename option name to validationErrorMessage
    Renames the option customValidationError to validationErrorMessage. Also bypass the option to the localization component when the option is set.

--- packages/neos-ui-validators/src/RegularExpression/index.tsx
+++ packages/neos-ui-validators/src/RegularExpression/index.tsx
@@ -6,7 +6,7 @@ import I18n from '@neos-project/neos-ui-i18n';
  */
 interface RegularExpressionOptions {
     regularExpression: string;
-    customValidationError?: string;
+    validationErrorMessage?: string;
 }
 const RegularExpression = (value: any, validatorOptions: RegularExpressionOptions) => {
     // NOTE: we do not need to localize the two strings here; as they are purely integrator issues which an end user
@@ -24,10 +24,8 @@ const RegularExpression = (value: any, validatorOptions: RegularExpressionOption
         return null;
     }
     const pattern = regularExpression.toString();
-    if (validatorOptions.customValidationError) {
-        return validatorOptions.customValidationError;
-    }
-    return <I18n id="content.inspector.validators.regularExpressionValidator.patternDoesNotMatch" params={{pattern}}/>;
+    const label = validatorOptions.validationErrorMessage ?? 'content.inspector.validators.regularExpressionValidator.patternDoesNotMatch';
+    return <I18n id={label} params={{pattern}}/>;
 };
 
 export default RegularExpression;

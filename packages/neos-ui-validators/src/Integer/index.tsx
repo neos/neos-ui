d8cf6705c8ea1841334c0f7e1be2fb450f3dbf1f
pkallert <paula.kallert@paessler.com>
2021-12-16 14:34:21 +0100

    FEATURE: add custom validation error to IntegerValidator, LabelValidator, NumberRangeValidator

--- packages/neos-ui-validators/src/Integer/index.tsx
+++ packages/neos-ui-validators/src/Integer/index.tsx
@@ -8,14 +8,18 @@ const isNotEmpty = (value: any) => {
 /**
  * Checks if the given value is a valid Integer number
  */
-const Integer = (value: any) => {
+interface IntegerOptions {
+    validationErrorMessage?: string;
+}
+const Integer = (value: any, validatorOptions: IntegerOptions) => {
     const number = parseInt(value, 10);
 
     if (isNotEmpty(value) &&
         (!Number.isSafeInteger(number) ||
         // if the value contains other characters than '-' or a digit it's not valid
         !(/^[\d-]+$/.test(value)))) {
-        return <I18n id="content.inspector.validators.integerValidator.aValidIntegerNumberIsExpected"/>;
+        const label = validatorOptions.validationErrorMessage ?? 'content.inspector.validators.integerValidator.aValidIntegerNumberIsExpected';
+        return <I18n id={label}/>;
     }
     return null;
 };

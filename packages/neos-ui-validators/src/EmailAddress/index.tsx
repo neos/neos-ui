aa04ee2a7693085b9580870c8b39138ac35b4e8e
pkallert <paula.kallert@paessler.com>
2021-12-16 13:52:05 +0100

    FEATURE: add custom validation error to EmailAdressValidation

--- packages/neos-ui-validators/src/EmailAddress/index.tsx
+++ packages/neos-ui-validators/src/EmailAddress/index.tsx
@@ -8,12 +8,16 @@ const isNil = (value: any) => value === null || value === undefined;
  * Checks if the given value is a valid email address
  * Source: http://fightingforalostcause.net/misc/2006/compare-email-regex.php
  */
-const EmailAddress = (value: any) => {
+interface EmailAddressOptions {
+    regularExpression: string;
+    validationErrorMessage?: string;
+}
+const EmailAddress = (value: any, validatorOptions: EmailAddressOptions) => {
     if (isNil(value) || value === '') {
         return null;
     }
-
-    return isEmail.validate(value) ? null : <I18n id="content.inspector.validators.emailAddressValidator.invalidEmail"/>;
+    const label = validatorOptions.validationErrorMessage ?? 'content.inspector.validators.emailAddressValidator.invalidEmail';
+    return isEmail.validate(value) ? null : <I18n id={label}/>;
 };
 
 export default EmailAddress;

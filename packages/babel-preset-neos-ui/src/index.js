a4304219e4ffca275c051af6d1890b119f03e789
Markus Günther <info@unikka.de>
2022-03-14 22:17:31 +0100

    TASK: Refactor babel preset config and use the api with caching

--- packages/babel-preset-neos-ui/src/index.js
+++ packages/babel-preset-neos-ui/src/index.js
@@ -1,11 +1,31 @@
-module.exports = function () {
-    return {
-        presets: ["@babel/react", "@babel/env"],
-        plugins: [
-            ["@babel/plugin-proposal-decorators", { legacy: true }],
-            ["@babel/plugin-proposal-class-properties", { loose: true }],
-            "@babel/plugin-syntax-jsx",
-            "@babel/plugin-proposal-object-rest-spread",
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = [
+        [
+            "@babel/preset-env",
+            {
+                modules: false,
+                debug: false,
+                useBuiltIns: "usage"
+            },
         ],
+        ["@babel/preset-react"],
+    ];
+
+    const plugins = [
+        "@babel/plugin-proposal-export-default-from",
+        "@babel/plugin-proposal-export-namespace-from",
+        "@babel/plugin-syntax-jsx",
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-transform-regenerator",
+        ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ];
+
+    return {
+        presets,
+        plugins,
     };
 };

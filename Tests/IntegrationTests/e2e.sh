eb91f5b44485d1bdaafd9fa2f310fc4c9325a659
Karsten Dambekalns <karsten@dambekalns.de>
2022-08-31 10:20:01 +0200

    TASK: Tweak e2e.sh a bit

--- Tests/IntegrationTests/e2e.sh
+++ Tests/IntegrationTests/e2e.sh
@@ -2,9 +2,8 @@
 
 set -ex
 
-if [ -z "$1" ]
-    then
-        echo "No testcafe browser supplied, e.g. 'chrome:headless'"
+if [ -z "$1" ]; then
+    echo "No testcafe browser supplied, e.g. 'chrome:headless'"
 fi
 
 cd ../../..
@@ -13,15 +12,20 @@ rm -rf DummyDistributionPackages || true
 mv DistributionPackages DummyDistributionPackages
 mkdir DistributionPackages
 
+ln -s "../Packages/Application/Neos.Neos.Ui/Tests/IntegrationTests/SharedNodeTypesPackage" DistributionPackages/Neos.TestNodeTypes
+
 for fixture in Packages/Application/Neos.Neos.Ui/Tests/IntegrationTests/Fixtures/*/; do
     echo "$fixture"
 
-    ln -s "../Packages/Application/Neos.Neos.Ui/Tests/IntegrationTests/SharedNodeTypesPackage" DistributionPackages/Neos.TestNodeTypes
     ln -s "../${fixture}SitePackage" DistributionPackages/Neos.TestSite
 
     # TODO: optimize this
-    composer update
+    #composer update
+    composer reinstall neos/test-site
     ./flow flow:cache:flush --force
+    ./flow flow:cache:warmup
+    ./flow configuration:show --path Neos.ContentRepository.contentDimensions
+
     if ./flow site:list | grep -q 'Node name'; then
         ./flow site:prune '*'
     fi

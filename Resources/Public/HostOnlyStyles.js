3e29d8746a8b094d7dd069f840b5d8c0f9089c89
Markus Günther <info@unikka.de>
2022-11-16 17:53:51 +0100

    BUGFIX: Use @neos-project/eslint-config-neos in version 2.5
    Resolve merge error

---
+++ Resources/Public/HostOnlyStyles.js
@@ -0,0 +1,5 @@
+(() => {
+  // stylePlugin:/Volumes/Development/Playground/neos-82-dev/Packages/Application/Neos.Neos.Ui/packages/neos-ui/src/styleHostOnly.css
+  var styleHostOnly_default = {};
+})();
+//# sourceMappingURL=HostOnlyStyles.js.map

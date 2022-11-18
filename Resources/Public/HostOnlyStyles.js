3427593078bf7e77fde3582756e81fc8eb0f7a28
Sebastian Helzle <sebastian@helzle.it>
2022-11-18 08:38:06 +0100

    BUGFIX: Remove compiled assets from git again

--- Resources/Public/HostOnlyStyles.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  // stylePlugin:/Volumes/Development/Playground/neos-82-dev/Packages/Application/Neos.Neos.Ui/packages/neos-ui/src/styleHostOnly.css
-  var styleHostOnly_default = {};
-})();
-//# sourceMappingURL=HostOnlyStyles.js.map

43035d1fc75b64983a4edc3010926a503528a37f
Sebastian Helzle <sebastian@helzle.it>
2022-11-24 14:45:11 +0100

    TASK: Resolve eslint warnings in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,37 +2,40 @@ module.exports = {
     root: true,
     extends: [
         '@neos-project/eslint-config-neos',
-        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/eslint-recommended'
     ],
     globals: {
         expect: true,
         sinon: false
     },
     env: {
-        node: true,
+        node: true
     },
     plugins: [
-        "@typescript-eslint",
+        '@typescript-eslint'
     ],
+    parserOptions: {
+        requireConfigFile: false
+    },
     rules: {
         // Fix for incorrect unused var detection
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": ["error"],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
 
         // The following rules should be fixed and enabled again
-        "no-use-before-define": "off",
-        "default-case": "off",
-        "no-mixed-operators": "off",
-        "no-negated-condition": "off",
-        "complexity": "off",
+        'no-use-before-define': 'off',
+        'default-case': 'off',
+        'no-mixed-operators': 'off',
+        'no-negated-condition': 'off',
+        'complexity': 'off'
     },
     overrides: [
         {
             files: [
-                "**/*.ts",
-                "**/*.tsx",
+                '**/*.ts',
+                '**/*.tsx'
             ],
-            parser: "@typescript-eslint/parser"
+            parser: '@typescript-eslint/parser'
         }
     ]
 }

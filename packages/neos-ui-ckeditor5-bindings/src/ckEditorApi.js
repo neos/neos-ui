7c9fc845981d13c7a413efb33094949026071dfa
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-06-28 16:02:59 +0200

    BUGFIX: Fix inline ckeditor mode
    Since there is still no native support for a inline ckeditor mode. https://github.com/ckeditor/ckeditor5/issues/762 we will continue our hacky road ;)
    
    This fixes several issues:
    - Inserting text with newlines will now correctly insert soft breaks (br)
    - We use our own pseodo `<neos-inline-wrapper>` tag to avoid the issue of having to parse html with regex and doing it wrong #2918
    
    i also added a test for some inline editing behavior. (Like that there are no outer span or p tags)
    But writing a tests for #2918 is currently impossible, as our tests currently dont use `https` which means we cant access the `navigator.clipboard` and test that ckeditor works correctly when content is pasted.

--- packages/neos-ui-ckeditor5-bindings/src/ckEditorApi.js
+++ packages/neos-ui-ckeditor5-bindings/src/ckEditorApi.js
@@ -3,16 +3,26 @@ import DecoupledEditor from '@ckeditor/ckeditor5-editor-decoupled/src/decouplede
 import {actions} from '@neos-project/neos-ui-redux-store';
 
 // We remove opening and closing span tags that are produced by the inlineMode plugin
+/** @param {String} content */
 const cleanupContentBeforeCommit = content => {
     // TODO: remove when this is fixed: https://github.com/ckeditor/ckeditor5/issues/401
     if (content.match(/^<([a-z][a-z0-9]*)\b[^>]*>&nbsp;<\/\1>$/)) {
         return '';
     }
 
-    if (content.match(/^<span>[^<]*<\/span>$/)) {
-        return content
-            .replace(/^<span>/, '')
-            .replace(/<\/span>$/, '');
+    if (content.startsWith('<neos-inline-wrapper>') && content.endsWith('</neos-inline-wrapper>')) {
+        const contentWithoutOuterInlineWrapper = content
+            .replace(/^<neos-inline-wrapper>/, '')
+            .replace(/<\/neos-inline-wrapper>$/, '');
+
+        if (contentWithoutOuterInlineWrapper.includes('<neos-inline-wrapper>')) {
+            // in the case, multiple root paragraph elements were inserted into the ckeditor (wich is currently not prevented if the html is modified from outside)
+            // we have multiple root elements of type <neos-inline-wrapper>. We will convert all of them into spans.
+            return content
+                .replace(/<neos-inline-wrapper>/g, '<span>')
+                .replace(/<\/neos-inline-wrapper>/g, '</span>');
+        }
+        return contentWithoutOuterInlineWrapper;
     }
     return content;
 };
--- packages/neos-ui-ckeditor5-bindings/src/plugins/inlineMode.js
+++ packages/neos-ui-ckeditor5-bindings/src/plugins/inlineMode.js
@@ -1,5 +1,9 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
+/**
+ * HACK, since there is yet no native support
+ * see https://github.com/ckeditor/ckeditor5/issues/762
+ */
 export default class InlineMode extends Plugin {
     static get pluginName() {
         return 'InlineMode';
@@ -7,10 +11,26 @@ export default class InlineMode extends Plugin {
     init() {
         const editor = this.editor;
 
-        // We map paragraph model into plain <span> element
-        editor.conversion.elementToElement({model: 'paragraph', view: 'span', converterPriority: 'high'});
+        // we avoid multiple paragraph's to be created - eg when pasting a text with newlines (this will created soft breaks (<br>))
+        editor.model.schema.extend('paragraph', { isLimit: true });
 
-        // We redefine enter key to great soft breaks instead of paragraphs
+
+        // we map paragraph model into plain <span> element in edit mode
+        editor.conversion.for('editingDowncast').elementToElement({model: 'paragraph', view: 'span', converterPriority: 'high'});
+
+        // to avoid having a wrapping "span" tag, we will convert the outmost 'paragraph' and strip the custom tag 'neos-inline-wrapper'
+        // in a hacky cleanup in cleanupContentBeforeCommit
+        // see https://neos-project.slack.com/archives/C07QEQ1U2/p1687952441254759 - i could find a better solution
+        editor.conversion.for('dataDowncast').elementToElement({model: 'paragraph', view: ( modelElement, viewWriter ) => {
+            const parentIsRoot = modelElement.parent.is('$root');
+            const hasAttributes = [...modelElement.getAttributes()].length !== 0;
+            if (!parentIsRoot || hasAttributes) {
+                return viewWriter.createContainerElement('span');
+            }
+            return viewWriter.createContainerElement('neos-inline-wrapper');
+        }, converterPriority: 'high'});
+
+        // we redefine enter key to create soft breaks (<br>) instead of new paragraphs
         editor.editing.view.document.on('enter', (evt, data) => {
             editor.execute('shiftEnter');
             data.preventDefault();

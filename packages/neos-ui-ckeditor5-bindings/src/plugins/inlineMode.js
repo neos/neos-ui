39dafdcef67e302938990c634a51ca571d4275ab
Simon Krull <krull@punkt.de>
2023-06-29 17:06:16 +0200

    BUGFIX: make linter happy

--- packages/neos-ui-ckeditor5-bindings/src/plugins/inlineMode.js
+++ packages/neos-ui-ckeditor5-bindings/src/plugins/inlineMode.js
@@ -18,7 +18,7 @@ export default class InlineMode extends Plugin {
         // to avoid having a wrapping "span" tag, we will convert the outmost 'paragraph' and strip the custom tag 'neos-inline-wrapper'
         // in a hacky cleanup in cleanupContentBeforeCommit
         // see https://neos-project.slack.com/archives/C07QEQ1U2/p1687952441254759 - i could find a better solution
-        editor.conversion.for('dataDowncast').elementToElement({model: 'paragraph', view: ( modelElement, viewWriter ) => {
+        editor.conversion.for('dataDowncast').elementToElement({model: 'paragraph', view: (modelElement, viewWriter) => {
             const parentIsRoot = modelElement.parent.is('$root');
             const hasAttributes = [...modelElement.getAttributes()].length !== 0;
             if (!parentIsRoot || hasAttributes) {

bdd46a54a47d4b3326c3f4c85382831229231dbc
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-06-29 15:01:11 +0200

    BUGFIX: Fix cleanupContentBeforeCommit and add test
    Fix bug in the case you had multiple paragraphs and a headline and switched to autoparagrahp: false

--- packages/neos-ui-ckeditor5-bindings/src/ckEditorApi.js
+++ packages/neos-ui-ckeditor5-bindings/src/ckEditorApi.js
@@ -1,31 +1,7 @@
 import debounce from 'lodash.debounce';
 import DecoupledEditor from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor';
 import {actions} from '@neos-project/neos-ui-redux-store';
-
-// We remove opening and closing span tags that are produced by the inlineMode plugin
-/** @param {String} content */
-const cleanupContentBeforeCommit = content => {
-    // TODO: remove when this is fixed: https://github.com/ckeditor/ckeditor5/issues/401
-    if (content.match(/^<([a-z][a-z0-9]*)\b[^>]*>&nbsp;<\/\1>$/)) {
-        return '';
-    }
-
-    if (content.startsWith('<neos-inline-wrapper>') && content.endsWith('</neos-inline-wrapper>')) {
-        const contentWithoutOuterInlineWrapper = content
-            .replace(/^<neos-inline-wrapper>/, '')
-            .replace(/<\/neos-inline-wrapper>$/, '');
-
-        if (contentWithoutOuterInlineWrapper.includes('<neos-inline-wrapper>')) {
-            // in the case, multiple root paragraph elements were inserted into the ckeditor (wich is currently not prevented if the html is modified from outside)
-            // we have multiple root elements of type <neos-inline-wrapper>. We will convert all of them into spans.
-            return content
-                .replace(/<neos-inline-wrapper>/g, '<span>')
-                .replace(/<\/neos-inline-wrapper>/g, '</span>');
-        }
-        return contentWithoutOuterInlineWrapper;
-    }
-    return content;
-};
+import {cleanupContentBeforeCommit} from './cleanupContentBeforeCommit'
 
 let currentEditor = null;
 let editorConfig = {};

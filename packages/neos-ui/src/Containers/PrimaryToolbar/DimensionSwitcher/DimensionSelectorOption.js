fbca17a41d723699335846b3520bf482b13a7c44
hwala <henry.wala@paessler.com>
2023-07-21 14:44:29 +0200

    fix: add visual information if dimension exists, if it does provide a link to make it possible to open it in a new browser tab

--- packages/neos-ui/src/Containers/PrimaryToolbar/DimensionSwitcher/DimensionSelectorOption.js
+++ packages/neos-ui/src/Containers/PrimaryToolbar/DimensionSwitcher/DimensionSelectorOption.js
@@ -1,25 +1,51 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import style from './style.module.css';
-// eslint-disable-next-line camelcase
+import SelectBox_Option_SingleLineLink from '@neos-project/react-ui-components/src/SelectBox_Option_SingleLineLink/index';
 import SelectBox_Option_SingleLine from '@neos-project/react-ui-components/src/SelectBox_Option_SingleLine/index';
+import mergeClassNames from 'classnames';
 
 export default class DimensionSelectorOption extends PureComponent {
     static propTypes = {
         option: PropTypes.shape({
             label: PropTypes.string.isRequired,
-            disallowed: PropTypes.bool
+            disallowed: PropTypes.bool,
+            existing: PropTypes.bool,
+            url: PropTypes.string
         })
     };
 
     render() {
         const {option} = this.props;
 
+        const className = mergeClassNames({
+            [style.lighter]: !option.existing,
+            [style.strikethrough]: option.disallowed
+        });
+
+        if (option.existing) {
+            const linkOptions = {
+                className: style.whiteLink,
+                href: option.url,
+                target: '_blank',
+                rel: 'noopener noreferrer',
+                onClick: (event) => event.preventDefault()
+            }
+
+            return (
+                <SelectBox_Option_SingleLineLink
+                    {...this.props}
+                    className={className}
+                    label={option.label}
+                    linkOptions={linkOptions}
+                />
+            );
+        }
+
         return (
-            // eslint-disable-next-line camelcase
             <SelectBox_Option_SingleLine
                 {...this.props}
-                className={option.disallowed ? style.dimmed : ''}
+                className={className}
                 label={option.label}
             />
         );
---
+++ packages/react-ui-components/src/SelectBox_Option_SingleLineLink/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable camelcase, react/jsx-pascal-case */
+import SelectBox_Option_SingleLineLink from './selectBox_Option_SingleLineLink';
+
+export default SelectBox_Option_SingleLineLink;

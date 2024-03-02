1ebdfe24c2a8d1f69c8d258ae93dbd878c56698d
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-03-02 11:35:23 +0100

    TASK: Remove cr import prune hack

--- Tests/IntegrationTests/e2e.sh
+++ Tests/IntegrationTests/e2e.sh
@@ -9,18 +9,16 @@ fi
 cd ../../..
 
 ./flow cr:setup --content-repository onedimension
+./flow cr:import --content-repository onedimension --path ./DistributionPackages/Neos.Test.OneDimension/Resources/Private/Content
+# Connect to a Neos site, todo the nodeTypeName parameter is obsolete but necessary
 ./flow site:create neos-test-onedimension Neos.Test.OneDimension Neos.TestNodeTypes:Document.HomePage
 ./flow domain:add neos-test-onedimension onedimension.localhost --port 8081
-# TODO: Replace with "--assume-yes" flag once "./flow cr:prune" has one
-printf "y\n" | ./flow cr:prune --content-repository onedimension
-./flow cr:import --content-repository onedimension --path ./DistributionPackages/Neos.Test.OneDimension/Resources/Private/Content
 
 ./flow cr:setup --content-repository twodimensions
+./flow cr:import --content-repository twodimensions --path ./DistributionPackages/Neos.Test.TwoDimensions/Resources/Private/Content
+# Connect to a Neos site, todo the nodeTypeName parameter is obsolete but necessary
 ./flow site:create neos-test-twodimensions Neos.Test.TwoDimensions Neos.TestNodeTypes:Document.HomePage
 ./flow domain:add neos-test-twodimensions twodimensions.localhost --port 8081
-# TODO: Replace with "--assume-yes" flag once "./flow cr:prune" has one
-printf "y\n" | ./flow cr:prune --content-repository twodimensions
-./flow cr:import --content-repository twodimensions --path ./DistributionPackages/Neos.Test.TwoDimensions/Resources/Private/Content
 
 ./flow resource:publish
 

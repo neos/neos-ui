852987dff345f190419bc81bfda72eb9c831d2ac
xLaura3m5 <laura.haenel@3m5.de>
2024-03-13 17:40:55 +0100

    TASK: removed unneccessary constrains

--- packages/neos-ui/src/Containers/Drawer/UserDropDown/UserImage.js
+++ packages/neos-ui/src/Containers/Drawer/UserDropDown/UserImage.js
@@ -2,19 +2,17 @@ import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
-import {neos} from '@neos-project/neos-ui-decorators';
-
 import style from './style.module.css';
 
 @connect(state => ({
     userFirstName: state?.user?.name?.firstName,
     userLastName: state?.user?.name?.lastName
 }))
-@neos()
+
 export default class UserImage extends PureComponent {
     static propTypes = {
-        userFirstName: PropTypes.string.isRequired,
-        userLastName: PropTypes.string.isRequired
+        userFirstName: PropTypes.string,
+        userLastName: PropTypes.string
     };
 
     render() {

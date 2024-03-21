28c43c328bdc6712467d100086867fa38210b26a
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2024-03-21 12:07:58 +0100

    TASK: Merge publish and discard sagas

--- packages/neos-ui-sagas/src/Publish/index.js
+++ packages/neos-ui-sagas/src/Publish/index.js
@@ -1,51 +1,76 @@
-import {put, call, select, takeEvery, takeLatest, take, race} from 'redux-saga/effects';
+import {put, call, select, takeEvery, take, race} from 'redux-saga/effects';
 
 import {actionTypes, actions, selectors} from '@neos-project/neos-ui-redux-store';
 import {PublishingMode, PublishingScope} from '@neos-project/neos-ui-redux-store/src/CR/Publishing';
 import backend from '@neos-project/neos-ui-backend-connector';
 import {getGuestFrameDocument} from '@neos-project/neos-ui-guest-frame/src/dom';
 
-export function * watchPublish() {
-    const {publishChangesInSite, publishChangesInDocument} = backend.get().endpoints;
+export function * watchPublishing({routes}) {
+    const {endpoints} = backend.get();
+    const ENDPOINT_BY_MODE_AND_SCOPE = {
+        [PublishingMode.PUBLISH]: {
+            [PublishingScope.SITE]:
+                endpoints.publishChangesInSite,
+            [PublishingScope.DOCUMENT]:
+                endpoints.publishChangesInDocument
+        },
+        [PublishingMode.DISCARD]: {
+            [PublishingScope.SITE]:
+                endpoints.discardChangesInSite,
+            [PublishingScope.DOCUMENT]:
+                endpoints.discardChangesInDocument
+        }
+    };
+    const SELECTORS_BY_SCOPE = {
+        [PublishingScope.SITE]: {
+            ancestorIdSelector: selectors.CR.Nodes.siteNodeContextPathSelector,
+            publishableNodesSelector: selectors.CR.Workspaces.publishableNodesSelector
+        },
+        [PublishingScope.DOCUMENT]: {
+            ancestorIdSelector: selectors.CR.Nodes.documentNodeContextPathSelector,
+            publishableNodesSelector: selectors.CR.Workspaces.publishableNodesInDocumentSelector
+        }
+    };
 
-    yield takeEvery(actionTypes.CR.Publishing.STARTED, function * publishNodes(action) {
-        const {scope, mode} = action.payload;
-        if (mode !== PublishingMode.PUBLISH) {
+    yield takeEvery(actionTypes.CR.Publishing.STARTED, function * publishingWorkflow(action) {
+        const confirmed = yield * waitForConfirmation();
+        if (!confirmed) {
             return;
         }
 
-        const workspaceName = yield select(selectors.CR.Workspaces.personalWorkspaceNameSelector);
+        const {scope, mode} = action.payload;
+        const endpoint = ENDPOINT_BY_MODE_AND_SCOPE[mode][scope];
+        const {ancestorIdSelector, publishableNodesSelector} = SELECTORS_BY_SCOPE[scope];
 
-        if (yield * confirm()) {
-            let feedback = null;
-            let publishedNodes = [];
-            try {
-                if (scope === PublishingScope.SITE) {
-                    const siteId = yield select(selectors.CR.Nodes.siteNodeContextPathSelector);
-                    publishedNodes = yield select(selectors.CR.Workspaces.publishableNodesSelector);
-                    feedback = yield call(publishChangesInSite, siteId, workspaceName);
-                } else if (scope === PublishingScope.DOCUMENT) {
-                    const documentId = yield select(selectors.CR.Nodes.documentNodeContextPathSelector);
-                    publishedNodes = yield select(selectors.CR.Workspaces.publishableNodesInDocumentSelector);
-                    feedback = yield call(publishChangesInDocument, documentId, workspaceName);
-                }
-                yield put(actions.CR.Publishing.succeed());
-            } catch (error) {
-                console.error('Failed to publish', error);
-                yield put(actions.CR.Publishing.fail('Failed to Publish'));
-            }
+        let feedback = null;
+        let publishableNodes = [];
 
-            if (feedback !== null) {
-                yield put(actions.ServerFeedback.handleServerFeedback(feedback));
-            }
+        try {
+            const workspaceName = yield select(selectors.CR.Workspaces.personalWorkspaceNameSelector);
+            const ancestorId = yield select(ancestorIdSelector);
+            publishableNodes = yield select(publishableNodesSelector);
+            feedback = yield call(endpoint, ancestorId, workspaceName)
 
-            yield take(actionTypes.CR.Publishing.ACKNOWLEDGED);
-            yield put(actions.CR.Publishing.finish(publishedNodes));
+            yield put(actions.CR.Publishing.succeed());
+        } catch (error) {
+            console.error('Publishing failed', error);
+            yield put(actions.CR.Publishing.fail('Publishing failed'));
         }
+
+        if (feedback !== null) {
+            yield put(actions.ServerFeedback.handleServerFeedback(feedback));
+        }
+
+        if (mode === PublishingMode.DISCARD) {
+            yield * reloadAfterDiscard(publishableNodes, routes);
+        }
+
+        yield take(actionTypes.CR.Publishing.ACKNOWLEDGED);
+        yield put(actions.CR.Publishing.finish(publishableNodes));
     });
 }
 
-function * confirm() {
+function * waitForConfirmation() {
     const waitForNextAction = yield race([
         take(actionTypes.CR.Publishing.CANCELLED),
         take(actionTypes.CR.Publishing.CONFIRMED)
@@ -57,87 +82,6 @@ function * confirm() {
     }
 
     return false;
-}
-
-export function * watchChangeBaseWorkspace() {
-    const {changeBaseWorkspace} = backend.get().endpoints;
-    yield takeEvery(actionTypes.CR.Workspaces.CHANGE_BASE_WORKSPACE, function * change(action) {
-        try {
-            const documentNode = yield select(
-                state => state?.cr?.nodes?.documentNode
-            );
-            const feedback = yield call(changeBaseWorkspace, action.payload, documentNode);
-            yield put(actions.ServerFeedback.handleServerFeedback(feedback));
-
-            // Reload the page tree
-            yield put(actions.CR.Nodes.reloadState());
-        } catch (error) {
-            console.error('Failed to change base workspace', error);
-        }
-    });
-}
-
-export function * watchRebaseWorkspace() {
-    const {rebaseWorkspace, getWorkspaceInfo} = backend.get().endpoints;
-    yield takeEvery(actionTypes.CR.Workspaces.REBASE_WORKSPACE, function * change(action) {
-        yield put(actions.UI.Remote.startSynchronization());
-
-        try {
-            const feedback = yield call(rebaseWorkspace, action.payload);
-            yield put(actions.ServerFeedback.handleServerFeedback(feedback));
-        } catch (error) {
-            console.error('Failed to sync user workspace', error);
-        } finally {
-            const workspaceInfo = yield call(getWorkspaceInfo);
-            yield put(actions.CR.Workspaces.update(workspaceInfo));
-            yield put(actions.UI.Remote.finishSynchronization());
-        }
-    });
-}
-
-export function * discardIfConfirmed({routes}) {
-    yield takeLatest(actionTypes.CR.Publishing.STARTED, function * waitForConfirmation(action) {
-        const {mode} = action.payload;
-        if (mode !== PublishingMode.DISCARD) {
-            return;
-        }
-
-        if (yield * confirm()) {
-            yield * discard(action.payload.scope, routes);
-        }
-    });
-}
-
-function * discard(scope, routes) {
-    const {discardChangesInSite, discardChangesInDocument} = backend.get().endpoints;
-    const workspaceName = yield select(selectors.CR.Workspaces.personalWorkspaceNameSelector);
-
-    let feedback = null;
-    let discardedNodes = [];
-    try {
-        if (scope === PublishingScope.SITE) {
-            const siteId = yield select(selectors.CR.Nodes.siteNodeContextPathSelector);
-            discardedNodes = yield select(selectors.CR.Workspaces.publishableNodesSelector);
-            feedback = yield call(discardChangesInSite, siteId, workspaceName);
-        } else if (scope === PublishingScope.DOCUMENT) {
-            const documentId = yield select(selectors.CR.Nodes.documentNodeContextPathSelector);
-            discardedNodes = yield select(selectors.CR.Workspaces.publishableNodesInDocumentSelector);
-            feedback = yield call(discardChangesInDocument, documentId, workspaceName);
-        }
-        yield put(actions.CR.Publishing.succeed());
-    } catch (error) {
-        console.error('Failed to discard', error);
-        yield put(actions.CR.Publishing.fail('Failed to discard'));
-    }
-
-    if (feedback !== null) {
-        yield put(actions.ServerFeedback.handleServerFeedback(feedback));
-    }
-
-    yield * reloadAfterDiscard(discardedNodes, routes);
-
-    yield take(actionTypes.CR.Publishing.ACKNOWLEDGED);
-    yield put(actions.CR.Publishing.finish(discardedNodes));
 }
 
 const NODE_HAS_BEEN_CREATED = 0b0001;
@@ -189,4 +133,40 @@ function * reloadAfterDiscard(discardedNodes, routes) {
         // If it's gone navigate to the next available ancestor document
         yield put(actions.UI.ContentCanvas.setSrc(avilableAncestorDocumentNode.uri));
     }
+}
+
+export function * watchChangeBaseWorkspace() {
+    const {changeBaseWorkspace} = backend.get().endpoints;
+    yield takeEvery(actionTypes.CR.Workspaces.CHANGE_BASE_WORKSPACE, function * change(action) {
+        try {
+            const documentNode = yield select(
+                state => state?.cr?.nodes?.documentNode
+            );
+            const feedback = yield call(changeBaseWorkspace, action.payload, documentNode);
+            yield put(actions.ServerFeedback.handleServerFeedback(feedback));
+
+            // Reload the page tree
+            yield put(actions.CR.Nodes.reloadState());
+        } catch (error) {
+            console.error('Failed to change base workspace', error);
+        }
+    });
+}
+
+export function * watchRebaseWorkspace() {
+    const {rebaseWorkspace, getWorkspaceInfo} = backend.get().endpoints;
+    yield takeEvery(actionTypes.CR.Workspaces.REBASE_WORKSPACE, function * change(action) {
+        yield put(actions.UI.Remote.startSynchronization());
+
+        try {
+            const feedback = yield call(rebaseWorkspace, action.payload);
+            yield put(actions.ServerFeedback.handleServerFeedback(feedback));
+        } catch (error) {
+            console.error('Failed to sync user workspace', error);
+        } finally {
+            const workspaceInfo = yield call(getWorkspaceInfo);
+            yield put(actions.CR.Workspaces.update(workspaceInfo));
+            yield put(actions.UI.Remote.finishSynchronization());
+        }
+    });
 }

ced166a066b1c6944907cff142dbe9f52afeabd7
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2024-03-25 12:32:07 +0100

    TASK: Remove legacy `actions.UI.Remote` actions

--- packages/neos-ui-redux-store/src/CR/Workspaces/index.spec.js
+++ packages/neos-ui-redux-store/src/CR/Workspaces/index.spec.js
@@ -4,12 +4,6 @@ import {actionTypes as system} from '../../System/index';
 test(`should export actionTypes`, () => {
     expect(actionTypes).not.toBe(undefined);
     expect(typeof (actionTypes.UPDATE)).toBe('string');
-    expect(typeof (actionTypes.PUBLISH_STARTED)).toBe('string');
-    expect(typeof (actionTypes.PUBLISH_FINISHED)).toBe('string');
-    expect(typeof (actionTypes.DISCARD_STARTED)).toBe('string');
-    expect(typeof (actionTypes.DISCARD_ABORTED)).toBe('string');
-    expect(typeof (actionTypes.DISCARD_CONFIRMED)).toBe('string');
-    expect(typeof (actionTypes.DISCARD_FINISHED)).toBe('string');
     expect(typeof (actionTypes.CHANGE_BASE_WORKSPACE)).toBe('string');
     expect(typeof (actionTypes.REBASE_WORKSPACE)).toBe('string');
 });
@@ -17,12 +11,6 @@ test(`should export actionTypes`, () => {
 test(`should export action creators`, () => {
     expect(actions).not.toBe(undefined);
     expect(typeof (actions.update)).toBe('function');
-    expect(typeof (actions.publish)).toBe('function');
-    expect(typeof (actions.finishPublish)).toBe('function');
-    expect(typeof (actions.discard)).toBe('function');
-    expect(typeof (actions.abortDiscard)).toBe('function');
-    expect(typeof (actions.confirmDiscard)).toBe('function');
-    expect(typeof (actions.finishDiscard)).toBe('function');
     expect(typeof (actions.changeBaseWorkspace)).toBe('function');
     expect(typeof (actions.rebaseWorkspace)).toBe('function');
 });

0da253e0931caa469ed74b00249a7c0e7dc07e03
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2024-05-30 17:59:42 +0200

    TASK: Convert Drawer/constants to typescript

---
+++ packages/neos-ui/src/Containers/Drawer/constants.ts
@@ -0,0 +1,12 @@
+/*
+ * This file is part of the Neos.Neos.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+export const TARGET_WINDOW = 'Window';
+export const TARGET_CONTENT_CANVAS = 'ContentCanvas';
+export const THRESHOLD_MOUSE_LEAVE = 500;

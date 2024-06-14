d9f91e069109456f460ceb6f1eb567fff2b245a6
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2024-06-14 19:52:39 +0200

    TASK: Extract function `substitutePlaceholders`

--- packages/neos-ui-i18n/src/registry/I18nRegistry.js
+++ packages/neos-ui-i18n/src/registry/I18nRegistry.js
@@ -3,56 +3,9 @@ import {SynchronousRegistry} from '@neos-project/neos-ui-extensibility/src/regis
 import logger from '@neos-project/utils-logger';
 
 import {getTranslationAddress} from './getTranslationAddress';
+import {substitutePlaceholders} from './substitutePlaceholders';
 
 const errorCache = {};
-
-/**
- * This code is taken from the Ember version with minor adjustments. Possibly refactor it later
- * as its style is not superb.
- */
-const substitutePlaceholders = function (textWithPlaceholders, parameters) {
-    const result = [];
-    let startOfPlaceholder;
-    let offset = 0;
-    while ((startOfPlaceholder = textWithPlaceholders.indexOf('{', offset)) !== -1) {
-        const endOfPlaceholder = textWithPlaceholders.indexOf('}', offset);
-        const startOfNextPlaceholder = textWithPlaceholders.indexOf('{', startOfPlaceholder + 1);
-
-        if (endOfPlaceholder === -1 || (startOfPlaceholder + 1) >= endOfPlaceholder || (startOfNextPlaceholder !== -1 && startOfNextPlaceholder < endOfPlaceholder)) {
-            // There is no closing bracket, or it is placed before the opening bracket, or there is nothing between brackets
-            logger.error('Text provided contains incorrectly formatted placeholders. Please make sure you conform the placeholder\'s syntax.');
-            break;
-        }
-
-        const contentBetweenBrackets = textWithPlaceholders.substr(startOfPlaceholder + 1, endOfPlaceholder - startOfPlaceholder - 1);
-        const placeholderElements = contentBetweenBrackets.replace(' ', '').split(',');
-
-        const valueIndex = placeholderElements[0];
-        const value = parameters[valueIndex];
-        if (typeof value === 'undefined') {
-            logger.error('Placeholder "' + valueIndex + '" was not provided, make sure you provide values for every placeholder.');
-            break;
-        }
-
-        let formattedPlaceholder;
-        if (typeof placeholderElements[1] === 'undefined') {
-            // No formatter defined, just string-cast the value
-            formattedPlaceholder = parameters[valueIndex];
-        } else {
-            logger.error('Placeholder formatter not supported.');
-            break;
-        }
-
-        result.push(textWithPlaceholders.substr(offset, startOfPlaceholder - offset));
-        result.push(formattedPlaceholder);
-
-        offset = endOfPlaceholder + 1;
-    }
-
-    result.push(textWithPlaceholders.substr(offset));
-
-    return result.join('');
-};
 
 const getPluralForm = (translation, quantity = 0) => {
     const translationHasPlurals = translation instanceof Object;

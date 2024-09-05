9c85cef13d1a7d6e3fad64f935db40f394f5d76c
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-09-05 11:15:43 +0200

    BUGFIX: 3839 & 3832 only issue changes if ckeditor is dirty
    The dirty state can be tracked via the callback see https://github.com/ckeditor/ckeditor5/issues/996#issuecomment-407360730

--- packages/neos-ui-ckeditor5-bindings/src/ckEditorApi.js
+++ packages/neos-ui-ckeditor5-bindings/src/ckEditorApi.js
@@ -57,9 +57,19 @@ export const createEditor = store => async options => {
     return NeosEditor
         .create(propertyDomNode, ckEditorConfig)
         .then(editor => {
+            let isDirty = false;
+            editor.model.document.on('change:data', () => {
+                isDirty = true;
+            });
+            const issueOnChangeIfDirty = () => {
+                if (isDirty) {
+                    isDirty = false;
+                    onChange(cleanupContentBeforeCommit(editor.getData()))
+                }
+            };
             editor.ui.focusTracker.on('change:isFocused', event => {
                 if (!event.source.isFocused) {
-                    onChange(cleanupContentBeforeCommit(editor.getData()))
+                    issueOnChangeIfDirty();
                     return
                 }
 
@@ -74,7 +84,7 @@ export const createEditor = store => async options => {
             });
 
             editor.model.document.on('change', () => handleUserInteractionCallback());
-            editor.model.document.on('change:data', debounce(() => onChange(cleanupContentBeforeCommit(editor.getData())), 1500, {maxWait: 5000}));
+            editor.model.document.on('change:data', debounce(issueOnChangeIfDirty, 1500, {maxWait: 5000}));
             return editor;
         }).catch(e => {
             if (e instanceof TypeError && e.message.match(/Class constructor .* cannot be invoked without 'new'/)) {

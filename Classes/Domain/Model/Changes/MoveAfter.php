33df996450b5c11ab1e221b93bf99a86082e7c45
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2024-10-21 13:42:40 +0200

    BUGFIX: Cut/Paste across dimensions should not lead to error
    After thoroughly thinking this through, I decided
    that moving nodes across dimensions is effectively a copy followed by
    a delete, thus it CHANGES IDENTITIES of nodes then.
    
    This means it will work in all cases, even if the moved node already
    existed with the same ID in the target dimension.
    
    Lateron, we additionally need to expose CreateNodeVariant (which creates
    connected variants) in the UI as well.
    
    Resolves: #4614

--- Classes/Domain/Model/Changes/MoveAfter.php
+++ Classes/Domain/Model/Changes/MoveAfter.php
@@ -13,9 +13,13 @@ namespace Neos\Neos\Ui\Domain\Model\Changes;
  */
 
 use InvalidArgumentException;
+use Neos\ContentRepository\Core\DimensionSpace\OriginDimensionSpacePoint;
+use Neos\ContentRepository\Core\Feature\NodeDuplication\Command\CopyNodesRecursively;
 use Neos\ContentRepository\Core\Feature\NodeMove\Command\MoveNodeAggregate;
 use Neos\ContentRepository\Core\Feature\NodeMove\Dto\RelationDistributionStrategy;
-use Neos\Neos\Ui\Domain\Model\Feedback\Operations\RemoveNode;
+use Neos\ContentRepository\Core\Feature\NodeRemoval\Command\RemoveNodeAggregate;
+use Neos\ContentRepository\Core\Projection\ContentGraph\VisibilityConstraints;
+use Neos\ContentRepository\Core\SharedModel\Node\NodeVariantSelectionStrategy;
 use Neos\Neos\Ui\Domain\Model\Feedback\Operations\UpdateNodeInfo;
 
 /**
@@ -71,19 +75,49 @@ class MoveAfter extends AbstractStructuralChange
             $hasEqualParentNode = $parentNode->aggregateId
                 ->equals($parentNodeOfPreviousSibling->aggregateId);
 
-
             $contentRepository = $this->contentRepositoryRegistry->get($subject->contentRepositoryId);
 
-            $command = MoveNodeAggregate::create(
-                $subject->workspaceName,
-                $subject->dimensionSpacePoint,
-                $subject->aggregateId,
-                RelationDistributionStrategy::STRATEGY_GATHER_ALL,
-                $hasEqualParentNode ? null : $parentNodeOfPreviousSibling->aggregateId,
-                $precedingSibling->aggregateId,
-                $succeedingSibling?->aggregateId,
-            );
-            $contentRepository->handle($command);
+            if (!$precedingSibling->dimensionSpacePoint->equals($subject->dimensionSpacePoint)) {
+                // WORKAROUND for MOVE ACROSS DIMENSIONS:
+                // - we want it to work like a copy/paste, followed by an original delete.
+                // - This is to ensure the user can use it as expected from text editors, where context
+                //   is not preserved during cut/paste.
+                // - LATERON, we need to expose CreateNodeVariant (which creates connected variants) in the UI as well.
+                $command = CopyNodesRecursively::createFromSubgraphAndStartNode(
+                    $contentRepository->getContentGraph($subject->workspaceName)->getSubgraph(
+                        $subject->dimensionSpacePoint,
+                        VisibilityConstraints::withoutRestrictions()
+                    ),
+                    $subject->workspaceName,
+                    $subject,
+                    // NOTE: in order to be able to copy/paste across dimensions, we need to use
+                    // the TARGET NODE's DimensionSpacePoint to create the node in the target dimension.
+                    OriginDimensionSpacePoint::fromDimensionSpacePoint($precedingSibling->dimensionSpacePoint),
+                    $parentNodeOfPreviousSibling->aggregateId,
+                    $succeedingSibling?->aggregateId
+                );
+
+                $contentRepository->handle($command);
+
+                $command = RemoveNodeAggregate::create(
+                    $subject->workspaceName,
+                    $subject->aggregateId,
+                    $subject->dimensionSpacePoint,
+                    NodeVariantSelectionStrategy::STRATEGY_ALL_SPECIALIZATIONS,
+                );
+                $contentRepository->handle($command);
+            } else {
+                $command = MoveNodeAggregate::create(
+                    $subject->workspaceName,
+                    $subject->dimensionSpacePoint,
+                    $subject->aggregateId,
+                    RelationDistributionStrategy::STRATEGY_GATHER_ALL,
+                    $hasEqualParentNode ? null : $parentNodeOfPreviousSibling->aggregateId,
+                    $precedingSibling->aggregateId,
+                    $succeedingSibling?->aggregateId,
+                );
+                $contentRepository->handle($command);
+            }
 
             $updateParentNodeInfo = new UpdateNodeInfo();
             $updateParentNodeInfo->setNode($parentNodeOfPreviousSibling);

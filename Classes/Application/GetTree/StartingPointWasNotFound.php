4381f37b66ce7d79c9913e5ec66bfd32c4d2d0f7
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-04-23 21:36:55 +0200

    WIP: Copy and adjust namespace of archaeopterix backend code

---
+++ Classes/Application/GetTree/StartingPointWasNotFound.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Neos.Neos.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+declare(strict_types=1);
+
+namespace Neos\Neos\Ui\Application\GetTree;
+
+use Neos\ContentRepository\Core\Projection\ContentGraph\AbsoluteNodePath;
+use Neos\ContentRepository\Core\Projection\ContentGraph\ContentSubgraphInterface;
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * @internal
+ */
+#[Flow\Proxy(false)]
+final class StartingPointWasNotFound extends \Exception
+{
+    private function __construct(string $message, int $code)
+    {
+        parent::__construct($message, $code);
+    }
+
+    public static function becauseNodeWithGivenPathDoesNotExistInCurrentSubgraph(
+        AbsoluteNodePath $nodePath,
+        ContentSubgraphInterface $subgraph,
+    ): self {
+        return new self(
+            sprintf(
+                'The starting point at path "%s" does not exist in subgraph: %s',
+                $nodePath->serializeToString(),
+                json_encode([
+                    'contentRepositoryId' => $subgraph->getContentRepositoryId(),
+                    'workspaceName' => $subgraph->getWorkspaceName(),
+                    'dimensionSpacePoint' => $subgraph->getDimensionSpacePoint(),
+                ], JSON_PRETTY_PRINT),
+            ),
+            1715082893
+        );
+    }
+}

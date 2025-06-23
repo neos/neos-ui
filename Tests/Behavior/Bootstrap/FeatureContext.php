b97fc1e3f3ff4706b3ae4ab2e50822ab0445c805
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-06-23 14:56:36 +0200

    TASK: Introduce dedicated get-for-tree endpoints
    The two apis were taken from https://github.com/sitegeist/Sitegeist.Archaeopteryx/tree/2.0
    They were documented originally in the https://github.com/sitegeist/Sitegeist.Archaeopteryx/pull/63 pr
    
    - GetChildrenForTreeNodeQuery
    - GetTreeQuery
    
    Adjusted to the neos ui namespace and uris
    
    The endpoints will yet have to be adjusted to the Neos Ui usecase and have to also be invoked on the client side

---
+++ Tests/Behavior/Bootstrap/FeatureContext.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+use Behat\Behat\Context\Context;
+use Neos\Behat\FlowBootstrapTrait;
+use Neos\Behat\FlowEntitiesTrait;
+use Neos\ContentRepository\BehavioralTests\TestSuite\Behavior\CRBehavioralTestsSubjectProvider;
+use Neos\ContentRepository\Core\ContentRepository;
+use Neos\ContentRepository\Core\Factory\ContentRepositoryServiceFactoryInterface;
+use Neos\ContentRepository\Core\Factory\ContentRepositoryServiceInterface;
+use Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId;
+use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\CRTestSuiteTrait;
+use Neos\ContentRepository\TestSuite\Fakes\FakeContentDimensionSourceFactory;
+use Neos\ContentRepository\TestSuite\Fakes\FakeNodeTypeManagerFactory;
+use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
+
+class FeatureContext implements Context
+{
+    use FlowBootstrapTrait;
+    use FlowEntitiesTrait;
+    use CRTestSuiteTrait;
+    use CRBehavioralTestsSubjectProvider;
+    use NeosUiTrait;
+
+    private ContentRepositoryRegistry $contentRepositoryRegistry;
+
+    public function __construct()
+    {
+        self::bootstrapFlow();
+        $this->contentRepositoryRegistry = $this->getObject(ContentRepositoryRegistry::class);
+    }
+
+    protected function getContentRepositoryService(
+        ContentRepositoryServiceFactoryInterface $factory
+    ): ContentRepositoryServiceInterface {
+        return $this->contentRepositoryRegistry->buildService(
+            $this->currentContentRepository->id,
+            $factory
+        );
+    }
+
+    protected function createContentRepository(
+        ContentRepositoryId $contentRepositoryId
+    ): ContentRepository {
+        $this->contentRepositoryRegistry->resetFactoryInstance($contentRepositoryId);
+        $contentRepository = $this->contentRepositoryRegistry->get($contentRepositoryId);
+        FakeContentDimensionSourceFactory::reset();
+        FakeNodeTypeManagerFactory::reset();
+
+        return $contentRepository;
+    }
+}

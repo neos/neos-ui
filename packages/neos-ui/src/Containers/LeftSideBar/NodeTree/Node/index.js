neos/neos-ui

commit 43efd9c63709ecf87a029d2636b33b0a935d41f3

FEATURE: Timeable node visibility

packages/neos-ui/src/Containers/LeftSideBar/NodeTree/Node/index.js
@@ -170,21 +170,27 @@ export default class Node extends PureComponent {
     getCustomIconComponent() {
         const {node} = this.props;
 
-        const isHidden = node?.properties?._hidden;
-        const isHiddenBefore = node?.properties?.enableAfterDateTime;
-        const isHiddenAfter = node?.properties?.disableAfterDateTime;
+        const isDisabled = node?.properties?._hidden;
+        const enableAfterDateTime = node?.properties?.enableAfterDateTime;
+        const disableAfterDateTime = node?.properties?.disableAfterDateTime;
 
-        if (isHiddenBefore || isHiddenAfter) {
-            let isCurrentlyHidden = false;
-            isCurrentlyHidden = isHiddenBefore && moment(isHiddenBefore).isAfter(moment()) ? true : isCurrentlyHidden;
-            isCurrentlyHidden = isHiddenAfter && moment(isHiddenAfter).isBefore(moment()) ? true : isCurrentlyHidden;
-            const circleColor = isCurrentlyHidden && isHidden ? 'error' : 'primaryBlue';
+        if (enableAfterDateTime || disableAfterDateTime) {
+            const circleColor = isDisabled ? 'error' : 'primaryBlue';
 
             return (
                 <span className="fa-layers fa-fw">
                     <Icon icon={this.getIcon()} />
                     <Icon icon="circle" color={circleColor} transform="shrink-5 down-6 right-4" />
                     <Icon icon="clock" transform="shrink-9 down-6 right-4" />
+                </span>
+            );
+        }
+        if (isDisabled) {
+            return (
+                <span className="fa-layers fa-fw">
+                    <Icon icon={this.getIcon()} />
+                    <Icon icon="circle" color="error" transform="shrink-3 down-6 right-4" />
+                    <Icon icon="times" transform="shrink-7 down-6 right-4" />
                 </span>
             );
         }

commit 7dc19421961f8e8543e682153c088ecdc806249b

BUGFIX: Use correct color for timed visibility icon
This fixes a regression introduced with 43efd9c63709ecf87a029d2636b33b0a935d41f3

Resolves: #3997

packages/neos-ui/src/Containers/LeftSideBar/NodeTree/Node/index.js
@@ -1,16 +1,15 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-
+import moment from 'moment';
+import hashSum from 'hash-sum';
 import flowright from 'lodash.flowright';
+
 import {Tree, Icon} from '@neos-project/react-ui-components';
 import {stripTags, decodeHtml} from '@neos-project/utils-helpers';
-
 import {actions, selectors} from '@neos-project/neos-ui-redux-store';
 import {isNodeCollapsed} from '@neos-project/neos-ui-redux-store/src/CR/Nodes/helpers';
 import {neos} from '@neos-project/neos-ui-decorators';
-
-import hashSum from 'hash-sum';
 import {urlWithParams} from '@neos-project/utils-helpers/src/urlWithParams';
 
 const getContextPath = node => node?.contextPath;
@@ -172,7 +171,13 @@ export default class Node extends PureComponent {
         const hasTimeableNodeVisibility = node?.properties?._hasTimeableNodeVisibility;
 
         if (hasTimeableNodeVisibility) {
-            const circleColor = isDisabled ? 'error' : 'primaryBlue';
+            const {enableAfterDateTime, disableAfterDateTime} = node.properties;
+
+            let isCurrentlyHidden = isDisabled
+                || (enableAfterDateTime && moment(enableAfterDateTime).isAfter(moment()))
+                || (disableAfterDateTime && moment(disableAfterDateTime).isBefore(moment()));
+
+            const circleColor = isDisabled || isCurrentlyHidden ? 'error' : 'primaryBlue';
 
             return (
                 <span className="fa-layers fa-fw">
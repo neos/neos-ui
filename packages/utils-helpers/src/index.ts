be18795825935a09a5ca8d377f2a0969015d0b1a
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-09-30 10:22:17 +0200

    TASK: Remove now obsolete `isUri` implementation

--- packages/utils-helpers/src/index.ts
+++ packages/utils-helpers/src/index.ts
@@ -5,7 +5,6 @@ import requestIdleCallback from './requestIdleCallback';
 import cancelIdleCallback from './cancelIdleCallback';
 import getVersion from './getVersion';
 import isEmail from './isEmail';
-import {isUri} from './isUri';
 import isEqualSet from './isEqualSet';
 import isNil from './isNil';
 import svgToDataUri from './svgToDataUri';
@@ -14,7 +13,6 @@ export {
     decodeHtml,
     getVersion,
     isThenable,
-    isUri,
     isEmail,
     isNil,
     isEqualSet,
--- packages/utils-helpers/src/isUri.ts
+++
@@ -1,6 +0,0 @@
-const isUri = (uri: any) => {
-    const trimedUri = String(uri).trim();
-    return trimedUri === '' ? false : Boolean(trimedUri.match('^(https?://|news://|ftp://|mailto:|tel:|#)'));
-};
-
-export {isUri};
